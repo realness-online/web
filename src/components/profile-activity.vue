402cd6ef6ce07ab67a9da49e07d57e979637a909
Scott Fryxell <sfryxell@me.com>
2017-12-20 18:08:57 -0500

    tests for profile activity

--- src/components/profile-activity.vue
+++ src/components/profile-activity.vue
@@ -1,7 +1,9 @@
 <template>
   <ol id="activity" itemprop="activity">
     <li v-for="event in activity" itemscope itemtype="/activity">
-      <a itemprop="where"   :href="event.where"><b itemprop="what">{{event.what}}</b></a>
+      <a itemprop="where"   :href="event.where">
+        <b itemprop="what">{{event.what}}</b>
+      </a>
       <time itemprop="when" :datetime="event.when"></time>
     </li>
   </ol>
@@ -15,7 +17,6 @@
     props: ['activity'],
     watch: {
       activity() {
-        console.log('inside activity watch')
         Vue.nextTick(() => {
           activity_storage.save()
         })
--- test/unit/specs/profile-activity.spec.js
+++ test/unit/specs/profile-activity.spec.js
@@ -2,9 +2,33 @@ import Vue from 'vue'
 import ProfileActivity from '@/components/profile-activity'
 
 describe('profile-activity.vue', () => {
+  let Constructor
+
+  beforeEach(() => {
+    Constructor = Vue.extend(ProfileActivity)
+  })
+
   it('should render ol#activity', () => {
-    const Constructor = Vue.extend(ProfileActivity)
     const vm = new Constructor().$mount()
     expect(vm.$el).toMatchSnapshot()
   })
+
+  it('should render some activity', () => {
+    let event = {
+      who: '/users/uid',
+      what: 'Created a post',
+      why: 'unknown',
+      when: '2017-12-20T23:01:14.310Z',
+      where: '/posts/1'
+    }
+    const vm = new Constructor({
+      propsData: {
+        activity: [event]
+      }
+
+    }).$mount()
+    // expect(cmp.element).toMatchSnapshot()
+    expect(vm.$el).toMatchSnapshot()
+  })
+
 })
