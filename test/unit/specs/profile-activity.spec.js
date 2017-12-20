3a940102eb7c610c2e41aa5fa956ea67eef20cab
Scott Fryxell <sfryxell@me.com>
2017-12-20 13:47:46 -0500

    first passing test

--- test/unit/specs/profile-activity.spec.js
+++ test/unit/specs/profile-activity.spec.js
@@ -5,7 +5,6 @@ describe('profile-activity.vue', () => {
   it('should render correct contents', () => {
     const Constructor = Vue.extend(ProfileActivity)
     const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
-    .toEqual('Welcome to Your Vue.js App')
+    expect(vm.$el).toMatchSnapshot()
   })
 })
