2c7716664d331a8b24ed9f905ffeafb9002011f7
Scott Fryxell <sfryxell@me.com>
2017-12-20 13:53:52 -0500

    watch tests without coverage

--- test/unit/specs/profile-activity.spec.js
+++ test/unit/specs/profile-activity.spec.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import ProfileActivity from '@/components/profile-activity'
 
 describe('profile-activity.vue', () => {
-  it('should render correct contents', () => {
+  it('should render ol#activity', () => {
     const Constructor = Vue.extend(ProfileActivity)
     const vm = new Constructor().$mount()
     expect(vm.$el).toMatchSnapshot()
