5f568b2280a81b7477c2566992984d1cab1121d2
Scott Fryxell <sfryxell@me.com>
2018-01-13 20:41:48 -0800

    cleanup test

--- test/unit/specs/modules/Storage.spec.js
+++ test/unit/specs/modules/Storage.spec.js
@@ -1,5 +1,4 @@
 import Storage from '@/modules/Storage'
-import Item from '@/modules/Item'
 describe('Storage.js', () => {
   let item_as_string, storage
   beforeEach(() => {
@@ -13,34 +12,27 @@ describe('Storage.js', () => {
      document.body.innerHTML = item_as_string
      storage = new Storage('/person')
   })
-
   describe('hydrate()', ()=>{
     it('exists', () => {
-      expect(Storage.hydrate(item_as_string)).toBeDefined()
+      expect(Storage.hydrate).toBeDefined()
     })
     it('will create an html fragmend from a string', () => {
       expect(Storage.hydrate(item_as_string).getItems().length).toBe(1)
     })
   })
-
-
   describe('load()', () =>{
     it('exists', () => {
       expect(storage.load).toBeDefined()
     })
   })
-
   describe('save()', () => {
     it('exists', () => {
       expect(storage.save).toBeDefined()
     })
-
-    it('saves an item type to local storage', () => {
-
+    it('saves and loads an item from local storage', () => {
       storage.save()
       const items = storage.load()
       expect(items.length).toBe(1)
     })
   })
-
 })
