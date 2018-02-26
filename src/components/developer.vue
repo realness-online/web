d5d43082341d5b958cf27c4d418ca76f76b04224
Scott Fryxell <sfryxell@me.com>
2018-02-26 14:35:14 -0800

    add some tooling for debelopers
    attribue that will outline elements

---
+++ src/components/developer.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="stylus" >
+  @require '../style/variables'
+  [outline]
+  [outline] *
+    outline: 0.25vmin solid blue
+</style>
--- src/index.vue
+++ src/index.vue
@@ -4,6 +4,7 @@
     <aside>
       <activity-list></activity-list>
       <posts-list></posts-list>
+      <developer-tools></developer-tools>
     </aside>
   </section>
 </template>
@@ -12,11 +13,13 @@
   import main_nav from '@/components/main-nav'
   import posts_list from '@/components/posts-list'
   import activity_list from '@/components/activity-list'
+  import developer_tools from '@/components/developer'
   export default {
     components: {
       'main-nav': main_nav,
       'activity-list': activity_list,
-      'posts-list': posts_list
+      'posts-list': posts_list,
+      'developer-tools':developer_tools
     }
   }
 </script>
