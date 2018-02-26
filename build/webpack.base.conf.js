5c629cf4f429981ddcaa8d65d4c32a30a5a1a63a
Scott Fryxell <sfryxell@me.com>
2018-02-25 23:36:06 -0800

    compile without the template compiler.

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -23,7 +23,8 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js',
+      // 'vue$': 'vue/dist/vue.esm.js',
+      'vue$': 'vue/dist/vue.runtime.js',
       '@': resolve('src'),
     }
   },
