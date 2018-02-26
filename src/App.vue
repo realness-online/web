5940bacf4a818ab08c84fd78de9702439981b6c8
Scott Fryxell <sfryxell@me.com>
2018-02-26 15:14:13 -0800

    use src attribute with @ reolver

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
+<style src="@/style/index.styl" lang="stylus"></style>
 <template lang="html">
   <main id="social_network">
     <router-view></router-view>
   </main>
 </template>
-<style lang="stylus">
-  @require '/style'
-</style>
