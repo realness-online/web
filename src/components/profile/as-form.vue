52f96f1db6a2edffb8970d91d468de859e353d8d
Scott Fryxell <sfryxell@me.com>
2018-04-05 15:27:49 -0700

    validate form input for mobile number and username

--- src/components/profile/as-form.vue
+++ src/components/profile/as-form.vue
@@ -1,17 +1,27 @@
 <template lang="html">
   <form id="profile-form">
-    <fieldset>
-      <input id="first-name" type='text' placeholder="First name"
+    <fieldset id="name">
+      <input id="first-name" type='text' placeholder="First"
              v-model="person.first_name"
              v-on:blur="save_person">
-      <input id="last-name" type='text' placeholder="Last name"
+      <input id="last-name" type='text' placeholder="Last"
              v-model="person.last_name"
+             v-on:blur="save_person">
+    </fieldset>
+    <fieldset>
+      <label for="mobile">1+</label>
+      <input id="mobile" type="tel" placeholder="(555) 555-5555"
+             v-model="person.mobile"
+             v-on:keypress="validate_mobile_keypress"
+             v-on:paste="validate_mobile_paste"
              v-on:blur="save_person">
     </fieldset>
     <fieldset>
       <label for="profile-name">/</label>
       <input id='profile-name' type="text" placeholder="profile-name"
              v-model="person.profile_name"
+             v-on:keypress="validate_profile_name_keypress"
+             v-on:paste="validate_profile_name_paste"
              v-on:blur="save_person">
     </fieldset>
   </form>
@@ -19,11 +29,27 @@
 
 <script>
   import {person_storage} from '@/modules/Storage'
+  const valid_mobile = /^[0-9]+$/
+  const valid_username = /^[0-9a-z\-_]+$/
   export default {
     props: ['person'],
     methods: {
-      save_person: function() {
+      save_person() {
         person_storage.save()
+      },
+      validate_profile_name_keypress(event) {
+        if (!event.key.match(valid_username)) { event.preventDefault() }
+      },
+      validate_profile_name_paste(event) {
+        let paste = (event.clipboardData || window.clipboardData).getData('text')
+        if (!paste.match(valid_mobile)) { event.preventDefault() }
+      },
+      validate_mobile_keypress(event) {
+        if (!event.key.match(valid_mobile)) { event.preventDefault() }
+      },
+      validate_mobile_paste(event) {
+        let paste = (event.clipboardData || window.clipboardData).getData('text')
+        if (!paste.match(valid_mobile)) { event.preventDefault() }
       }
     }
   }
@@ -34,12 +60,11 @@
   form#profile-form
     & > fieldset
       padding: (base-line / 2 )
-      border:1vmin solid black
+      border:0.66vmin solid black
       margin-bottom: base-line
       &:last-of-type
         margin-bottom:0
     input
-      width:30vw
       color: red
       &:focus
         outline:0
@@ -48,9 +73,13 @@
       &::placeholder
         color: orange
     label[for=profile-name]
+    label[for=mobile]
       margin-right:-0.3em
     input#first-name
+      width:40%
       margin-right:base-line
+    input#last-name
+      width:40%
     input#profile-name
       width:auto
 </style>
--- test/unit/specs/components/profile/as-form.spec.js
+++ test/unit/specs/components/profile/as-form.spec.js
@@ -5,11 +5,25 @@ describe('as-form.vue', () => {
   it('should render form to set user profile info', () => {
     const person = {
       profile_vector: '/people/scott-fryxell/profile.svg',
-      first_name: 'scott',
-      last_name: 'fryxell',
+      first_name: 'Scott',
+      last_name: 'Fryxell',
       profile_name: 'oingo'
     }
     let wrapper = shallow(as_form, { propsData: { person: person } })
     expect(wrapper.element).toMatchSnapshot()
   })
+
+  describe("#mobile", () =>{
+    it('should only allow number to be entered')
+    it('should only allow number to be pasted in')
+    it('should validate that the user provided a mobile number', () => {
+      // <a href="tel:+13174562564">
+    })
+    it('sign the user in with their phone number', () => {
+      expect(true).toBe(false)
+    })
+  })
+
+  it('should validate that the @username is not taken')
+  it('should only allow lower case text, numbers, dashes, and underscores')
 })
