031a320e262d3ae757e46abb066ffee8c306368f
Scott Fryxell <sfryxell@me.com>
2018-06-27 00:17:39 -0700

    update some packages remove one that i'm not using

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -9,7 +9,6 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
-const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
 const WorkboxPlugin = require('workbox-webpack-plugin')
 const Visualizer = require('webpack-visualizer-plugin')
 const env = process.env.NODE_ENV === 'testing'
