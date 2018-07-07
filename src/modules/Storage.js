0bf0255dddff402f522dfe3696d29375568bdfcc
Scott Fryxell <sfryxell@me.com>
2018-07-06 18:55:24 -0700

    Storage.sync tested up too the call to fetch

--- src/modules/Storage.js
+++ src/modules/Storage.js
@@ -4,11 +4,14 @@ import 'firebase/auth'
 import 'firebase/storage'
 
 class Storage {
+  static hydrate(item_as_string) {
+    return document.createRange().createContextualFragment(item_as_string)
+  }
   constructor(item_type, selector = `[itemtype="/${item_type}"]`,
-    location = `${item_type}.html`) {
+    filename = `${item_type}.html`) {
     this.item_type = item_type
     this.selector = selector
-    this.location = location
+    this.filename = filename
     this.metadata = {'contentType': 'text/html'}
   }
   from_storage() {
@@ -23,7 +26,6 @@ class Storage {
   }
   save() {
     // save the information to local storage and if appropriate to the server
-    // under the currrent users profile.
     let items = document.querySelector(this.selector)
     if (!items) { return false }
     items = items.outerHTML
@@ -34,29 +36,39 @@ class Storage {
     return true
   }
   sync() {
-    // check if post.html already exist for this user.
-      // If so merge it's content whith what's stored locally
-      console.log('It\'s a log!')
-    // if (['person', 'posts'].includes(this.item_type) && navigator.onLine) {
-      // firebase.auth().onAuthStateChanged(user => {
-      //   if (user) {
-      //     const doc_u_path = `/people/${user.phoneNumber}/${this.location}`
-      //     firebase.storage().ref()
-      //       .child(doc_u_path)
-      //       .getDownloadURL()
-      //       .catch(console.log.bind(console))
-      //     local_item = this.from_storage()
-      //
-      //   }
-      // })
-    // }
+    // check if an 'item_type'.html already exist for this user.
+    // If so merge it's content whith what's stored locally
+    const url = this.get_server_file_url()
+    if (['person', 'posts'].includes(this.item_type) && navigator.onLine) {
+      firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+          const doc_u_path = `/people/${user.phoneNumber}/${this.filename}`
+          firebase.storage().ref().child(doc_u_path).getDownloadURL().then((url) => {
+            fetch(url).then(response => {
+              response.text().then(server_text => {
+                const server_as_fragment = Storage.hydrate(server_text)
+                let server_as_list = Item.get_items(server_as_fragment)
+                console.log(server_as_list)
+              })
+              let items = [...this.from_storage, ...from_server]
+              // console.log(items)
+            })
+          })
+        }
+      })
+    }
   }
+
+  get_server_file_url() {
+
+  }
+
   persist(doc_u_ment, doc_u_path) {
     firebase.auth().onAuthStateChanged(user => {
       if (user && navigator.onLine) {
-        const file = new File([doc_u_ment], this.location)
+        const file = new File([doc_u_ment], this.filename)
         if (!doc_u_path) {
-          doc_u_path = `/people/${user.phoneNumber}/${this.location}`
+          doc_u_path = `/people/${user.phoneNumber}/${this.filename}`
         }
         firebase.storage().ref()
           .child(doc_u_path)
@@ -64,9 +76,6 @@ class Storage {
           .catch(console.log.bind(console))
       }
     })
-  }
-  static hydrate(item_as_string) {
-    return document.createRange().createContextualFragment(item_as_string)
   }
 }
 
--- test/unit/specs/modules/Storage.spec.js
+++ test/unit/specs/modules/Storage.spec.js
@@ -1,5 +1,9 @@
 import Storage from '@/modules/Storage'
-describe('@/modules/Storage.js', () => {
+import as_form from '@/components/profile/as-form'
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+
+describe.only('@/modules/Storage.js', () => {
   let item_as_string, person
   beforeEach(() => {
     item_as_string = `
@@ -12,52 +16,87 @@ describe('@/modules/Storage.js', () => {
     document.body.innerHTML = item_as_string
     person = new Storage('person')
   })
-  describe('CRUD', () => {
-    describe('hydrate()', () => {
-      it('exists', () => {
-        expect(Storage.hydrate).toBeDefined()
-      })
-      it('will create an html fragmend from a string', () => {
-        person = Storage.hydrate(item_as_string)
-        expect(person.querySelectorAll('h1').length).toBe(1)
-      })
+  describe('#hydrate', () => {
+    it('exists', () => {
+      expect(Storage.hydrate).toBeDefined()
     })
-    describe('from_storage()', () => {
-      it('exists', () => {
-        expect(person.from_storage).toBeDefined()
-      })
+    it('will create an html fragmend from a string', () => {
+      person = Storage.hydrate(item_as_string)
+      expect(person.querySelectorAll('h1').length).toBe(1)
     })
-    describe('save()', () => {
-      it('exists', () => {
-        expect(person.save).toBeDefined()
-      })
+  })
+  describe('#from_storage', () => {
+    it('exists', () => {
+      expect(person.from_storage).toBeDefined()
     })
-    describe('retrieving objects from html', () => {
-      describe('as_list()', () => {
-        it('exists', () => {
-          expect(person.as_list).toBeDefined()
-        })
-        it('will return a list of items')
-      })
-      describe('as_object()', () => {
-        it('exists', () => {
-          expect(person.as_object).toBeDefined()
-        })
-        it('will return the first item it finds')
-      })
+  })
+  describe('#save()', () => {
+    beforeEach(() => {
+      // turn off persistence for this test
+      person.persist = () => { return true }
+    })
+    it('exists', () => {
+      expect(person.save).toBeDefined()
     })
     it('saves and loads an item from local storage', () => {
-      person.persist = () => { return true }
       expect(person.save()).toBe(true)
       const items = person.from_storage()
       expect(items.querySelectorAll('h1').length).toBe(1)
     })
-
   })
-  describe('syncing', () => {
-    it('syncs when the user signs in')
-    it('only saves posts, profile, profile_image')
+  describe('#sync', () => {
+    beforeEach(() => {
+      // const onAuthStateChanged = jest.fn(state_changed => state_changed())
+      const onAuthStateChanged = jest.fn(state_changed => {
+        state_changed({user: person})
+      })
+
+      let auth_mock = jest.spyOn(firebase, 'auth').mockImplementation(() => {
+        return { onAuthStateChanged }
+      })
+
+      let storage_mock = jest.spyOn(firebase, 'storage').mockImplementation(() => {
+        return {
+          ref: jest.fn(() => {
+            return {
+              child: jest.fn(() => {
+                return {
+                  getDownloadURL: jest.fn(() => Promise.resolve('http://example.com/file.html'))
+                }
+              })
+            }
+          })
+        }
+      })
+
+    })
+    it('exists', () => {
+      expect(person.sync).toBeDefined()
+    })
+    it('syncs posts from server to local storage', () => {
+      person.sync()
+    })
+  })
+  describe('#persist', () => {
+    it('exists', () => {
+      expect(person.persist).toBeDefined()
+    })
     it('does not save activity to the server')
+    it('only saves posts, profile, profile_image')
+  })
+  describe('retrieving objects from html', () => {
+    describe('as_list()', () => {
+      it('exists', () => {
+        expect(person.as_list).toBeDefined()
+      })
+      it('will return a list of items')
+    })
+    describe('as_object()', () => {
+      it('exists', () => {
+        expect(person.as_object).toBeDefined()
+      })
+      it('will return the first item it finds')
+    })
   })
   describe('indexing', () => {
     it('creates an index page for posts')
