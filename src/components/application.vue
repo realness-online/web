af1d0ab64937107576d772d9847912de32a9ff6a
Scott Fryxell <sfryxell@me.com>
2018-07-08 13:07:47 -0700

    don't need this anymore now that posts list is handling sync decisions

--- src/components/application.vue
+++ src/components/application.vue
@@ -7,25 +7,9 @@
 </template>
 <script>
   import developer_tools from '@/components/developer'
-  import * as firebase from 'firebase/app'
-  import 'firebase/auth'
-  import {person_storage} from '@/modules/Storage'
-
   export default {
-    data() {
-      return {
-        storage: person_storage
-      }
-    },
     components: {
       'developer-tools': developer_tools
-    },
-    created() {
-      firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-          this.storage.sync()
-        }
-      })
     }
   }
 </script>
