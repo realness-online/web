8ec64250387f001e4b2da836927ba58553b88e4b
Scott Fryxell <sfryxell@me.com>
2018-07-07 17:34:10 -0700

    and now it sorts

--- src/components/posts-list.vue
+++ src/components/posts-list.vue
@@ -34,25 +34,18 @@
             response.text().then(server_text => {
               const server_as_fragment = Storage.hydrate(server_text)
               let from_server = Item.get_items(server_as_fragment)
-              console.log('local list:', posts_storage.as_list())
-              console.log('server list:', from_server)
-
               let filtered_local = posts_storage.as_list().filter(local_item => {
                 return !from_server.some(server_item => {
                   return local_item.created_at === server_item.created_at
                 })
               })
-              console.log('filtered_local:', filtered_local)
-              // let filtered_server = from_server.filter(server_item => {
-              //   return posts_storage.as_list().some(local_item => {
-              //     return !server_item.created_at === local_item.created_at
-              //   })
-              // })
-              // console.log('filtered_server:', filtered_server)
               let items = [...filtered_local, ...from_server]
-              console.log('merged list:', items)
+              items.sort((a,b) => {
+                let a_date = Date.parse(a.created_at)
+                let b_date = Date.parse(b.created_at)
+                return a_date - b_date
+              })
               this.posts = items
-              // console.log('sorted list:')
             })
           })
         })
