7237840866a682cea55ed59d8929aac7546d7d9c
Scott Fryxell <sfryxell@me.com>
2018-07-08 20:51:11 -0700

    some tests for profile as list

---
+++ test/unit/specs/components/profile/as-list.spec.js
@@ -0,0 +1,5 @@
+describe('indexing', () => {
+  it('creates an index page for posts')
+  it('creates an global index page of users to search')
+  it('updates a search index of users when they are saved')
+})
