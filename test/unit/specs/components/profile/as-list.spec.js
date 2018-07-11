adc1193b44fc022e12d0e0809409104b4a278d7b
Scott Fryxell <sfryxell@me.com>
2018-07-11 15:55:05 -0700

    setting up to write lists of users to and from the server

--- test/unit/specs/components/profile/as-list.spec.js
+++ test/unit/specs/components/profile/as-list.spec.js
@@ -1,4 +1,4 @@
-describe('indexing', () => {
+describe('as-list', () => {
   it('creates an index page for posts')
   it('creates an global index page of users to search')
   it('updates a search index of users when they are saved')
