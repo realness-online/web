164becf664f72b1fc454e3a291da4b1b240e9dc2
Scott Fryxell <sfryxell@me.com>
2018-07-23 11:25:03 -0700

    these tests are not covered in phonebook.vue

--- test/unit/specs/components/profile/as-list.spec.js
+++ test/unit/specs/components/profile/as-list.spec.js
@@ -8,9 +8,5 @@ describe('@/compontent/profile/as-list.vue', () => {
   it('should render a list of people', () => {
     expect(wrapper.element).toMatchSnapshot()
   })
-  it('creates an index page for people when')
-
-  it('listens for an sign in event')
-  it('creates an global index page of users to search')
-  it('updates a search index of users when they are saved')
+  
 })
