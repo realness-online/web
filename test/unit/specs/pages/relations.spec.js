c451caa7ae5c63dc827217f57ec3b67d26ccdbb6
Scott Fryxell <sfryxell@me.com>
2018-07-25 08:21:44 -0700

    tests for relations events

--- test/unit/specs/pages/relations.spec.js
+++ test/unit/specs/pages/relations.spec.js
@@ -2,8 +2,27 @@ import { shallow } from 'vue-test-utils'
 import relations from '@/pages/relations'
 
 describe('@/pages/relations.vue', () => {
+  let wrapper
+  const person = {
+    first_name: 'Scott',
+    last_name: 'Fryxell',
+    mobile: '4151234356'
+  }
+  beforeEach(() => {
+    wrapper = shallow(relations)
+  })
   it('render relationship information', () => {
-    let wrapper = shallow(relations)
     expect(wrapper.element).toMatchSnapshot()
+  })
+  it('should emit an add-relationship event', () => {
+    expect(wrapper.vm.relations.length).toBe(0)
+    wrapper.vm.$bus.$emit('add-relationship', person)
+    expect(wrapper.vm.relations.length).toBe(1)
+  })
+  it('should respond to a remove-relationship event', () => {
+    wrapper.setData({relations:[person]})
+    expect(wrapper.vm.relations.length).toBe(1)
+    wrapper.vm.$bus.$emit('remove-relationship', person)
+    expect(wrapper.vm.relations.length).toBe(0)
   })
 })
