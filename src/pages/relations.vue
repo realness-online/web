c19579b312acde5a0dd2282c6aeeeeb942a0d825
Scott Fryxell <sfryxell@me.com>
2018-07-31 18:31:21 -0700

    moving some common css into section.styl

--- src/pages/relations.vue
+++ src/pages/relations.vue
@@ -16,22 +16,8 @@
 </script>
 <style lang='stylus'>
   @require '../style/variables'
-  section#relations
-    position: relative
-    & > header
-      &:focus-within > h1
-        transition-property: all
-        overflow: hidden
-        width:0
-      & > h1
-        color:blue
-        vertical-align: top
-        margin: 0
-        line-height: 1.33
-        @media (min-width: max-screen)
-          line-height: .66
-    svg.heart
-      fill:blue
-      height: (base-line * 2)
-      width: (base-line * 2)
+  section#relations svg.heart
+    fill:blue
+    height: (base-line * 2)
+    width: (base-line * 2)
 </style>
