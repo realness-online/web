bc4861029e79404a8ab0147f126edd84dd528974
Scott Fryxell <sfryxell@me.com>
2018-08-05 13:12:08 -0700

    ad a property and functionality to remove navigation from within a figure.
    has no affect on object properties. 
    
    storing without a nav is smaller

--- src/components/profile/as-figure.vue
+++ src/components/profile/as-figure.vue
@@ -2,15 +2,17 @@
   <figure class="profile" itemscope itemtype='/person'>
     <meta itemprop="created_at" :content="person.created_at">
     <meta itemprop="updated_at" :content="person.updated_at">
-    <a :href="profile_link">
+    <a v-if="nav" :href="profile_link">
       <svg><use itemprop="image" :xlink:href="guaranteed_image"/></svg>
     </a>
+    <svg v-else><use itemprop="image" :xlink:href="guaranteed_image"/></svg>
     <figcaption>
       <p>
         <span itemprop="first_name">{{person.first_name}}</span>
         <span itemprop="last_name">{{person.last_name}}</span>
       </p>
-      <a itemprop="mobile" :data-value="person.mobile" :href="sms_link">{{mobile_display}}</a>
+      <a v-if="nav" itemprop="mobile" :data-value="person.mobile" :href="sms_link">{{mobile_display}}</a>
+      <p v-else itemprop="mobile" :data-value="person.mobile">{{mobile_display}}</p>
     </figcaption>
   </figure>
 </template>
@@ -18,13 +20,24 @@
   import { AsYouType } from 'libphonenumber-js'
   import icons from '@/icons.svg'
   export default {
-    props: ['person'],
+    props: {
+      person: Object,
+      nav: {
+        type: Boolean,
+        default: true
+      }
+    },
+    data() {
+      return {
+        me: true
+      }
+    },
     computed: {
       sms_link() {
         return !!this.person.mobile && `sms:+1${this.person.mobile}`
       },
       profile_link() {
-        return !!this.person.mobile && `/profiles/+1${this.person.mobile}`
+        return !!this.person.mobile && `/+1${this.person.mobile}`
       },
       mobile_display() {
         return new AsYouType('US').input(this.person.mobile)
@@ -49,12 +62,12 @@
     & > a > svg
       fill: black
       standard-border()
-      border-radius: (base-line / 3)
     & > figcaption
       padding-left: (base-line / 2)
       vertical-align: middle
       line-height: (base-line * 2)
       & > p
+        color:black
         margin:0
         & > span
           text-transform: capitalize
