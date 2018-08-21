e057f5f2831833bfc3c143ce6be2ca8e43249ac9
Scott Fryxell <sfryxell@me.com>
2018-08-20 21:25:11 -0700

    fully test the profile

--- test/unit/specs/pages/profile.spec.js
+++ test/unit/specs/pages/profile.spec.js
@@ -1,6 +1,5 @@
 import { shallow } from 'vue-test-utils'
 import profile from '@/pages/profile'
-
 describe('@/pages/profile.vue', () => {
   it('shows the users profile information', () => {
     const $route = {
@@ -19,7 +18,6 @@ describe('@/pages/profile.vue', () => {
         mobile: '4151231234'
       }
     }
-    profile.methods.get_items = jest.fn(() => Promise.resolve('result of confirm_spy') )
     let wrapper = shallow(profile, {
       mocks: {
         $route
@@ -28,7 +26,31 @@ describe('@/pages/profile.vue', () => {
     expect(wrapper.element).toMatchSnapshot()
   })
   it('#get_url', () => {
+    expect.assertions(1)
+    const $route = {
+      params: {}
+    }
+    let wrapper = shallow(profile, {
+      mocks: {
+        $route
+      }
+    })
+    wrapper.vm.get_url('4151231234', 'posts').then(url => {
+      expect(url).toBe('https://download_url/people/+14151231234/posts.html')
+    })
   })
   it('#get_items', () => {
+    expect.assertions(1)
+    const $route = {
+      params: {}
+    }
+    let wrapper = shallow(profile, {
+      mocks: {
+        $route
+      }
+    })
+    wrapper.vm.get_items('4151231234', 'posts').then(items => {
+      expect(items).toEqual([])
+    })
   })
 })
