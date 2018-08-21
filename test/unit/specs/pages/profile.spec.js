8aed4c08d4848e4b3f6acae9c82c704506d5e287
Scott Fryxell <sfryxell@me.com>
2018-08-20 18:54:48 -0700

    test the ways created can be called

--- test/unit/specs/pages/profile.spec.js
+++ test/unit/specs/pages/profile.spec.js
@@ -4,9 +4,7 @@ import profile from '@/pages/profile'
 describe('@/pages/profile.vue', () => {
   it('shows the users profile information', () => {
     const $route = {
-        params: {
-          mobile: '4151231234'
-        }
+      params: {}
     }
     let wrapper = shallow(profile, {
       mocks: {
@@ -14,5 +12,23 @@ describe('@/pages/profile.vue', () => {
       }
     })
     expect(wrapper.element).toMatchSnapshot()
+  })
+  it('shows a phone numbers profile information', () => {
+    const $route = {
+      params: {
+        mobile: '4151231234'
+      }
+    }
+    profile.methods.get_items = jest.fn(() => Promise.resolve('result of confirm_spy') )
+    let wrapper = shallow(profile, {
+      mocks: {
+        $route
+      }
+    })
+    expect(wrapper.element).toMatchSnapshot()
+  })
+  it('#get_url', () => {
+  })
+  it('#get_items', () => {
   })
 })
