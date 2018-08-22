da547d06574fe837ecee6f95e6e513e9710268b8
Scott Fryxell <sfryxell@me.com>
2018-08-21 20:13:51 -0700

    feed testing

--- src/pages/feed.vue
+++ src/pages/feed.vue
@@ -42,74 +42,45 @@
       },
       sort_feed() {
         this.feed.sort((a, b) => {
-          console.log('sorting')
           return Date.parse(a.created_at) - Date.parse(b.created_at)
         })
       },
       insert_me_into_my_posts() {
         const me = person_storage.as_object()
         let my_posts = posts_storage.as_list()
-        my_posts.forEach(post => {
-          post.person = me
-        })
+        my_posts.forEach(post => (post.person = me))
         this.feed.push(...my_posts)
       },
       add_posts_to_feed(person, their_posts_as_text) {
         console.log(person.first_name)
         let posts = Item.get_items(Storage.hydrate(their_posts_as_text))
-        posts.forEach(post => (post.person = person))
+        posts.forEach(post => {
+          post.person = person
+        })
         this.feed.push(...posts)
       },
       add_relations_to_feed() {
-        relations_storage.as_list().forEach((person) => {
-          this.posts_url(person).then((url) => {
-            fetch(url).then((response) => {
-              response.text().then(text => {
-                this.add_posts_to_feed(person, text)
-                this.sort_feed()
-                this.working = false
+        return new Promise((resolve, reject) => {
+          relations_storage.as_list().forEach((person) => {
+            this.posts_url(person).then((url) => {
+              fetch(url).then((response) => {
+                response.text().then(text => {
+                  this.add_posts_to_feed(person, text)
+                  this.sort_feed()
+                  this.working = false
+                  resolve(this.working)
+                })
               })
             })
           })
         })
-      },
-      initialize_feed() {
-        this.insert_me_into_my_posts()
-        this.add_relations_to_feed()
-      },
-      fetch_posts(post_urls = []) {
-        console.log(post_urls)
-        // post_urls.map(fetch)
-        // post_urls.map(fetch).then(responses => {
-        //   console.log('no way')
-        // })
       }
     },
     created() {
-      this.initialize_feed()
-      // let relations = relations_storage.as_list()
-      // Promise.all(relations.map(this.posts_url)).then(urls => {
-      //   console.log(urls)
-      //   this.fetch_posts(urls)
-      // })
-      // Promise.all(urls.map(fetch)).then(responses =>
-      //   Promise.all(responses.map(res => res.text())
-      // ).then(texts => {
-      //   this.sort_feed()
-      // })
+      this.insert_me_into_my_posts()
+      this.add_relations_to_feed()
     }
   }
-  // check if their updated since last time.
-  // if their are new updates then update Feed
-  // this requires that posts update the updated at property for each person
-  // when they create a post
-  // let profile_path = `/people/+1${person.mobile}/profile.html`
-  // this.get_item_by_path(relations_path).then((network_person) => {
-  //   if (network_person.updated_at > person.updated_at){
-  //
-  //     console.log('getting updates for:', person.first_name)
-  //
-  //   }
 </script>
 <style lang="stylus">
   @require '../style/variables'
--- test/unit/specs/pages/feed.spec.js
+++ test/unit/specs/pages/feed.spec.js
@@ -1,12 +1,74 @@
-import { shallow } from 'vue-test-utils'
+import {shallow} from 'vue-test-utils'
+import Storage from '@/modules/Storage'
 import feed from '@/pages/feed'
-
+const server_text = `
+  <div itemprop="posts" itemref="profile">
+   <article itemscope="itemscope" itemtype="/post"><blockquote itemprop="articleBody">This is a word</blockquote> <time itemprop="created_at" datetime="2018-04-13T20:02:50.533Z"></time></article>
+   <article itemscope="itemscope" itemtype="/post"><blockquote itemprop="articleBody">These are some words.</blockquote> <time itemprop="created_at" datetime="2018-07-05T23:25:58.145Z"></time></article>
+   <article itemscope="itemscope" itemtype="/post"><blockquote itemprop="articleBody">I am writing some words right now testing some magical magic out</blockquote> <time itemprop="created_at" datetime="2018-07-07T20:13:42.760Z">2 days ago</time></article>
+   <article itemscope="itemscope" itemtype="/post"><blockquote itemprop="articleBody">zipper faced monkey</blockquote> <time itemprop="created_at" datetime="2018-07-07T23:11:51.460Z"></time></article>
+   <article itemscope="itemscope" itemtype="/post"><blockquote itemprop="articleBody">from the bottom of the zero's</blockquote> <time itemprop="created_at" datetime="2018-07-07T23:44:05.363Z"></time></article>
+  </div>`
 describe('@/pages/feed.vue', () => {
   it('render feed info', () => {
     let wrapper = shallow(feed)
     expect(wrapper.element).toMatchSnapshot()
   })
-  it('should create feed from my relationships', () => {
-    // expect.assertions(1)
+  it('#posts_url', () => {
+    expect.assertions(1)
+    let wrapper = shallow(feed)
+    let person = {
+      mobile: '4152223666'
+    }
+    wrapper.vm.posts_url(person).then(url => {
+      expect(url).toBe('https://download_url/people/+14152223666/posts.html')
+    })
+  })
+  it('#sort_feed', () => {
+    expect.assertions(1)
+    let wrapper = shallow(feed)
+    wrapper.setData({
+      feed: [
+        {
+          name: 'later',
+          created_at: '2018-05-19T20:27:30.118Z'
+        },
+        {
+          name: 'earlier',
+          created_at: '2017-05-19T20:27:30.118Z'
+        }
+      ]
+    })
+    wrapper.vm.sort_feed()
+    expect(wrapper.vm.feed[1].name).toBe('later')
+  })
+  it('#insert_me_into_my_posts', () => {
+    expect.assertions(2)
+    let wrapper = shallow(feed)
+    jest.spyOn(Storage.prototype, 'as_list').mockImplementation(() => {
+      return [{}]
+    })
+    jest.spyOn(Storage.prototype, 'as_object').mockImplementation(() => {
+      return {first_name:'scott'}
+    })
+    wrapper.vm.insert_me_into_my_posts()
+    expect(wrapper.vm.feed.length).toBe(1)
+    expect(wrapper.vm.feed[0].person.first_name).toBe('scott')
+  })
+  it('#add_relations_to_feed', () => {
+    expect.assertions(1)
+    fetch.mockResponseOnce(server_text)
+    let relation = {
+      first_name: 'katie',
+      last_name: 'caffey',
+      mobile: '2134445566'
+    }
+    let wrapper = shallow(feed)
+    jest.spyOn(Storage.prototype, 'as_list').mockImplementation(() => {
+      return [relation]
+    })
+    wrapper.vm.add_relations_to_feed().then(() => {
+      expect(wrapper.vm.feed.length).toBe(5)
+    })
   })
 })
