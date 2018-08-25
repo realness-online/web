42e04762fbf79a8a68fe46eb4d19f89168422284
Scott Fryxell <sfryxell@me.com>
2018-08-24 19:47:53 -0700

    quick and dirty cleanup for events

--- src/pages/events.vue
+++ src/pages/events.vue
@@ -1,24 +1,31 @@
 <template>
   <section id="events" class="page">
     <header>
+      <icon name="hamburger"></icon>
       <h1>Events</h1>
-      <nav>
-        <router-link to="/">home</router-link>
-      </nav>
+      <logo-as-link></logo-as-link>
     </header>
+    <h2>It's gonna be a minute</h2>
   </section>
 </template>
-
 <script>
+  import logoAsLink from '@/components/logo-as-link'
+  import icon from '@/components/icon'
   export default {
+    components: {
+      logoAsLink,
+      icon
+    },
     data() {
       return {}
     }
   }
 </script>
-
 <style lang="stylus">
   @require '../style/variables'
   section#events
-    padding: 0 base-line
+    & > header
+      margin-bottom: base-line
+      & > svg
+        fill: transparent
 </style>
