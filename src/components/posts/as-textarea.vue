88689cf147e94702bc47c6863ab6527ccb57c976
Scott Fryxell <sfryxell@me.com>
2018-08-28 11:45:01 -0700

    sigh. kristin called it a millenial what.

--- src/components/posts/as-textarea.vue
+++ src/components/posts/as-textarea.vue
@@ -3,7 +3,7 @@
     v-model="new_post"
     v-on:focusout="prepare_post"
     v-on:focusin="wat_focused"
-    placeholder="Wat?">{{value}}</textarea>
+    placeholder="What?">{{value}}</textarea>
 </template>
 
 <script>
