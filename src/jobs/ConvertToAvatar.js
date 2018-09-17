9f41debf18fa052bdf8ffadb607c9ffd0d7eff6d
Scott Fryxell <sfryxell@me.com>
2018-09-17 15:58:30 -0700

    set up for testing firebase functions

---
+++ src/jobs/ConvertToAvatar.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const os = require('os')
+// const {Storage} = require('@google-cloud/storage')
+class ConvertToAvatar {
+  constructor(image) {
+    this.local_image = path.join(os.tmpdir(), path.basename(image.name))
+    this.as_file = this.local_image.replace(/\.[^/.]+$/, "")
+    this.server_image  = image.name
+  }
+  download() {
+    return storage.bucket('/people').file(server_image).download({
+      destination: local_image
+    })
+  }
+}
+export default ConvertToAvatar
+// reference links:
+// for croping to a square
+//  https://www.imagemagick.org/discourse-server/viewtopic.php?t=28283
+// removing file extension
+//  https://stackoverflow.com/questions/39007908/filename-without-extension-terminology
--- test/unit/specs/jobs/convert_to_avatar.spec.js
+++
@@ -1,6 +0,0 @@
-describe('@/jobs/convert_to_avatar', () => {
-  it('it should covert the image to a square ')
-  it('it should resize the image to 128x128px')
-  it('it should trace the imgae into an avatar')
-  it('it should optimize the avatar') 
-})
