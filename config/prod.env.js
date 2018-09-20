f5ddbaea1faa57a93f55e0e94d4414ce0e4d6c39
Scott Fryxell <sfryxell@me.com>
2018-09-20 13:14:29 -0700

    push to staging until merged back into master.

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,12 +2,12 @@
 module.exports = {
   NODE_ENV: '"production"',
   FIREBASE_CONFIG: `{
-    apiKey: 'AIzaSyAq__BJhue1yWE0zmufmW_k6AxwseQkTjQ',
-    authDomain: 'realness-online.firebaseapp.com',
-    databaseURL: 'https://realness-online.firebaseio.com',
-    projectId: 'realness-online',
-    storageBucket: 'realness-online.appspot.com',
-    messagingSenderId: '276668807926'
+    apiKey: "AIzaSyCzVtxfwUPcf9WZZa2hdJk8xf56Orforto",
+    authDomain: "realness-staging.firebaseapp.com",
+    databaseURL: "https://realness-staging.firebaseio.com",
+    projectId: "realness-staging",
+    storageBucket: "realness-staging.appspot.com",
+    messagingSenderId: "412986481355"
   }`
 }
 
@@ -18,3 +18,11 @@ module.exports = {
 // projectId: "realness-staging",
 // storageBucket: "realness-staging.appspot.com",
 // messagingSenderId: "412986481355"
+
+// production:
+// apiKey: 'AIzaSyAq__BJhue1yWE0zmufmW_k6AxwseQkTjQ',
+// authDomain: 'realness-online.firebaseapp.com',
+// databaseURL: 'https://realness-online.firebaseio.com',
+// projectId: 'realness-online',
+// storageBucket: 'realness-online.appspot.com',
+// messagingSenderId: '276668807926'
