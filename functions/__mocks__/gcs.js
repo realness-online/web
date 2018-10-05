78a0dbae2f6950e80c5b638672c786a9d16566b9
Scott Fryxell <sfryxell@me.com>
2018-10-04 18:17:31 -0700

    Make upload_mock and file_mock available to tests.

--- functions/__mocks__/gcs.js
+++ functions/__mocks__/gcs.js
@@ -1,27 +1,32 @@
 const download_mock = jest.fn(config => {
   // console.log(`download() called with ${config}`);
-  return Promise.resolve(name);
+  return Promise.resolve(name)
+})
+
+const upload_mock = jest.fn((tempFilePath, config) => {
+  // console.log(`Jest @google-cloud/storage.bucket.upload called with ${tempFilePath} and config`, config);
+  return config.destination
+})
+
+const file_mock = jest.fn(name => {
+  // console.log(`Jest @google-cloud/storage.bucket.file() called with ${name}`);
+  return {
+    download: download_mock
+  }
 })
 // @google-cloud/storage mock needs to be in a separate file because of https://github.com/facebook/jest/issues/553
 function bucketFn(bucketName) {
   // console.log(`Jest @google-cloud/storage.bucket called with ${bucketName}`);
   return {
-    upload: jest.fn((tempFilePath, config) => {
-      // console.log(`Jest @google-cloud/storage.bucket.upload called with ${tempFilePath} and config`, config);
-      return config.destination;
-    }),
-    file: jest.fn(name => {
-      // console.log(`Jest @google-cloud/storage.bucket.file() called with ${name}`);
-      return {
-        download: download_mock
-      };
-    })
-  };
+    upload: upload_mock,
+    file: file_mock
+  }
 }
-
 module.exports = options => {
   return {
     download_mock,
+    upload_mock,
+    file_mock,
     bucket: bucketFn
   };
 };
