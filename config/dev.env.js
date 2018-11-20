008ff680c113cdded1ae0b75691e36f08e9fde6c
Scott Fryxell <sfryxell@me.com>
2018-11-20 11:15:49 -0800

    updated dev enviroment

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,11 +5,11 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   FIREBASE_CONFIG: `{
-    apiKey: "AIzaSyDpRbQe67nfP2HTxkThxhY2Fk-ru0x2aus",
-    authDomain: "littleman-8f289.firebaseapp.com",
-    databaseURL: "https://littleman-8f289.firebaseio.com",
-    projectId: "littleman-8f289",
-    storageBucket: "littleman-8f289.appspot.com",
-    messagingSenderId: "363642054727"
+    apiKey: "AIzaSyBVTH4MM_gtcjiyiIicUvknrS7VGFRP9d4",
+    authDomain: "realness-development.firebaseapp.com",
+    databaseURL: "https://realness-development.firebaseio.com",
+    projectId: "realness-development",
+    storageBucket: "realness-development.appspot.com",
+    messagingSenderId: "639469569733"
   }`
 })
