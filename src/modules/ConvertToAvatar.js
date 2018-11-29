9b21bcf752bbaab12471919dfc816f08e3efe2c6
Scott Fryxell <sfryxell@me.com>
2018-11-28 17:35:45 -0800

    remove commented code

--- src/modules/ConvertToAvatar.js
+++ src/modules/ConvertToAvatar.js
@@ -1,9 +1,3 @@
-// import 'jimp/browser/lib/jimp'
-// import jimp from 'jimp'
-// import potrace from 'potrace'
-// const jimp = require('jimp')
-// window.Buffer = require('buffer/').Buffer // note: the trailing slash is important!
-// window.Jimp = jimp
 const potrace = require('potrace')
 function trace(avatar_image) {
   return new Promise((resolve, reject) => {
@@ -25,37 +19,3 @@ function trace(avatar_image) {
 export default {
   trace
 }
-// let reader = new FileReader()
-// reader.onload = function(readerEvent) {
-//   console.log('onload')
-//   let arrayBuffer = this.result
-//   let array = new Uint8Array(arrayBuffer)
-//   let binaryString = String.fromCharCode.apply(null, array)
-//   console.log(binaryString)
-// }
-// let avatar_data = reader.readAsArrayBuffer(avatar_image)
-// console.log(avatar_data)
-// var reader = new FileReader()
-// var fileByteArray = []
-// var hope_buffer = null
-// reader.readAsArrayBuffer(avatar_image)
-// reader.onloadend = function (evt) {
-//   if (evt.target.readyState === FileReader.DONE) {
-//     var arrayBuffer = evt.target.result
-//     var array = new Uint8Array(arrayBuffer)
-//     hope_buffer = new Uint8Array(arrayBuffer)
-//     for (var i = 0; i < array.length; i++) {
-//       fileByteArray.push(array[i])
-//     }
-//   }
-// }
-// document.querySelector('input').addEventListener('change', function() {
-//   var reader = new FileReader();
-//   reader.onload = function() {
-//     var arrayBuffer = this.result
-//     var array = new Uint8Array(arrayBuffer)
-//     var binaryString = String.fromCharCode.apply(null, array)
-//     console.log(binaryString);
-//   }
-//   reader.readAsArrayBuffer(this.files[0]);
-// }, false);
