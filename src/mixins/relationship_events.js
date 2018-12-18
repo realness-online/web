ef98f3578bdbec25290b2eb9f72eb9030e4985d4
Scott Fryxell <sfryxell@me.com>
2018-12-18 10:21:30 -0800

    Remove comments from relationship_events

--- src/mixins/relationship_events.js
+++ src/mixins/relationship_events.js
@@ -8,17 +8,14 @@ export default {
     }
   },
   created() {
-    console.log('relationship_events')
     this.$bus.$off('remove-relationship')
     this.$bus.$off('add-relationship')
     this.$bus.$on('add-relationship', person => {
-      console.log('add-relationship', person)
       this.relations.push(person)
       localStorage.setItem('relations-count', this.relations.length)
       Vue.nextTick(() => relations_storage.save())
     })
     this.$bus.$on('remove-relationship', person => {
-      console.log('remove-relationship', person)
       const index = this.relations.findIndex(p => (p.id === person.id))
       if (index > -1) {
         this.relations.splice(index, 1)
