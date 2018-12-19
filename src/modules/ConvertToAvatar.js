6f533ed5d79d968b4e36cc3bbf1619c6a1c0c1f6
Scott Fryxell <sfryxell@me.com>
2018-12-19 11:48:59 -0800

    Ignore coverage info for ConvertToAvatar
    this code is going to be refactored inside events branch

--- src/modules/ConvertToAvatar.js
+++ src/modules/ConvertToAvatar.js
@@ -1,7 +1,7 @@
 const potrace = require('potrace')
 const Jimp = require('jimp')
 const EXIF = require('exif-js');
-
+/* istanbul ignore next */
 function trace(avatar, identifier) {
   return new Promise((resolve, reject) => {
     console.log('trace...')
