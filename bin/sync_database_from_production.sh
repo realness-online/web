08c6f34715b3b923757ba173f56c65dbdeeb6e29
Scott Fryxell <sfryxell@me.com>
2018-12-22 09:18:35 -0800

    Add a script to sync data from production to staging and development.

---
+++ bin/sync_database_from_production.sh
@@ -0,0 +1,12 @@
+##!/usr/bin/env bash
+
+# delete staging repo then copy repo from production
+gsutil -m rm -r gs://realness-staging.appspot.com/people
+gsutil -m cp -R gs://realness-online.appspot.com/people/ gs://realness-staging.appspot.com/
+
+# delete development repo
+gsutil -m rm -r gs://realness-development.appspot.com/people
+gsutil -m cp -R gs://realness-online.appspot.com/people/ gs://realness-development.appspot.com/
+
+# delete and copy to the desktop too
+gsutil -m cp -R gs://realness-online.appspot.com/people/ ~/realness.online/
