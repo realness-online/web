fee0cde4cde8f61f412a88bd1b54313b6aded45e
Scott Fryxell <sfryxell@me.com>
2019-01-16 13:31:45 -0800

    Main application is ported over

--- src/router.js
+++ src/router.js
@@ -1,24 +1,33 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Events from '@/views/Events'
+import Feed from '@/views/Feed'
+import Where from '@/views/Where'
+import Account from '@/views/Account'
+import Index from '@/views/Index'
+import Profile from '@/views/Profile'
+import Avatar from '@/views/Avatar'
+import Relations from '@/views/Relations'
+import Phonebook from '@/views/PhoneBook'
 
-Vue.use(Router);
-
+Vue.use(Router)
 export default new Router({
   mode: 'history',
+  el: '#loading-realness',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
+    { path: '/', component: Index },
+    { path: '/events', component: Events },
+    { path: '/feed', component: Feed },
+    { path: '/where', component: Where },
+    { path: '/relations', component: Relations },
+    { path: '/phonebook', component: PhoneBook },
+    { path: '/profile', component: Profile },
+    { path: '/account',
+      name: 'account',
+      component: () => import(/* webpackChunkName: "account" */ './views/Account.vue')
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-    },
-  ],
-});
+    { path: '/:phone_number/avatar', component: avatar },
+    { path: '/:phone_number', component: profile }
+  ]
+})
