0fc586fe5aabcfe8c89bf3ece2049723722b6390
Scott Fryxell <sfryxell@me.com>
2019-01-16 16:23:46 -0800

    Index says it's version number

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <style src="@/style/index.styl" lang="stylus"></style>
 <template lang="html">
   <main id="realness">
+    <h6>Version {{version}}</h6>
     <router-view></router-view>
     <developer-tools></developer-tools>
   </main>
@@ -14,12 +15,34 @@
         sessionStorage.previous = from.path
       }
     },
+    data() {
+      return {
+        version: process.env.VUE_APP_VERSION
+      }
+    },
     created() {
-      console.log('initialize app');
-      firebase.initializeApp(process.env.FIREBASE_CONFIG)
+      console.log('initialize app')
+      let config = {
+        apiKey: process.env.VUE_APP_API_KEY,
+        authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+        databaseUrl: process.env.VUE_APP_DATABASE_URL,
+        projectId: process.env.VUE_APP_PROJECT_ID,
+        storage_bucket: process.env.VUE_APP_STORAGE_BUCKET,
+        messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID
+      }
+      firebase.initializeApp(config)
     },
     components: {
       'developer-tools': developer_tools
     }
   }
 </script>
+<style lang="stylus">
+  @require '/style/variables'
+  #realness > h6
+    position:fixed
+    bottom: base-line
+    left: base-line
+    color: green
+    opacity: 0.5
+</style>
