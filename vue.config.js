81af5ea60c3db3f814b8f1fd7954b91c899acc0b
Scott Fryxell <sfryxell@me.com>
2019-01-17 16:46:43 -0800

    Configureing PWA features

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,20 @@
 
 process.env.VUE_APP_VERSION = require('./package.json').version
 module.exports = {
-  configureWebpack:{
+  pwa: {
+    themeColor: '#52a0d1',
+    msTileColor: '#52a0d1',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black-translucent',
+    iconPaths: {
+      favicon32: 'icons/favicon-32x32.png',
+      favicon16: 'icons/favicon-16x16.png',
+      appleTouchIcon: 'icons/apple-touch-icon.png',
+      maskIcon: 'icons/safari-pinned-tab.svg',
+      msTileImage: 'icons/mstile-150x150.png'
+    }
+  },
+  configureWebpack: {
     optimization: {
       splitChunks: {
         maxSize: 250000
