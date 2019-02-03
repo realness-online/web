f21c979922269428539bd1e52938a02504cd614b
Scott Fryxell <sfryxell@me.com>
2019-02-03 15:23:59 -0800

    Check if there are updated on load

--- src/controller.js
+++ src/controller.js
@@ -1,4 +1,4 @@
-
+console.log('I am the service worker');
 workbox.core.setCacheNameDetails({prefix: "Realness"});
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
@@ -11,3 +11,12 @@ self.addEventListener('message', message => {
     self.skipWaiting
   }
 })
+
+self.addEventListener('install', function(event) {
+  // The promise that skipWaiting() returns can be safely ignored.
+  self.skipWaiting()
+  clients.claim()
+  // Perform any other actions required for your
+  // service worker to install, potentially inside
+  // of event.waitUntil();
+});
