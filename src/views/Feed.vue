a5fde9c65dea111b2e31bce2d147bb435de515d7
Scott Fryxell <sfryxell@me.com>
2019-03-11 09:24:20 -0700

    exponentially grow the feed length limit as the user scrolls to the bottom.
    This will render their entire feed if they choose, while initially just 
    rendering the first 13 or so posts.

--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -1,5 +1,5 @@
 <template>
-  <section id="feed" class="page left">
+  <section id="feed" class="page left" @:scroll.passive="scrolled">
     <header>
       <icon name='nothing'></icon>
       <h1>Feed</h1>
@@ -7,7 +7,7 @@
     </header>
     <icon v-show="working" name="working"></icon>
     <profile-as-list :people='relations'></profile-as-list>
-    <article v-if="!working" v-for="post in size_limited_feed" itemscope itemtype="/post">
+    <article v-if="!working" v-for="post in size_limited_feed" itemscope itemtype="/post" >
       <header>
         <profile-as-figure :person='post.person' :avatar_by_reference="true"></profile-as-figure>
       </header>
@@ -32,12 +32,18 @@
     },
     data() {
       return {
-        feed_limit: 100,
+        feed_limit: 13,
         feed: [],
         relations: [],
         working: true,
         unsorted_relations: null
       }
+    },
+    beforeMount () {
+      window.addEventListener('scroll', this.scrolled)
+    },
+    beforeDestroy () {
+      window.removeEventListener('scroll', this.scrolled)
     },
     created() {
       const people_in_feed = relations_storage.as_list()
@@ -46,6 +52,13 @@
       this.insert_me_into_feed()
     },
     methods: {
+      scrolled(o)  {
+        const last_article = document.querySelector('#feed > article:last-of-type')
+        const bottom = last_article.getBoundingClientRect().bottom
+        if (bottom < window.scrollY && this.feed.length > this.feed_limit) {
+          this.feed_limit = this.feed_limit * 2
+        }
+      },
       insert_me_into_feed() {
         const me = person_storage.as_object()
         let my_posts = posts_storage.as_list()
