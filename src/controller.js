272e79efb6cecb92f296b79635ebcaf94c410b55
Scott Fryxell <scott@Kates-MacBook-Air.local>
2019-04-09 12:28:19 -0700

    Cleanup what's in the controller

--- src/controller.js
+++ src/controller.js
@@ -1,16 +1,7 @@
-console.log('I am the service worker');
 workbox.core.setCacheNameDetails({prefix: "Realness"});
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-
-// self.addEventListener('message', message => {
-//   console.log(message)
-//   if(message.data.action == 'skip_waiting') {
-//     console.log('calling skipWaiting')
-//     self.skipWaiting
-//   }
-// })
 
 self.addEventListener('install', function(event) {
   self.skipWaiting().then(() => {
@@ -19,6 +10,5 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('activate', function(event) {
-  console.log('calling self.update()')
   self.update()
 });
