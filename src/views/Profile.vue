44f163ffdcccade2c100cf05c512645c33490010
Scott Fryxell <sfryxell@me.com>
2019-05-08 16:10:40 -0700

    Simplify profile layout

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,15 +1,12 @@
 <template lang="html">
   <section id="profile" class="page" ref='profile'>
-    <profile-as-avatar v-if="!is_me" v-bind:by_reference="true" :person="person"></profile-as-avatar>
     <header>
-      <profile-as-figure :person='person'></profile-as-figure>
+      <svg></svg>
       <logo-as-link></logo-as-link>
     </header>
-    <footer>
-      <h1>Posts</h1>
-      <my-posts v-if="is_me"></my-posts>
-      <posts-list v-else :posts='posts'></posts-list>
-    </footer>
+    <profile-as-avatar v-bind:by_reference="true" :person="person"></profile-as-avatar>
+    <profile-as-figure :person='person'></profile-as-figure>
+    <posts-as-list :posts='posts'></posts-as-list>
   </section>
 </template>
 <script>
@@ -18,19 +15,16 @@
   import 'firebase/auth'
   import '@/modules/time_ago'
   import profile_id from '@/modules/profile_id'
-  import { person_storage, posts_storage } from '@/modules/Storage'
-  import profileAsFigure from '@/components/profile/as-figure'
-  import profileAsAvatar from '@/components/profile/as-avatar'
-  import logoAsLink from '@/components/logo-as-link'
-  import postsList from '@/components/posts/as-list'
-  import myPosts from '@/components/posts/my-list'
+  import logo_as_link from '@/components/logo-as-link'
+  import profile_as_figure from '@/components/profile/as-figure'
+  import profile_as_avatar from '@/components/profile/as-avatar'
+  import posts_as_list from '@/components/posts/as-list'
   export default {
     components: {
-      profileAsFigure,
-      profileAsAvatar,
-      myPosts,
-      postsList,
-      logoAsLink
+      'profile-as-figure': profile_as_figure,
+      'profile-as-avatar': profile_as_avatar,
+      'posts-as-list': posts_as_list,
+      'logo-as-link': logo_as_link
     },
     data() {
       return {
@@ -40,57 +34,32 @@
       }
     },
     created() {
-      const phone_number = this.$route.params.phone_number
-      if (phone_number) {
-        this.load_from_network(phone_number)
-      } else {
-        firebase.auth().onAuthStateChanged(user => {
-          if (user) {
-            // this.load_from_local()
-            this.load_from_network(user.phoneNumber)
-          } else {
-            this.load_from_local()
-          }
-        })
-      }
-    },
-    methods: {
-
-      load_from_network(phone_number) {
-        const id = `/${phone_number}`
-        profile_id.load(id).then(profile => {
-          this.person = profile
-        })
-        profile_id.items(id, 'posts').then(items => {
-          this.posts = items
-          this.working = false
-        })
-      },
-      load_from_local() {
-        this.person = person_storage.as_object()
-        this.posts = posts_storage.as_list()
+      const id = profile_id.from_e64(this.$route.params.phone_number)
+      profile_id.load(id).then(profile => {
+        this.person = profile
+      })
+      profile_id.items(id, 'posts').then(items => {
+        this.posts = items
         this.working = false
-      }
-    },
-    computed: {
-      is_me() {
-        return person_storage.as_object().id === this.person.id
-      }
+      })
     }
   }
 </script>
 <style lang='stylus'>
   section#profile
     & > header
-    & > footer
+      margin-bottom: -(base-line * 4)
+      z-index: 2
+      position: relative
+      & > a
+        -webkit-tap-highlight-color: blue
+    & > figure
+    & > div
       margin: auto
       max-width: page-width
-    & > footer
-      padding: 0 base-line
-    & > header > a
-      -webkit-tap-highlight-color: blue
+      padding: base-line
     & > svg:not(.working)
       width: 100vw
       min-height: 100vh
-      fill: blue
+      fill: white
 </style>
