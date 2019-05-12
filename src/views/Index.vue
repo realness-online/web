1da48b26243ecf041a9978c0a8fcdf10855c8df3
Scott Fryxell <sfryxell@me.com>
2019-05-12 08:00:30 -0700

    Update local version of profile once per session.
    This guarantees that local version of posts and profile are synced with 
    the network

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -4,20 +4,52 @@
     <h6 class="app_version">{{version}}</h6>
     <aside>
       <my-posts></my-posts>
+      <my-figure :person="me"></my-figure>
     </aside>
   </section>
 </template>
 <script>
-  import mainNav from '@/components/main-nav'
-  import myPosts from '@/components/posts/my-list'
+  import Vue from 'vue'
+  import * as firebase from 'firebase/app'
+  import 'firebase/auth'
+  import profile_id from '@/modules/profile_id'
+  import { person_storage } from '@/modules/Storage'
+  import main_nav from '@/components/main-nav'
+  import my_posts_as_list from '@/components/posts/my-list'
+  import as_figure from '@/components/profile/as-figure'
   export default {
     components: {
-      mainNav,
-      myPosts
+      'main-nav': main_nav,
+      'my-posts': my_posts_as_list,
+      'my-figure': as_figure
     },
     data() {
       return {
-        version: process.env.VUE_APP_VERSION
+        version: process.env.VUE_APP_VERSION,
+        me: person_storage.as_object()
+      }
+    },
+    created() {
+      if (!sessionStorage.getItem('profile_synced')) {
+        firebase.auth().onAuthStateChanged(user => {
+          if (user) {
+            const id = profile_id.from_e64(user.phoneNumber)
+            profile_id.load(id).then(profile => {
+              this.me = profile
+              this.me.id = id
+            })
+          }
+        })
+      }
+    },
+    watch: {
+      me() {
+        Vue.nextTick(() => {
+          person_storage.save().then(message => {
+            sessionStorage.setItem('profile_synced', Date.now)
+            console.log('profile_synced')
+          })
+        })
       }
     }
   }
