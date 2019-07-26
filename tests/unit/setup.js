ee91339a0159ea83824917d5abb20d60c089f66f
Scott Fryxell <sfryxell@me.com>
2019-07-25 20:05:52 -0700

    added default currentUser
    tests will act as though a user is logged in

--- tests/unit/setup.js
+++ tests/unit/setup.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import * as firebase from 'firebase/app'
 import 'firebase/storage'
+import 'firebase/auth'
 Vue.prototype.$bus = new Vue({})
 Vue.config.productionTip = false
 global.fetch = require('jest-fetch-mock')
-const storage_mock = jest.spyOn(firebase, 'storage').mockImplementation(() => {
+const currentUser = { phoneNumber: '+16282281824' }
+jest.spyOn(firebase, 'auth').mockImplementation(() => {
+  return { currentUser: currentUser}
+})
+jest.spyOn(firebase, 'storage').mockImplementation(() => {
   return {
     ref: jest.fn(() => {
       return {
