6e2a4bf43be7930dcbc075520d52389bace4f95b
Scott Fryxell <sfryxell@me.com>
2019-07-28 23:31:24 -0700

    add outline for doing statments

---
+++ src/components/posts/as-statement.vue
@@ -0,0 +1,5 @@
+<template lang="html">
+  <p itemprop="statement" :data-who="statement.who" :data-why="statement.why" :data-created_at="statement.created_at" :data-where="statement.where">
+    {{statement.what}}
+  </p>
+</template>
