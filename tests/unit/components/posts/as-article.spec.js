7a040c670dbce7fbeff878645c294d74f959d1d0
Scott Fryxell <sfryxell@me.com>
2019-07-29 09:56:34 -0700

    basic test for as-article

---
+++ tests/unit/components/posts/as-article.spec.js
@@ -0,0 +1,25 @@
+import { shallow } from 'vue-test-utils'
+import as_article from '@/components/posts/as-article'
+const person = {
+  first_name: 'Scott',
+  last_name: 'Fryxell',
+  id: '/+14151234356'
+}
+const post = {
+  articleBody: 'I am saying it',
+  created_at: '2019-05-11T22:40:04.580Z',
+  id: '/+14151234356/2019-05-11T22:40:04.580Z',
+  statements: [],
+  person
+}
+describe('@/components/posts/as-article.vue', () => {
+  let wrapper
+  beforeEach(() => {
+    wrapper = shallow(as_article, {
+      propsData: { post }
+    })
+  })
+  it('Render a post as an article element', () => {
+    expect(wrapper.element).toMatchSnapshot()
+  })
+})
