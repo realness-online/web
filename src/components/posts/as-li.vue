3d7fe4a40e3190a4d16299be16eb8664368ae7df
Scott Fryxell <sfryxell@me.com>
2019-08-05 10:18:15 -0700

    Reference computed properties correctly

--- src/components/posts/as-li.vue
+++ src/components/posts/as-li.vue
@@ -1,8 +1,8 @@
 <template lang="html">
   <li itemscope itemtype='/post' :itemid="id">
     <meta itemprop="muted" v-if="post.muted" :content="post.muted">
-    <time itemprop="created_at" :datetime="post.created_at">{{created_time}}</time>
-    <p itemprop="statement" :contenteditable="me" @blur="save_me">{{statement}}</p>
+    <time itemprop="created_at" :datetime="post.created_at">{{as_created_time}}</time>
+    <p itemprop="statement" :contenteditable="me" @blur="save_me">{{as_statement}}</p>
   </li>
 </template>
 <script>
