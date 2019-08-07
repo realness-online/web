a2511e41feb9dec97a3ab7043e7c3819e360d051
Scott Fryxell <sfryxell@me.com>
2019-08-06 17:52:26 -0700

    ItemId is redundant.

--- src/components/posts/as-article.vue
+++ src/components/posts/as-article.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-  <article itemscope itemtype='/post' :itemid="id" :key="id">
+  <article itemscope itemtype='/post' :key="id">
     <time itemprop="created_at" :datetime="post.created_at">{{as_created_time}}</time>
     <p itemprop="statement" :contenteditable="me" @blur="save">{{as_statement}}</p>
     <ol v-if="has_statements">
