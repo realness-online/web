3fcf6899d51085bb6abeea4eb460af27e9c828a0
Scott Fryxell <sfryxell@me.com>
2019-08-11 15:03:06 -0700

    Move logging output for clarity

--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -75,9 +75,9 @@
         everyones_posts.forEach(post => this.insert_post_into_day(post, this.days))
       },
       async next_page(person) {
-        console.log(`posts.${person.page}`, person.first_name)
         if (person.page) person.page = growth.next(person.page)
         else person.page = growth.first()
+        console.log(`posts.${person.page}`, person.first_name)
         let posts = await profile.items(person.id, `posts.${person.page}`)
         posts = this.condense_posts(posts, person)
         posts.forEach(post => this.insert_post_into_day(post, this.days))
