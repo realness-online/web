deb40f0968d8cefd4589c01daee1d3908b7d8c2d
Scott Fryxell <sfryxell@me.com>
2019-08-10 18:00:23 -0700

    load a profiles posts and sort them into pages
    just like the account page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -6,36 +6,67 @@
     </header>
     <profile-as-avatar v-bind:by_reference="true" :person="person"></profile-as-avatar>
     <profile-as-figure :person='person'></profile-as-figure>
-
+    <div id="pages-of-posts">
+      <div :itemprop="page_name" v-for="[page_name, days] in pages" :key="page_name">
+        <section class="day" v-for="[date, day] in days" :key="date" v-bind:class="{today: is_today(date)}">
+          <header><h4>{{as_day(date)}}</h4></header>
+          <post-as-article v-for="post in day" :key="post.id"
+                           :post="post"
+                           :person="post.person"
+                           @end-of-articles="next_page">
+          </post-as-article>
+        </section>
+      </div>
+    </div>
   </section>
 </template>
 <script>
-  import profile_id from '@/helpers/profile'
+  import posts_into_days from '@/mixins/posts_into_days'
+  import date_mixin from '@/mixins/date'
+  import condense_posts from '@/mixins/condense_posts'
+  import profile from '@/helpers/profile'
   import logo_as_link from '@/components/logo-as-link'
+  import growth from '@/modules/growth'
   import profile_as_figure from '@/components/profile/as-figure'
   import profile_as_avatar from '@/components/profile/as-avatar'
-
+  import as_article from '@/components/posts/as-article'
   export default {
+    mixins: [date_mixin, condense_posts, posts_into_days],
     components: {
       'profile-as-figure': profile_as_figure,
       'profile-as-avatar': profile_as_avatar,
-      'logo-as-link': logo_as_link
+      'logo-as-link': logo_as_link,
+      'post-as-article': as_article
     },
     data() {
       return {
+        pages: new Map(),
+        limit: growth.first(),
         working: true,
-        person: {},
-        posts: []
+        person: {}
       }
     },
     mounted() {
       document.body.scrollTop = document.documentElement.scrollTop = 0
     },
     async created() {
-      const id = profile_id.from_e64(this.$route.params.phone_number)
-      this.person = await profile_id.load(id)
-      this.posts = await profile_id.items(id, 'posts')
-      this.working = false
+      const id = profile.from_e64(this.$route.params.phone_number)
+      let [person, posts] = await Promise.all([
+        profile.load(id),
+        profile.items(id, 'posts')
+      ])
+      this.person = person
+      this.populate_pages(person, posts)
+    },
+    methods: {
+      next_page(){},
+      populate_pages(person, posts) {
+        const days = new Map()
+        posts = this.condense_posts(posts, person)
+        posts.forEach(post => this.insert_post_into_day(post, days))
+        this.pages = new Map(this.pages.set(`posts`, days));
+        this.working = false
+      }
     }
   }
 </script>
