6201feb1ba7543214142e3689d1f69a04f5484cd
Scott Fryxell <sfryxell@me.com>
2019-08-19 15:33:15 -0700

    Add a check for the same day

--- src/helpers/date.js
+++ src/helpers/date.js
@@ -1,4 +1,4 @@
-const a_year_ago = Date.parse(new Date().getFullYear())
+const this_year = Date.parse(new Date().getFullYear())
 const format_as_time = {
   hour: 'numeric',
   minute: 'numeric',
@@ -30,14 +30,21 @@ export default {
   },
   as_day(date) {
     let day
-    if (Date.parse(date) < a_year_ago) {
+    if (Date.parse(date) < this_year) {
       day = this.as_time(date, format_as_day_and_year)
     } else day = this.as_time(date, format_as_day)
-    const today = this.as_time(new Date().toISOString(), format_as_day)
-    if (day === today) day = `Today – ${day}`
+    if (this.same_day(date, new Date())) day = `Today – ${day}`
     return day
   },
   as_day_and_time(date) {
     return this.as_time(date, format_as_day_and_time)
+  },
+  same_day(d1, d2) {
+    d1 = new Date(d1)
+    d2 = new Date(d2)
+    return d1.getFullYear() === d2.getFullYear() &&
+      d1.getDate() === d2.getDate() &&
+      d1.getMonth() === d2.getMonth()
   }
+
 }
