50cfd73930f04f470ad660cbe0199221fa80a2b2
Scott Fryxell <sfryxell@me.com>
2019-09-08 19:32:14 -0700

    lets animate the logo like a button when it's active

--- src/components/logo-as-link.vue
+++ src/components/logo-as-link.vue
@@ -7,3 +7,11 @@
     components: { icon }
   }
 </script>
+<style lang="stylus">
+  id="logo"
+  svg.realness:active
+    padding-top: 0.22em
+    transition-duration: 0.1s
+    transition-timing-function: ease
+    animation-duration: 0s
+</style>
