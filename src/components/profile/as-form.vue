3e77886e8b2d3dcca57541d660bb9ffdea0c7d3d
Scott Fryxell <sfryxell@me.com>
2019-09-08 18:18:36 -0700

    Call modified apropriatly

--- src/components/profile/as-form.vue
+++ src/components/profile/as-form.vue
@@ -3,10 +3,10 @@
     <fieldset id="name">
       <input id="first-name" type='text' tabindex="1" placeholder="First" required
              v-model="person.first_name"
-             v-on:blur="save_person">
+             v-on:blur="modified">
       <input id="last-name" type='text' tabindex="2"  placeholder="Last" required
              v-model="person.last_name"
-             v-on:blur="save_person">
+             v-on:blur="modified">
     </fieldset>
     <fieldset id="phone" v-if="!show_sign_out">
       <label for="mobile">1</label>
@@ -14,8 +14,8 @@
              v-model="person.mobile"
              v-on:keypress="mobile_keypress"
              v-on:keyup="mobile_keyup"
-             v-on:paste="mobile_paste"
-             v-on:blur="save_person">
+             v-on:paste.prevent="mobile_paste"
+             v-on:blur="modified">
     </fieldset>
     <fieldset id="captcha"
          v-if='show_captcha'
@@ -31,13 +31,13 @@
       <button id="authorize"
               :disabled="disabled_sign_in"
               v-if="show_authorize"
-              v-on:click='begin_authorization'>Sign in</button>
+              v-on:click.prevent='begin_authorization'>Sign in</button>
       <button id='submit-verification'
               v-if="show_code"
-              v-on:click="sign_in_with_code">Sign in</button>
+              v-on:click.prevent="sign_in_with_code">Sign in</button>
       <button id="sign-out"
               v-if="show_sign_out"
-              v-on:click="sign_out">
+              v-on:click.prevent="sign_out">
               <icon name="remove"></icon>
       </button>
     </menu>
@@ -49,6 +49,7 @@
   import { parseNumber } from 'libphonenumber-js'
   import profile_id from '@/helpers/profile'
   import icon from '@/components/icon'
+  import { person_local } from '@/modules/LocalStorage'
   export default {
     props: ['person'],
     components: {
@@ -91,64 +92,56 @@
     methods: {
       validate_mobile_number(event) {
         const is_valid = !!this.person.mobile && parseNumber(this.person.mobile, 'US').phone
-        if (is_valid) {
-          this.disabled_sign_in = false
-        } else {
-          this.disabled_sign_in = true
-        }
+        if (is_valid) this.disabled_sign_in = false
+        else this.disabled_sign_in = true
         return is_valid
       },
       disable_input() {
         const mobile = this.$el.querySelector('#mobile')
         if (mobile) mobile.disabled = true
       },
-      save_person() {
-        this.$emit('modified', this.person)
+      modified() {
+        console.log('modified')
+        const me = person_local.as_object()
+        let modified = false
+        if (me.id != this.person.id) modified = true
+        if (me.first_name != this.person.first_name) modified = true
+        if (me.last_name != this.person.last_name) modified = true
+        if (modified) this.$emit('modified', this.person)
       },
-      begin_authorization(event) {
-        event.preventDefault()
+      async begin_authorization(event) {
         this.working = true
         this.disable_input()
-        this.save_person()
         this.show_authorize = false
         this.show_captcha = true
-        this.$nextTick(() => {
-          this.human = new firebase.auth.RecaptchaVerifier('captcha', {
-            'size': 'invisible',
-            'badge': 'inline',
-            'callback': this.text_human_verify_code
-          })
-          this.human.verify()
+        await this.$nextTick()
+        this.human = new firebase.auth.RecaptchaVerifier('captcha', {
+          'size': 'invisible',
+          'badge': 'inline',
+          'callback': this.text_human_verify_code
         })
+        this.human.verify()
       },
-      text_human_verify_code(response) {
+      async text_human_verify_code(response) {
         this.working = false
         this.show_code = true
         this.show_captcha = false
         this.hide_captcha = true
-        firebase.auth()
-          .signInWithPhoneNumber(`+1${this.person.mobile}`, this.human)
-          .then(result => {
-            this.authorizer = result
-            this.$el.querySelector('#verification-code').scrollIntoView(false)
-            this.$el.querySelector('#verification-code').focus()
-          })
+        const result = await firebase.auth()
+        .signInWithPhoneNumber(`+1${this.person.mobile}`, this.human)
+        this.authorizer = result
+        this.$el.querySelector('#verification-code').scrollIntoView(false)
+        this.$el.querySelector('#verification-code').focus()
       },
-      sign_in_with_code(event) {
-        event.preventDefault()
-        sessionStorage.removeItem('posts-synced')
-        sessionStorage.removeItem('profile-synced')
+      async sign_in_with_code(event) {
         this.working = true
         this.disable_input()
         this.show_code = false
-        this.authorizer.confirm(this.code).then(result => {
-          this.working = false
-          this.show_sign_out = true
-          this.save_person()
-        })
+        await this.authorizer.confirm(this.code)
+        this.working = false
+        this.show_sign_out = true
       },
       sign_out(event) {
-        event.preventDefault()
         firebase.auth().signOut()
         this.show_sign_out = false
         this.show_authorize = true
@@ -162,23 +155,18 @@
         this.validate_mobile_number()
       },
       mobile_paste(event) {
-        event.preventDefault()
         const past_text = (event.clipboardData).getData('text/plain')
         const phone_number = parseNumber(past_text, 'US').phone
         if (phone_number) {
           this.person.mobile = phone_number
           this.validate_mobile_number()
-        } else {
-          return false
-        }
+        } else return false
       },
       code_keypress(event) {
         if (!event.key.match(/^\d$/)) event.preventDefault()
-        let button = this.$el.querySelector('#submit-verification')
-        let input = this.$el.querySelector('#verification-code')
-        if (input.value.length === 5) { // after this keypress it will be 6
-          button.disabled = false
-        }
+        const button = this.$el.querySelector('#submit-verification')
+        const input = this.$el.querySelector('#verification-code')
+        if (input.value.length === 5) button.disabled = false
       }
     }
   }
