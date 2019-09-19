cde51b2f11aa74d7e8942f599502ecaf1aa15629
Scott Fryxell <sfryxell@me.com>
2019-09-19 12:57:48 -0700

    Turn full vector worker back on and add support for creating a poster and avatar via a webworker

--- src/vector.worker.js
+++ src/vector.worker.js
@@ -1,58 +1,62 @@
-// Worker.js
-
-self.addEventListener('message', event => {
-  self.postMessage(event.data)
-}, false)
-
-// const potrace = require('javascript-potrace')
-// const Jimp = require('jimp')
-// const EXIF = require('exif-js')
-// onmessage = (message) => {
-//   // console.log(Jimp)
-//   // console.log('message')
-// }
-// importScripts(location.origin + "/manifest.js");
-// importScripts(location.origin + "/worker_bundle.js");
-
-// const options = { threshold: 133, turdSize: 22 }
-// function is_wrong_orientation(image_file) {
-//   EXIF.getData(image_file, () => {
-//     if (image_file.exifdata.Orientation === 6) return true
-//     else return false
-//   })
-// }
-// function prepare(image) {
-//   return image.greyscale().normalize().contrast(0.5).brightness(0.2)
-// }
-// async function read_image(file) {
-//   let reader = new FileReaderSync()
-//   let image = await Jimp.read(reader.readAsArrayBuffer(file))
-//   if (is_wrong_orientation(avatar)) image = image.rotate(-90)
-//   return image
-// }
-// function trace_as_symbol(image, identifier) {
-//   let trace = new potrace.Potrace()
-//   trace.setParameters(options)
-//   trace.loadImage(image, error => {
-//     if (error) return error
-//     return trace.get_as_symbol(identifier)
-//   })
-// }
-// function trace(image) {
-//   let trace = new potrace.Potrace()
-//   trace.setParameters(options)
-//   trace.loadImage(image, error => {
-//     if (error) return error
-//     return trace.get_as_symbol(identifier)
-//   })
-// }
-// export default {
-//   async make_avatar(file, identifier) {
-//     let image = read_image(file)
-//     return trace_as_symbol(prepare(image.resize(333, Jimp.AUTO)), identifier)
-//   },
-//   async make_poster(poster) {
-//     let image = read_image(file)
-//     return trace(prepare(image.resize(512, Jimp.AUTO)))
-//   }
-// }
+import potrace from 'javascript-potrace'
+import Jimp from 'jimp'
+import EXIF from 'exif-js'
+const options = { threshold: 133, turdSize: 22 }
+const convert_to_vector = {
+  make_avatar(file, identifier) {
+    let image = read_image(file)
+    return trace_as_symbol(prepare(image.resize(333, Jimp.AUTO)), identifier)
+  },
+  make_poster(poster) {
+    let image = read_image(file)
+    return trace(prepare(image.resize(512, Jimp.AUTO)))
+  },
+  read_image(file) {
+    const reader = new FileReaderSync()
+    let image = Jimp.read(reader.readAsArrayBuffer(file))
+    if (is_wrong_orientation(file)) image = image.rotate(-90)
+    return image
+  },
+  is_wrong_orientation(image_file) {
+    EXIF.getData(image_file, () => {
+      if (image_file.exifdata.Orientation === 6) return true
+      else return false
+    })
+  },
+  prepare(image) {
+    return image.greyscale().normalize().contrast(0.5).brightness(0.2)
+  },
+  trace_as_symbol(image, identifier) {
+    let trace = new potrace.Potrace()
+    trace.setParameters(options)
+    trace.loadImage(image, error => {
+      if (error) return error
+      return trace.as_symbol(identifier)
+    })
+  },
+  trace(image) {
+    let trace = new potrace.Potrace()
+    trace.setParameters(options)
+    trace.loadImage(image, error => {
+      if (error) return error
+      return trace.as_svg(identifier)
+    })
+  }
+}
+onmessage = (message) => {
+  switch (message.data.cmd) {
+    case 'make_avatar':
+      convert_to_vector
+      .make_avatar(message.data.image_file, message.data.id)
+      .then(vector => {
+        self.postMessage({ vector: vector })
+      })
+    case 'make_poster':
+      convert_to_vector
+      .make_poster(message.data.image_file)
+      .then(vector => {
+        self.postMessage({ vector: vector })
+      })
+  }
+}
+export default convert_to_vector
