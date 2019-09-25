fa57726ca1a180a00e932a6b1beefce635deeed9
Scott Fryxell <sfryxell@me.com>
2019-09-25 11:03:08 -0700

    stubbed component for downloading avatars

---
+++ src/components/download_vector.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <a id="download-avatar" :href="downloadable" download='vector.svg'>
+    <icon name="download"></icon>
+  </a>
+</template>
+<script>
+  export default {
+    computed: {
+      downloadable() {
+        const svg = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+          ${this.person.avatar}
+          <use href="${profile_id.as_avatar_fragment(this.person.id)}"/>
+        </svg>`
+        return `data:application/octet-stream,${encodeURIComponent(svg)}`
+      }
+    }
+  }
+</script>
+<style lang="stylus" >
+</style>
--- src/components/profile/manage-avatar.vue
+++ src/components/profile/manage-avatar.vue
@@ -33,16 +33,6 @@
         avatar_changed: false
       }
     },
-    computed: {
-      // <a id="download-avatar" :href="downloadable" download='vector.svg'><icon name="download"></icon></a>
-      downloadable() {
-        const svg = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-          ${this.person.avatar}
-          <use href="${profile_id.as_avatar_fragment(this.person.id)}"/>
-        </svg>`
-        return `data:application/octet-stream,${encodeURIComponent(svg)}`
-      }
-    },
     created() {
       this.worker.addEventListener('message', this.worker_event)
     },
