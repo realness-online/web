9fbbc07b9637a6f1a01450d3822f359ac4545055
Scott Fryxell <sfryxell@me.com>
2019-11-27 11:36:44 -0800

    can sync from local repo to dev

--- bin/sync_database_from_production.sh
+++ bin/sync_database_from_production.sh
@@ -1,22 +1,27 @@
 #!/usr/bin/env bash
 
-# delete staging repo then copy repo from production
+# reset staging repo with production
 # gsutil -m rm -r gs://realness-staging.appspot.com/people
 # gsutil -m cp -R gs://realness-online.appspot.com/people/ gs://realness-staging.appspot.com/
 
-# delete development repo
+# reset development repo with production
 # gsutil -m rm -r gs://realness-development.appspot.com/people
 # gsutil -m cp -R gs://realness-online.appspot.com/people/ gs://realness-development.appspot.com/
 
-# delete and copy to the desktop too
+
+# reset development with home directory
+gsutil -m rm -r gs://realness-development.appspot.com/people
+gsutil -m cp -R ~/realness.online/ gs://realness-development.appspot.com/
+
+# # reset in home directory
 # rm -rf ~/realness.online
 # mkdir ~/realness.online
 # gsutil -m cp -R gs://realness-online.appspot.com/people/ ~/realness.online/
 
-# just me
-gsutil -m rm -r gs://realness-development.appspot.com/people/+16282281824
-gsutil -m cp -R gs://realness-online.appspot.com/people/+16282281824 gs://realness-development.appspot.com/people
+# reset just me
+# gsutil -m rm -r gs://realness-development.appspot.com/people/+16282281824
+# gsutil -m cp -R gs://realness-online.appspot.com/people/+16282281824 gs://realness-development.appspot.com/people
 
-# just me on staging
+# reset just me on staging
 # gsutil -m rm -r gs://realness-staging.appspot.com/peoplepeople/+16282281824
 # gsutil -m cp -R gs://realness-online.appspot.com/people/+16282281824 gs://realness-staging.appspot.com/people
