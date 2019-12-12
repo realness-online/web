faba321967b4bbe8c3682450b154c620903f4886
Scott Fryxell <sfryxell@me.com>
2019-12-12 14:15:31 -0800

    Improve redability of stored avatars

--- src/components/avatars/as-figure.vue
+++ src/components/avatars/as-figure.vue
@@ -1,13 +1,13 @@
 <template lang="html">
   <figure itemscope itemtype="/avatars" :itemid="avatar.id" >
-    <svg itemprop="path"preserveAspectRatio="xMidYMin slice"
-         :viewBox="avatar.view_box" v-html="avatar.path">
-    </svg>
     <figcaption>
       <link itemprop="created_by" rel="author" :href="avatar.created_by">
       <meta itemprop="created_at" :content="avatar.created_at">
       <meta itemprop="view_box" :content="avatar.view_box">
     </figcaption>
+    <svg itemprop="path"preserveAspectRatio="xMidYMin meet"
+         :viewBox="avatar.view_box" v-html="avatar.path">
+    </svg>
   </figure>
 </template>
 <script>
