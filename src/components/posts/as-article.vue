60358ef297c7263e938b47ba599f88dc10e73a5b
Scott Fryxell <sfryxell@me.com>
2019-12-29 20:37:17 -0800

    Contenteditable does not need colon
    vue has nothing to do here

--- src/components/posts/as-article.vue
+++ src/components/posts/as-article.vue
@@ -3,7 +3,7 @@
     <header>
       <time itemprop="created_at" :datetime="post.created_at">{{as_created_time}}</time>
     </header>
-    <p v-if="me" itemprop="statement" :contenteditable="true" @blur="save">{{as_statement}}</p>
+    <p v-if="me" itemprop="statement" contenteditable="true" @blur="save">{{as_statement}}</p>
     <p v-else itemprop="statement">{{as_statement}}</p>
     <ol>
       <post-as-li v-for="statement in post.statements" :key="statement.id"
