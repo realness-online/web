8618a81d111f5086ee1434eb66a904b214902fda
Scott Fryxell <sfryxell@me.com>
2019-12-29 20:33:44 -0800

    Remove method that should never be generically called

--- src/storage/Large.js
+++ src/storage/Large.js
@@ -4,18 +4,7 @@ import * as firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/storage'
 let Large = (superclass) => class extends superclass {
-  async as_network_list() {
-    const items = []
-    const directory = await this.directory()
-    await directory.items.forEach(async (item) => {
-      const url = await firebase.storage().ref().child(item.fullPath).getDownloadURL()
-      const items_as_text = await (await fetch(url)).text()
-      items.push(Item.get_first_item(items_as_text))
-    })
-    return items
-  }
-  async directory() {
-    const items = []
+    async directory() {
     const user = firebase.auth().currentUser
     const storage = firebase.storage().ref()
     if (user && navigator.onLine) {
