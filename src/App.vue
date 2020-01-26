1e0db03f2ca07bd005362c9dea12c05ecc1ecacb
Scott Fryxell <sfryxell@me.com>
2020-01-25 18:49:02 -0800

    write exceptions and console.info to a network activity folder

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,9 @@
   import developerTools from '@/components/developer-tools'
   import * as firebase from 'firebase/app'
   export default {
+    components: {
+      developerTools
+    },
     watch: {
       '$route' (to, from) {
         sessionStorage.previous = from.path
@@ -23,9 +26,6 @@
         storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
         messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID
       })
-    },
-    components: {
-      developerTools
     }
   }
 </script>
--- src/components/developer-tools.vue
+++ src/components/developer-tools.vue
@@ -1,12 +1,50 @@
+<template lang="html">
+  <ol itemscope itemtype="/activity">
+    <li v-for="[message, created_at] in activity">
+      <span>{{message}}</span>
+      <time>{{created_at}}</time>
+    </li>
+  </ol>
+</template>
 <script>
+  import Vue from 'vue'
+  import { Activity } from '@/persistance/Storage'
   export default {
-    render() {
-      return null
+    data() {
+      return {
+        previous: null,
+        activity_storage: new Activity(),
+        activity: [],
+        info: console.info
+      }
+    },
+    created() {
+      Vue.config.errorHandler = this.on_error
+      console.info = this.info_logger
+    },
+    mounted() {
+      throw new Error("satan is calling")
+    },
+    methods: {
+      async info_logger() {
+        this.info.apply(this, Array.prototype.slice.call(arguments))
+        this.activity.push([arguments[0], new Date().toISOString()])
+        await this.$nextTick()
+        this.activity_storage.save()
+      },
+      async on_error(event) {
+        this.activity.push([event.message, new Date().toISOString()])
+        await this.$nextTick()
+        this.activity_storage.save()
+      }
     }
   }
 </script>
 <style lang="stylus">
-  [outline]
+  [outline],
   [outline] *
     outline: 0.15vmin dashed orange
+  ol[itemtype="/activity"]
+    display:none
+
 </style>
