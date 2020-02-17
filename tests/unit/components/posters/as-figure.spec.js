495f421463c6d2e2f58669eff5c1d638cc2a12a5
Scott Fryxell <sfryxell@me.com>
2020-02-16 20:49:18 -0800

    test rendering a new poster

--- tests/unit/components/posters/as-figure.spec.js
+++ tests/unit/components/posters/as-figure.spec.js
@@ -4,24 +4,31 @@ import Item from '@/modules/item'
 import VueRouter from 'vue-router'
 import as_figure from '@/components/posters/as-figure'
 const fs = require('fs')
-const poster_mock = fs.readFileSync('./tests/unit/html/poster.html', 'utf8')
+const poster_file = fs.readFileSync('./tests/unit/html/poster.html', 'utf8')
+
+const poster = Item.get_items(poster_file)[0]
+const author = {
+  created_at: '2018-07-15T18:11:31.018Z',
+  first_name: 'Scott',
+  last_name: 'Fryxell',
+  id: '/+16282281823'
+}
 describe('@/compontent/posters/as-figure.vue', () => {
-  let person, wrapper
+  let wrapper
   beforeEach(() => {
-    person = {
-      created_at: '2018-07-15T18:11:31.018Z',
-      first_name: 'Scott',
-      last_name: 'Fryxell',
-      id: '/+16282281823'
-    }
     wrapper = shallow(as_figure, {
-      propsData: {
-        author: person,
-        poster: Item.get_items(poster_mock)[0]
-      }
+      propsData: { author, poster }
     })
   })
-  it('Render a poster', () => {
+  it ('Render a poster', () => {
     expect(wrapper.element).toMatchSnapshot()
   })
+
+  it ('Render a new poster', () => {
+    wrapper.setProps({
+      is_new: true
+    })
+    expect(wrapper.element).toMatchSnapshot()
+  })
+
 })
