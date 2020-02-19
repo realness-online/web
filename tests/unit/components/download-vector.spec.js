a58662caf7b2a7fe4f5b9e8c22aa1f78c185436f
Scott Fryxell <sfryxell@me.com>
2020-02-18 17:50:01 -0800

    Tests are aproaching acceptability again

---
+++ tests/unit/components/download-vector.spec.js
@@ -0,0 +1,25 @@
+import { shallow } from 'vue-test-utils'
+import download_vector from '@/components/download-vector'
+import Item from '@/modules/item'
+const fs = require('fs')
+const poster_html = fs.readFileSync('./tests/unit/html/poster.html', 'utf8')
+const poster = Item.get_items(poster_html)[0]
+const person = {
+  id: '/+14151234356',
+  first_name: 'Scott',
+  last_name: 'Fryxell',
+  mobile: '4151234356',
+  avatar: 'avatars/5553338945763'
+}
+
+describe('@/components/download-vector', () => {
+  it('Renders link to download svg', () => {
+    let wrapper = shallow(download_vector, {
+      propsData: {
+        vector: poster,
+        author: person
+      }
+    })
+    expect(wrapper.element).toMatchSnapshot()
+  })
+})
---
+++ tests/unit/components/profile/as-hgroup.spec.js
@@ -0,0 +1,19 @@
+import { shallow } from 'vue-test-utils'
+import as_hgroup from '@/components/profile/as-hgroup'
+describe('@/compontent/profile/as-hgroup.vue', () => {
+  let wrapper
+  const person = {
+    id: '/+14151234356',
+    first_name: 'Scott',
+    last_name: 'Fryxell',
+    mobile: '4151234356',
+    avatar: 'avatars/5553338945763'
+  }
+  beforeEach(() => {
+    wrapper = shallow(as_hgroup, { propsData: { person } })
+  })
+  it('Renders a person as a hgroup element', () => {
+    expect(wrapper.element).toMatchSnapshot()
+  })
+
+})
