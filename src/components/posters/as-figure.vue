41fe53a2190ed9c1a52dbd387e78aab0c1877418
Scott Fryxell <sfryxell@me.com>
2020-03-03 08:41:31 -0800

    Remove and select date switch places
    Awesome use of stylus and css calc to make it all work
    
    grateful these last few days for my tooling decisions.
    
    I can put this code down and now pick it up years later and know where I 
    am
    
    thank you stylus, thank you CSS, Thank you vue

--- src/components/posters/as-figure.vue
+++ src/components/posters/as-figure.vue
@@ -25,16 +25,16 @@
         </menu>
       </fieldset>
       <menu v-if="menu">
+        <a @click="remove_poster">
+          <icon v-if="working" name="working"></icon>
+          <icon v-else name="remove"></icon>
+        </a>
         <a id="create-event" v-if="!is_new">
           <svg viewBox="0 0 150 150" :class="has_event">
             <use :href="date_picker_icon"/>
             <text class="month" x="57" y="24" text-anchor="middle">{{month}}</text>
             <text x="57" y="84" text-anchor="middle">{{day}}</text>
           </svg>
-        </a>
-        <a @click="remove_poster">
-          <icon v-if="working" name="working"></icon>
-          <icon v-else name="remove"></icon>
         </a>
         <a @click="add_poster" v-if="is_new">
           <icon v-if="accept" name="finished"></icon>
@@ -213,6 +213,7 @@
       & > figcaption > input[type="date"]
         width: auto
         height: base-line
+        left: base-line
         @media (min-width: typing-begins)
           top: base-line * 2
           &::-webkit-datetime-edit-fields-wrapper
@@ -257,14 +258,14 @@
               fill: blue
     & > svg
       width: 100%
-      height: 100vh
+      height: 100%
       max-height: page-width
     & > figcaption
       position: relative
       & > input[type="date"]
         position: absolute
         top: base-line
-        left: base-line
+        left: s('calc( 50% - %s)', base-line)
         color: transparent
         z-index: 1
         width: base-line * 2
