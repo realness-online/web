1b8f270f5ae812d707eff09275a42cb79119b88f
Scott Fryxell <sfryxell@me.com>
2020-03-02 21:12:49 -0800

    Min height was weird

--- src/views/Navigation.vue
+++ src/views/Navigation.vue
@@ -158,7 +158,7 @@
       grid-template-columns: 1fr 1fr
       grid-template-rows: repeat(1fr)
       align-items: stretch
-      min-height: page-width
+      min-height: round(base-line * 20)
       max-height: page-width
       height: 100vmin
       width: 100vw
