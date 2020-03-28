548f91b5f7577ac5462e10f5045cd92ff25e7c93
Scott Fryxell <sfryxell@me.com>
2020-03-28 15:17:05 -0700

    Refactored dowload vector to load poster itself
    this is okay as we will locally cash all posters after their first load. 
    which is also okay because vectors are immutable

--- src/components/download-vector.vue
+++ src/components/download-vector.vue
@@ -1,32 +1,45 @@
 <template lang="html">
-  <a :href="downloadable" :download='vector_name()'>
+  <a :href="downloadable" :download="file_name">
     <icon name="download"></icon>
   </a>
 </template>
 <script>
   import date_helper from '@/helpers/date'
   import profile_helper from '@/helpers/profile'
+  import itemid from '@/helpers/itemid'
   import icon from '@/components/icon'
   export default {
     components: { icon },
     props: {
-      vector: {
-        type: Object,
+      itemid: {
+        type: String,
         required: true
       }
     },
+    data () {
+      return {
+        vector: {},
+        file_name: null
+      }
+    },
+    async created () {
+      this.file_name = await this.get_vector_name()
+      this.vector = await itemid.load(this.itemid)
+    },
     computed: {
       downloadable () {
+        console.log(this.vector)
         const svg = `<svg viewBox="${this.vector.view_box}" xmlns="http://www.w3.org/2000/svg">${this.vector.path}</svg>`
         return `data:application/octet-stream,${encodeURIComponent(svg)}`
       }
     },
     methods: {
-      async vector_name () {
-        const info = this.vector.id.split('/')
-        const author_id = `${info[0]}`
-        const type = `${info[1]}`
-        const time = date_helper.as_day_and_time(Number(info[2]))
+      async get_vector_name () {
+        console.log(this.itemid)
+        const info = this.itemid.split('/')
+        const author_id = `/${info[1]}`
+        const type = `${info[2]}`
+        const time = date_helper.as_day_and_time(Number(info[3]))
         const creator = await profile_helper.load(author_id)
         const name = `${creator.first_name}_${creator.last_name}`
         return `${name}_${type}_${time}.svg`
