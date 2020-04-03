6f9d0db6d6be1f64e4085c6a6d984d8e23f77ce2
Scott Fryxell <sfryxell@me.com>
2020-04-02 18:30:36 -0700

    Add some more ways to sync files

--- bin/sync_database_from_production.sh
+++ bin/sync_database_from_production.sh
@@ -5,14 +5,17 @@
 # gsutil -m cp -R gs://realness-online.appspot.com/people/ gs://realness-staging.appspot.com/
 
 # reset development repo with production
-gsutil -m rm -r gs://realness-development.appspot.com/people
-gsutil -m cp -R gs://realness-online.appspot.com/people/ gs://realness-development.appspot.com/
+# gsutil -m rm -r gs://realness-development.appspot.com/people
+# gsutil -m cp -R gs://realness-online.appspot.com/people/ gs://realness-development.appspot.com/
 
 # reset in home directory
 # rm -rf ~/realness.online
 # mkdir ~/realness.online
 # gsutil -m cp -R gs://realness-online.appspot.com/people/ ~/realness.online/
 # gsutil -m cp -R gs://realness-development.appspot.com/people ~/realness.online/
+
+# gsutil -m cp -R ~/realness.online/people gs://realness-development.appspot.com/
+# gsutil -m cp -R ~/realness.online/people gs://realness-online.appspot.com/
 
 # reset just me
 # gsutil -m rm -r gs://realness-development.appspot.com/people/+16282281824
