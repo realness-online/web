66713096ea9b446e03da37516dd996e8e8758490
Scott Fryxell <sfryxell@me.com>
2020-04-05 16:20:27 -0700

    itemid loads from the page then tries the network

--- src/helpers/itemid.js
+++ src/helpers/itemid.js
@@ -3,12 +3,22 @@ import { person_storage as myself } from '@/persistance/Storage'
 import { get_download_url } from '@/persistance/Cloud'
 // import { set } from 'idb-keyval'
 export default {
-  async load (itemid, me = myself.as_object().id) {
+  async load (itemid) {
+    const item = this.load_from_page(itemid)
+    if (item) return item
+    else return this.load_from_network(itemid)
+  },
+  async load_from_network (itemid, me = myself.as_object().id) {
     const url = await get_download_url(itemid)
     if (url) {
       const server_text = await (await fetch(url)).text()
       return Item.get_first_item(server_text)
     } else return null
+  },
+  load_from_page (itemid) {
+    const element = document.getElementById(this.as_query_id(itemid))
+    if (element) return Item.get_first_item(element)
+    else return null
   },
   as_query_id (itemid = '/+') {
     return itemid.substring(2).replace('/', '-').replace('/', '-')
--- tests/unit/helpers/itemid.spec.js
+++ tests/unit/helpers/itemid.spec.js
@@ -1,23 +1,28 @@
 import itemid from '@/helpers/itemid'
 import flushPromises from 'flush-promises'
 const fs = require('fs')
-const poster = fs.readFileSync('./tests/unit/html/poster.html', 'utf8')
+const poster_html = fs.readFileSync('./tests/unit/html/poster.html', 'utf8')
 const fetch = require('jest-fetch-mock')
 describe('@/helpers/itemid', () => {
   describe('#load', () => {
-    it('if it has an id it tries to find on the page ')
-    describe('device owner', () => {
-      it('if it is pageable it gets it from local storage')
-      it('if it is historical it tries to get it from indexdb')
-      it('if it is immutable it tries to get it from indexdb')
+    it('first it tries to find the item on the page', () => {
+      i
+      expect(false).toBe(true)
     })
-    it('if it is someone elses stuff it tries to get it from indexdb')
+    describe('device owner', () => {
+      it.todo('if it is pageable it gets it from local storage')
+      it.todo('if it is historical it tries to get it from indexdb')
+      it.todo('if it is immutable it tries to get it from indexdb')
+    })
+    it.todo('if it is someone elses stuff it tries to get it from indexdb')
     it('if it is nowhere else it gets it from the network', async () => {
-      fetch.mockResponseOnce(poster)
-      const poster = await itemid.load('/+16282281824/posters/55234654346678')
+      const test_id = '/+16282281824/posters/559666932867'
+      const network_request = fetch.mockResponseOnce(poster_html)
+      const poster = await itemid.load(test_id)
       await flushPromises()
       expect(poster.viewbox).toBe('0 0 333 444')
-      expect(poster).toBeCalled()
+      expect(poster.id).toBe(test_id)
+      expect(network_request).toBeCalled()
     })
   })
 })
