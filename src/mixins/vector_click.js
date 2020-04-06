530816dc6a165b00235f38169a7d5dce95cc7319
Scott Fryxell <sfryxell@me.com>
2020-04-06 07:49:34 -0700

    Improve the display of  square and landscape photos by setting preserve aspect ration based on vector width and height

--- src/mixins/vector_click.js
+++ src/mixins/vector_click.js
@@ -1,6 +1,7 @@
 export default {
   data () {
     return {
+      vector: null,
       menu: false
     }
   },
@@ -8,12 +9,24 @@ export default {
     vector_click () {
       this.menu = !this.menu
       this.$emit('vector-click', this.menu)
+    },
+    ratio () {
+      if (!this.vector) return 1.33
+      const numbers = this.vector.viewbox.split(' ')
+      const width = parseInt(numbers[2])
+      const height = parseInt(numbers[3])
+      return (height / width)
     }
   },
   computed: {
     aspect_ratio () {
-      if (this.menu) return 'xMidYMid meet'
-      else return 'xMidYMid slice'
+      if (this.ratio() < 1.1) {
+        if (this.menu) return 'xMidYMid slice'
+        else return 'xMidYMid meet'
+      } else {
+        if (this.menu) return 'xMidYMid meet'
+        else return 'xMidYMid slice'
+      }
     }
   }
 }
