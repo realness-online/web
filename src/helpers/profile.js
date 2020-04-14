471b0f1a82af928af2f829b96d02e8d6e9d7d345
Scott Fryxell <sfryxell@me.com>
2020-04-14 15:46:40 -0700

    DRY up common code between itemid and profile

--- src/helpers/profile.js
+++ src/helpers/profile.js
@@ -1,23 +1,25 @@
+import { as_query_id, as_fragment } from '@/helpers/itemid'
+export function as_avatar_id (id = 'avatar_') {
+  return `avatar_${as_query_id(id)}`
+}
+export function as_avatar_fragment (id = 'avatar_') {
+  return `#${this.as_avatar_id(id)}`
+}
+export function as_phone_number (id = '/+1') {
+  return id.substring(3)
+}
+export function from_phone_number (phone_number) {
+  return `/+1${phone_number}`
+}
+export function from_e64 (e64_number) {
+  return `/${e64_number}`
+}
 export default {
-  as_query_id (id = '/+') {
-    return id.substring(2)
-  },
-  as_avatar_id (id = 'avatar_') {
-    return `avatar_${this.as_query_id(id)}`
-  },
-  as_avatar_fragment (id = 'avatar_') {
-    return `#${this.as_avatar_id(id)}`
-  },
-  as_fragment (id) {
-    return `#${this.as_query_id(id)}`
-  },
-  as_phone_number (id = '/+1') {
-    return id.substring(3)
-  },
-  from_phone_number (phone_number) {
-    return `/+1${phone_number}`
-  },
-  from_e64 (e64_number) {
-    return `/${e64_number}`
-  }
+  as_query_id,
+  as_fragment,
+  as_avatar_id,
+  as_avatar_fragment,
+  as_phone_number,
+  from_phone_number,
+  from_e64
 }
