0412a2be25abf683ef6fa3a5db4cbd3144885c41
Scott Fryxell <sfryxell@me.com>
2020-04-16 18:43:07 -0700

    Shorten name

--- bin/sync_database.sh
+++ bin/sync_database.sh
@@ -14,7 +14,10 @@
 # gsutil -m cp -R gs://realness-online.appspot.com/people/ ~/realness.online/
 # gsutil -m cp -R gs://realness-development.appspot.com/people ~/realness.online/
 
-# gsutil -m cp -R ~/realness.online/people gs://realness-development.appspot.com/
+# reseet development from home directory
+gsutil -m cp -R ~/realness.online/people gs://realness-development.appspot.com/
+
+# reseet production from home directory
 # gsutil -m cp -R ~/realness.online/people gs://realness-online.appspot.com/
 
 # reset just me
