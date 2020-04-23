7acaa21268062f1c5c0513685d9b6593d731febd
Scott Fryxell <sfryxell@me.com>
2020-04-23 08:59:37 -0700

    Paged tests

--- src/persistance/Paged.js
+++ src/persistance/Paged.js
@@ -1,59 +1,85 @@
 // https://developers.caffeina.com/object-composition-patterns-in-javascript-4853898bb9d0
 import Item from '@/modules/item'
+import { load, load_from_network } from '@/helpers/itemid'
 import growth from '@/modules/growth'
 import sorting from '@/modules/sorting'
 import { History } from '@/persistance/Storage'
-const Paged = (superclass) => class extends superclass {
-  async optimize (limit = growth.first()) {
-    if (this.as_kilobytes() > limit) {
-      const current = Item.hydrate(localStorage.getItem(this.selector)).childNodes[0]
-      const offload = document.createDocumentFragment()
-      while (keep_going(current, limit)) {
-        const first_child = current.childNodes[0]
-        offload.appendChild(current.removeChild(first_child))
-      }
-      let div = document.createElement(current.nodeName)
-      div.setAttribute('itemprop', this.type)
-      const history = new History(`${this.type}/${limit}.html`)
-      const existing_history = await history.from_network()
-      if (existing_history) div = existing_history.childNodes[0]
-      div.appendChild(offload)
-      await history.save(div)
-      await this.save(current)
-      await history.optimize(growth.next(limit))
-    }
-  }``
-  async sync_list () {
-    const from_server = Item.get_items(await this.from_network())
-    const local_items = this.as_list()
-    // the larger the number the more recent it is
-    let oldest_date = 0
-    if (from_server.length) oldest_date = Date.parse(from_server[0].created_at)
-    let items
-    if (local_items.length > 0) {
-      const filtered_local = local_items.filter(local_item => {
-        const current_date = Date.parse(local_item.created_at)
-        if (oldest_date > current_date) return false
-        return !from_server.some(server_item => {
-          return local_item.created_at === server_item.created_at
-        })
-      })
-      items = [...filtered_local, ...from_server]
-      items.sort(sorting.older_first)
-    } else {
-      items = from_server
-    }
-    return items
-  }
-}
 function keep_going (current_items, limit) {
   const current_size = current_items.outerHTML.length / 1024
   if (current_size >= growth.previous(limit)) {
     const item = Item.get_first_item(current_items)
     const today = new Date().setHours(0, 0, 0, 0)
     const created_at = Date.parse(item.created_at)
-    if (created_at && created_at < today) return true
+    if (created_at && created_at < today) return true // never trim today's stuff
     else return false
   } else return false
 }
+const Paged = (superclass) => class extends superclass {
+  async optimize (limit = growth.first()) {
+    // First in first out storage (FIFO)
+    if (this.as_kilobytes() > limit) {
+      const current = await load(this.id)
+      const offload = document.createDocumentFragment()
+      let oldest = null
+      while (keep_going(current, limit)) {
+        const last_child = current.childNodes[current.childNodes.index - 1]
+        if (!oldest)
+        offload.insertBefore(current.removeChild(last_child))
+      }
+      let div = document.createElement(current.nodeName)
+      const existing_history = await load_from_network(this.id)
+      if (existing_history) div = existing_history.childNodes[0]
+      div.insertBefore(offload)
+      const history = new History(`${this.type}/${limit}.html`)
+      await history.save(div)
+      await this.save(current)
+      await history.optimize(growth.next(limit))
+    }
+  }
+  async sync_list () {
+    let items, oldest_at = 0 // the larger the number the more recent it is
+    const cloud_items = await load_from_network(this.id, '/+16282281824')
+    const local_items = await load(this.id, '/+16282281824')
+    const length = cloud_items.length
+    if (length) oldest_at = Date.parse(cloud_items[length - 1].created_at)
+    if (local_items.length) {
+      const new_local_stuff = local_items.filter(local_item => {
+        const created_at = Date.parse(local_item.created_at)
+        // local older items are ignored, probably optimized away
+        if (oldest_at > created_at) return false
+        return !cloud_items.some(server_item => {
+          return local_item.created_at === server_item.created_at
+        })
+      })
+      items = [...new_local_stuff, ...cloud_items]
+      items.sort(sorting.newer_first)
+    } else items = cloud_items
+    return items
+  }
+}
 export default Paged
+
+
+// async sync_list () {
+//   const from_server = Item.get_items(await this.from_network())
+//   const local_items = this.as_list()
+//   // the larger the number the more recent it is
+//   let oldest_date = 0
+//   if (from_server.length) oldest_date = Date.parse(from_server[0].created_at)
+//   let items
+//   if (local_items.length > 0) {
+//     const filtered_local = local_items.filter(local_item => {
+//       const current_date = Date.parse(local_item.created_at)
+//       if (oldest_date > current_date) return false
+//       return !from_server.some(server_item => {
+//         return local_item.created_at === server_item.created_at
+//       })
+//     })
+//     items = [...filtered_local, ...from_server]
+//     items.sort(sorting.older_first)
+//   } else {
+//     items = from_server
+//   }
+//   return items
+// }
+// }
--- tests/unit/persistance/Paged.spec.js
+++ tests/unit/persistance/Paged.spec.js
@@ -1,40 +1,76 @@
 import Item from '@/modules/Item'
+import * as itemid from '@/helpers/itemid'
+
 import { Posts } from '@/persistance/Storage'
 const fs = require('fs')
 const posts = fs.readFileSync('./tests/unit/html/posts.html', 'utf8')
 const hella_posts = fs.readFileSync('./tests/unit/html/hella_posts.html', 'utf8')
 const fetch = require('jest-fetch-mock')
 describe ('@/persistance/Paged.js', () => {
-  let posts_storage
+  let paged
   beforeEach(() => {
-    posts_storage = new Posts()
+    paged = new Posts('/+16282281824/posts')
   })
   afterEach(() => {
+    jest.clearAllMocks()
     localStorage.clear()
   })
   describe ('#sync_list', () => {
-    it ('Exists', () => {
-      expect(posts_storage.sync_list).toBeDefined()
+    const hella_as_list = Item.get_items(hella_posts)
+    const posts_as_list = Item.get_items(posts)
+    let cloud_spy, local_spy
+    beforeEach(() => {
+      cloud_spy = jest.spyOn(itemid, 'load_from_network')
+                      .mockImplementation(() => posts_as_list)
+      local_spy = jest.spyOn(itemid, 'load')
+                      .mockImplementation(() => hella_as_list)
     })
+    it ('Exists', () => {
+      expect(paged.sync_list).toBeDefined()
+    })
+
     it ('Syncs posts from server to local storage', async () => {
-      localStorage.setItem(posts_storage.selector, hella_posts)
-      fetch.mockResponseOnce(posts)
-      const server_list = Item.get_items(posts)
-      expect(server_list.length).toBe(9)
-      expect(posts_storage.as_list().length).toBe(79)
-      const list = await posts_storage.sync_list()
-      // 3 posts are the same 25 posts are older then will sync
-      // 9 + 79 - 3 = 60
+      expect(posts_as_list.length).toBe(9)
+      expect(hella_as_list.length).toBe(79)
+      const list = await paged.sync_list()
+      expect(cloud_spy).toBeCalled()
+      expect(local_spy).toBeCalled()
+      // 79 local posts - 25 were optimized away on another device
+      // 9 posts from the cloud 3 of which are shared between local and cloud
+      // 79 - 25 = 54
+      // 9 - 3 = 6
+      // 54 + 6 = 60
+      // 54 unsynced posts locally 6 unsynced posts in the cloud
       expect(list.length).toBe(60)
+    })
+    it('syncs if there are no server items', async () => {
+      cloud_spy = jest.spyOn(itemid, 'load_from_network')
+                      .mockImplementationOnce(() => [])
+      expect(posts_as_list.length).toBe(9)
+      expect(hella_as_list.length).toBe(79)
+      const list = await paged.sync_list()
+      expect(cloud_spy).toBeCalled()
+      expect(local_spy).toBeCalled()
+      expect(list.length).toBe(79)
+    })
+    it('syncs if there are no local items', async () => {
+      cloud_spy = jest.spyOn(itemid, 'load')
+                      .mockImplementationOnce(() => [])
+      expect(posts_as_list.length).toBe(9)
+      expect(hella_as_list.length).toBe(79)
+      const list = await paged.sync_list()
+      expect(cloud_spy).toBeCalled()
+      expect(local_spy).toBeCalled()
+      expect(list.length).toBe(9)
     })
   })
   describe ('#optimize', () => {
     it ('Exists', () => {
-      expect(posts_storage.optimize).toBeDefined()
+      expect(paged.optimize).toBeDefined()
     })
-    it.skip('It optimizes a list of items accross a set of pages', async () => {
-      localStorage.setItem(posts_storage.filename, hella_posts)
-      await posts_storage.optimize()
+    it ('It optimizes a list of items accross a set of pages', async () => {
+      localStorage.setItem(paged.id, hella_posts)
+      await paged.optimize()
       console.log(Object.keys(localStorage.__STORE__))
       expect(Object.keys(localStorage.__STORE__).length).toBe(3)
     })
