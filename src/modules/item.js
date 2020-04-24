e0b47ee3659a72547219e19163a8e250f8c7d806
Scott Fryxell <sfryxell@me.com>
2020-04-24 09:00:40 -0700

    Refactor for best practices

--- src/modules/item.js
+++ src/modules/item.js
@@ -1,85 +1,84 @@
 import { as_type } from '@/helpers/itemid'
-export default {
-  hydrate (item_as_string) {
-    if (item_as_string) {
-      return document.createRange().createContextualFragment(item_as_string)
-    } else return null
-  },
-  get_items (elements) {
-    if (!elements) return []
-    if (typeof elements === 'string') elements = this.hydrate(elements)
-    const items_as_data = []
-    let query = '[itemscope]'
-    const items = Array.from(elements.querySelectorAll(query))
-    items.forEach(item => {
-      const id = item.getAttribute('itemid')
-      let type = item.getAttribute('itemtype')
-      const meta = {}
-      if (id && !type) type = as_type(id)
-      if (id) meta.id = id
-      if (type) meta.type = type
-      const properties = this.get_item_properties(item)
-      items_as_data.push({ ...meta, ...properties })
-    })
-    return items_as_data
-  },
-  get_first_item (elements, type) {
-    const item = this.get_items(elements, type)[0]
-    return item || {}
-  },
-  get_item_properties (item) {
-    const props = {}
-    const properties = Array.from(item.querySelectorAll('[itemprop]'))
-    properties.forEach(prop => {
-      const name = prop.getAttribute('itemprop')
-      if (props[name]) return // first set value wins
-      const value = this.property_value(prop)
-      props[name] = value
-    })
-    switch (item.tagName.toLowerCase()) {
-      case 'svg':
-      case 'symbol':
-        props.viewbox = item.getAttribute('viewBox')
-    }
-    return props
-  },
-  property_value (element) {
-    if (element.getAttribute('content')) {
-      return element.getAttribute('content')
-    }
-    switch (element.tagName.toLowerCase()) {
-      case 'a':
-      case 'area':
-      case 'link':
-      case 'use':
-        return element.getAttribute('href')
-      case 'audio':
-      case 'iframe':
-      case 'source':
-      case 'track':
-      case 'video':
-      case 'img':
-      case 'embed':
-        return element.getAttribute('src')
-      case 'data':
-      case 'meter':
-      case 'input':
-      case 'textarea':
-      case 'select':
-        return element.value
-      case 'time':
-        return element.getAttribute('datetime')
-      case 'svg':
-      case 'path':
-      case 'symbol':
-        return element.outerHTML
-      case 'g':
-      case 'defs':
-        return element.innerHTML
-      case 'object':
-        return element.getAttribute('data')
-      default:
-        return element.textContent.trim()
-    }
+export function hydrate (item_as_string) {
+  if (item_as_string) {
+    return document.createRange().createContextualFragment(item_as_string)
+  } else return null
+}
+export function get_items (elements) {
+  if (!elements) return []
+  if (typeof elements === 'string') elements = hydrate(elements)
+  const items_as_data = []
+  let query = '[itemscope]'
+  const items = Array.from(elements.querySelectorAll(query))
+  items.forEach(item => {
+    const id = item.getAttribute('itemid')
+    let type = item.getAttribute('itemtype')
+    const meta = {}
+    if (id && !type) type = as_type(id)
+    if (id) meta.id = id
+    if (type) meta.type = type
+    const properties = get_item_properties(item)
+    items_as_data.push({ ...meta, ...properties })
+  })
+  return items_as_data
+}
+export function get_first_item (elements, type) {
+  const item = get_items(elements, type)[0]
+  return item || {}
+}
+export function get_item_properties (item) {
+  const props = {}
+  const properties = Array.from(item.querySelectorAll('[itemprop]'))
+  properties.forEach(prop => {
+    const name = prop.getAttribute('itemprop')
+    if (props[name]) return // first set value wins
+    const value = property_value(prop)
+    props[name] = value
+  })
+  switch (item.tagName.toLowerCase()) {
+    case 'svg':
+    case 'symbol':
+      props.viewbox = item.getAttribute('viewBox')
+  }
+  return props
+}
+export function property_value (element) {
+  if (element.getAttribute('content')) {
+    return element.getAttribute('content')
+  }
+  switch (element.tagName.toLowerCase()) {
+    case 'a':
+    case 'area':
+    case 'link':
+    case 'use':
+      return element.getAttribute('href')
+    case 'audio':
+    case 'iframe':
+    case 'source':
+    case 'track':
+    case 'video':
+    case 'img':
+    case 'embed':
+      return element.getAttribute('src')
+    case 'data':
+    case 'meter':
+    case 'input':
+    case 'textarea':
+    case 'select':
+      return element.value
+    case 'time':
+      return element.getAttribute('datetime')
+    case 'svg':
+    case 'path':
+    case 'symbol':
+      return element.outerHTML
+    case 'g':
+    case 'defs':
+      return element.innerHTML
+    case 'object':
+      return element.getAttribute('data')
+    default:
+      return element.textContent.trim()
   }
 }
+export default {get_items, get_first_item}
