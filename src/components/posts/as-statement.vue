5b73f3a85d43b1ba0d723a9fd7e86a4e0c7a7b2f
Scott Fryxell <sfryxell@me.com>
2020-05-01 18:38:32 -0700

    renameing posts to statements

--- src/components/posts/as-statement.vue
+++ src/components/posts/as-statement.vue
@@ -1,13 +1,18 @@
 <template lang="html">
   <div itemscope :itemid="statement.id">
-    <p itemprop="what">{{statement.what}}</p>
-    <meta itemprop="why" :content="statement.why">
-    <meta itemprop="where" :content="statement.where">
+    <p itemprop="statement">{{statement.statement}}</p>
+    <meta itemprop="why" :content="statement.why" v-if="statement.why">
+    <meta itemprop="where" :content="statement.where" v-if="statement.where">
   </div>
 </template>
 <script>
   export default {
-
+    props: {
+      statement: {
+        type: Object,
+        required: true
+      }
+    }
   }
 </script>
 <style lang="stylus">
