8f4eaf0ae2e89cab4e963b3bafbc6b836adfc540
Scott Fryxell <sfryxell@me.com>
2020-05-02 15:16:37 -0700

    this.me will work in tests

--- tests/unit/setup.js
+++ tests/unit/setup.js
@@ -3,6 +3,13 @@ Vue.config.productionTip = false
 Vue.config.devtools = false
 require('fake-indexeddb/auto')
 require('jest-fetch-mock').enableMocks()
+Vue.mixin({
+  data() {
+    return {
+      me: localStorage.getItem('me')
+    }
+  }
+})
 console.info = function () {
   // do nothing therby gounding info into the dirt
 }
