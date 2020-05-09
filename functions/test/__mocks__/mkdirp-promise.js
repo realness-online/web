49e4363a4dd0d33c2bb203662adbddacce38da57
Scott Fryxell <sfryxell@me.com>
2020-05-09 14:15:46 -0700

    Functions deploy

--- functions/test/__mocks__/mkdirp-promise.js
+++
@@ -1,3 +0,0 @@
-module.exports = options => {
-  return Promise.resolve(options)
-}
--- functions/test/__mocks__/potrace.js
+++
@@ -1,6 +0,0 @@
-const trace_mock =  jest.fn((local_image, callback) => {
-  // console.log('child-process-promise.spawn', command, options)
-  callback(null,)
-})
-exports.trace = trace_mock
-exports.trace_mock = trace_mock
