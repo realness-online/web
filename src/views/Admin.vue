17f61fb92de21759ba9545d45fe02e5dcec9734f
Scott Fryxell <sfryxell@me.com>
2020-05-10 08:53:11 -0700

    Get all the users in the people directory

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -8,15 +8,29 @@
   </section>
 </template>
 <script>
+  import * as firebase from 'firebase/app'
+  import 'firebase/storage'
   import logo_as_link from '@/components/logo-as-link'
   import icon from '@/components/icon'
+  import itemid from '@/helpers/itemid'
+  import profile from '@/helpers/profile'
   export default {
     components: {
       'logo-as-link': logo_as_link,
       icon
     },
-    created () {
-      console.log('I am running')
+    data () {
+      return {
+        phonebook: []
+      }
+    },
+    async created () {
+      console.info('Views Admin')
+      const phone_numbers = await firebase.storage().ref().child('/people/').listAll()
+      phone_numbers.prefixes.forEach(async (phone_number) => {
+        const person = await itemid.load(profile.from_e64(phone_number.name))
+        if (person) this.phonebook.push(person)
+      })
     }
   }
 </script>
