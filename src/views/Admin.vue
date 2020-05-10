86e641c71380e52724cddb6e6ac3014227d7d0e5
Scott Fryxell <sfryxell@me.com>
2020-05-10 08:42:34 -0700

    Add navigation

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,12 +1,20 @@
 <template lang="html">
-  <section class="page">
+  <section id="admin" class="page">
     <header>
-      <h1>Migrations</h1>
+      <icon name="nothing"></icon>
+      <h1>Admin</h1>
+      <logo-as-link></logo-as-link>
     </header>
   </section>
 </template>
 <script>
+  import logo_as_link from '@/components/logo-as-link'
+  import icon from '@/components/icon'
   export default {
+    components: {
+      'logo-as-link': logo_as_link,
+      icon
+    },
     created () {
       console.log('I am running')
     }
