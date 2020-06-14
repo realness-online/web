c8cba69e4ee3bba65d724e2e2e2afcc8b86bc19c
Scott Fryxell <sfryxell@me.com>
2020-06-14 16:49:55 -0700

    Correct naming

--- tests/unit/views/Admin.spec.js
+++ tests/unit/views/Admin.spec.js
@@ -1,6 +1,6 @@
 import { shallow } from 'vue-test-utils'
 import Admin from '@/views/Admin'
-describe('@/views/Events.vue', () => {
+describe('@/views/Admin.vue', () => {
   it('Renders list of upcoming events', () => {
     const wrapper = shallow(Admin)
     expect(wrapper.element).toMatchSnapshot()
