ecff943b06b693c058fa59dbe0fc84f832759ce5
Scott Fryxell <sfryxell@me.com>
2020-06-14 17:02:12 -0700

    Button for sending users to the sign on page

--- src/components/sign-on.vue
+++ src/components/sign-on.vue
@@ -1,16 +1,14 @@
 <template lang="html">
   <button class="sign-on" autofocus @click="sign_on">Sign on</button>
 </template>
-
 <script>
   export default {
     methods: {
-      sign_on () {
-
+      sign_on (event) {
+        this.$router.push({ path: '/sign-on' })
       }
     }
   }
 </script>
-
 <style lang="stylus">
 </style>
--- tests/unit/components/sign-on.spec.js
+++ tests/unit/components/sign-on.spec.js
@@ -1,11 +1,20 @@
-import { shallow } from 'vue-test-utils'
+import { shallow, createLocalVue } from 'vue-test-utils'
+import VueRouter from 'vue-router'
 import sign_on from '@/components/sign-on'
 describe('@/components/sign-on', () => {
-  let wrapper
-  beforeEach(() => {
-    wrapper = shallow(sign_on)
-  })
   it('Renders sign on button', () => {
+    const wrapper = shallow(sign_on)
     expect(wrapper.element).toMatchSnapshot()
+  })
+  it('Takes you to the sign-on page when clicked', () => {
+    const localVue = createLocalVue()
+    localVue.use(VueRouter)
+    const router = new VueRouter()
+    const wrapper = shallow(sign_on, {
+      localVue,
+      router
+    })
+    wrapper.vm.sign_on()
+    expect(wrapper.vm.$route.path).toBe('/sign-on')
   })
 })
