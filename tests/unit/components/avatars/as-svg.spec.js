93a2d21f9673df308120fcb1c8f89643113c0b4e
Scott Fryxell <sfryxell@me.com>
2020-06-22 22:44:59 -0700

    Test as-svg methods

--- tests/unit/components/avatars/as-svg.spec.js
+++ tests/unit/components/avatars/as-svg.spec.js
@@ -3,7 +3,8 @@ import as_svg from '@/components/avatars/as-svg'
 const person = {
   first_name: 'Scott',
   last_name: 'Fryxell',
-  id: '/+14151234356'
+  id: '/+14151234356',
+  avatar: '/+14151234356/avatars/559888845532'
 }
 describe('@/components/avatars/as-svg.vue', () => {
   let wrapper
@@ -14,5 +15,24 @@ describe('@/components/avatars/as-svg.vue', () => {
   })
   it('Render an avatar', () => {
     expect(wrapper.element).toMatchSnapshot()
+  })
+  describe('methods', () => {
+    describe('#first_instance', () => {
+      it('exists', () => {
+        expect(wrapper.vm.first_instance).toBeDefined()
+      })
+      it('checks if item is already on page', () => {
+        expect(wrapper.vm.first_instance()).toBe(true)
+      })
+    })
+    describe('#show', () => {
+      it('exists', () => {
+        expect(wrapper.vm.show).toBeDefined()
+      })
+      it('checks if item is already on page', async () => {
+        await wrapper.vm.show()
+        expect(wrapper.emitted('vector-loaded')).toBeTruthy()
+      })
+    })
   })
 })
