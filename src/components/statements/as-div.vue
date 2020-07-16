55ceb08d6420d87b185d0e0ba5afd7caa4787db2
Scott Fryxell <sfryxell@me.com>
2020-07-16 14:00:24 -0700

    Go ahead edit your statements all you like

--- src/components/statements/as-div.vue
+++ src/components/statements/as-div.vue
@@ -1,12 +1,13 @@
 <template lang="html">
   <div itemscope :itemid="statement.id">
-    <p v-if="editable" contenteditable="true" itemprop="statement">{{ statement.statement }}</p>
+    <p v-if="editable" ref="editable" contenteditable="true" itemprop="statement" @blur.prevent="save">{{ statement.statement }}</p>
     <p v-else itemprop="statement">{{ statement.statement }}</p>
     <meta v-if="statement.why" itemprop="why" :content="statement.why">
     <meta v-if="statement.where" itemprop="where" :content="statement.where">
   </div>
 </template>
 <script>
+  import { Statements } from '@/persistance/Storage'
   export default {
     props: {
       statement: {
@@ -17,6 +18,16 @@
         type: Boolean,
         required: false,
         default: false
+      }
+    },
+    methods: {
+      save (event) {
+        const possibly_changed = this.$refs.editable.textContent.trim()
+        if (this.statement.statement !== possibly_changed) {
+          console.log('changed')
+          const statements = new Statements()
+          statements.save()
+        }
       }
     }
   }
