91739ee0f0bfbdcb06f6425c2c449cec0d46fbfd
Scott Fryxell <sfryxell@me.com>
2020-07-23 14:41:34 -0700

    Remove comment

--- src/persistance/Large.js
+++ src/persistance/Large.js
@@ -3,7 +3,6 @@ import { set, get } from 'idb-keyval'
 import { as_directory_id, as_path_parts } from '@/helpers/itemid'
 const Large = (superclass) => class extends superclass {
   async save (items = document.querySelector(`[itemid="${this.id}"]`)) {
-    console.log(this.id)
     const path = as_directory_id(this.id)
     let directory = await get(path)
     if (directory && directory.items) directory.items.push(as_path_parts(this.id)[2])
