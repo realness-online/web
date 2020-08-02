85bb5ed53586e1e20fa6bde2426bea19d2b3a711
Scott Fryxell <sfryxell@me.com>
2020-08-02 08:24:50 -0700

    remove debounce and shorten the delay before blurr event is triggered

--- src/components/statements/as-article.vue
+++ src/components/statements/as-article.vue
@@ -25,7 +25,6 @@
 <script>
   import { load, as_author, as_created_at } from '@/helpers/itemid'
   import date_helper from '@/helpers/date'
-  // import debounce from '@/modules/debounce'
   import intersection from '@/mixins/intersection'
   import as_statement from '@/components/statements/as-div'
   import profile_as_avatar from '@/components/avatars/as-svg'
@@ -83,7 +82,7 @@
           if (!this.focused) {
             this.$emit('blurred', statement)
           }
-        }, 1000)
+        }, 750)
       }
     }
   }
