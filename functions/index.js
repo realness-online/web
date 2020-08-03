f643b7e68d32d01e0ae536c1f82ec3e39d7943cb
Scott Fryxell <sfryxell@me.com>
2020-08-03 12:11:23 -0700

    Linting

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 admin.initializeApp()
 exports.create_admin = functions.storage.object().onFinalize(file => {
-  let claims = {}
-  claims['admin'] = true
+  const claims = {}
+  claims.admin = true
   return admin.auth().setCustomUserClaims('mtHn4Rf4oGbF7qYOiooqBBTgCTt1', claims)
 })
