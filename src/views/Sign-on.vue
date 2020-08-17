d47e2b72d1f76dec95c4b7bfcb6ef6ab5bb1c695
Scott Fryxell <sfryxell@me.com>
2020-08-17 11:15:39 -0700

    Can clean yourself from machine entierly

--- src/views/Sign-on.vue
+++ src/views/Sign-on.vue
@@ -7,9 +7,14 @@
     <profile-as-form :person="person"
                      @modified="save_me"
                      @signed-on="signed_on" />
+    <footer v-if="cleanable">
+      <button @click="clean">Clean</button>
+    </footer>
   </section>
 </template>
 <script>
+  import { keys, clear } from 'idb-keyval'
+  import { load } from '@/helpers/itemid'
   import logo_as_link from '@/components/logo-as-link'
   import profile_as_figure from '@/components/profile/as-figure'
   import profile_as_form from '@/components/profile/as-form'
@@ -22,10 +27,26 @@
     },
     data () {
       return {
+        index_db_keys: {},
         person: {
-          id: '/+'
+          id: '/+',
+          mobile: null
         }
       }
+    },
+    computed: {
+      cleanable () {
+        if (this.me.length > 2) return true
+        if (localStorage.length > 2) return true
+        if (this.index_db_keys.length > 0) return true
+        else return false
+      }
+    },
+    async created () {
+      this.index_db_keys = await keys()
+      const person = await load(this.me)
+      console.log(person, this.me, this.index_db_keys.length)
+      if (person) this.person = person
     },
     methods: {
       async signed_on (event) {
@@ -38,11 +59,15 @@
         const me = new Me()
         await this.$nextTick()
         await me.save()
+      },
+      async clean () {
+        localStorage.clear()
+        await clear()
+        location.reload()
       }
     }
   }
 </script>
-
 <style lang="stylus">
   section#sign-on.page
     hgroup
@@ -50,5 +75,9 @@
     svg.background
       fill: red
     form
+    footer
       padding: base-line
+    @media (min-width: pad-begins)
+      form
+        margin: auto
 </style>
