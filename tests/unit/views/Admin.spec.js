a4489456d0929b172a6a296e98b9485ccb9a5f91
Scott Fryxell <sfryxell@me.com>
2020-08-17 19:55:45 -0700

    Updated snapshots

--- tests/unit/views/Admin.spec.js
+++
@@ -1,8 +0,0 @@
-import { shallow } from 'vue-test-utils'
-import Admin from '@/views/Admin'
-describe('@/views/Admin.vue', () => {
-  it('Renders list of upcoming events', () => {
-    const wrapper = shallow(Admin)
-    expect(wrapper.element).toMatchSnapshot()
-  })
-})
