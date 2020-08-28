4b9fd38e408a48640553f69edb093e6a6f8acd63
Scott Fryxell <sfryxell@me.com>
2020-08-28 07:49:31 -0700

    Add an entry to localstorage if the app is offline, or the user has never signed in
    will use these entries when the user does sign in or comes back online 
    to sync with the network

--- src/persistance/Large.js
+++ src/persistance/Large.js
@@ -1,4 +1,6 @@
 // https://developers.caffeina.com/object-composition-patterns-in-javascript-4853898bb9d0
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
 import { set, get } from 'idb-keyval'
 import { as_directory_id, as_path_parts } from '@/helpers/itemid'
 const Large = (superclass) => class extends superclass {
@@ -10,6 +12,10 @@ const Large = (superclass) => class extends superclass {
     if (items) {
       set(this.id, items.outerHTML)
       set(path, directory)
+    }
+    // set items to be synced when back online of after sign-in
+    if (!navigator.online || !firebase.auth().currentUser) {
+      localStorage.setItem(this.id, null)
     }
     if (super.save) super.save(items)
   }
