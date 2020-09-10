1f218f0995c0924d83fd629fec74bf250e8f4fa9
Scott Fryxell <sfryxell@me.com>
2020-09-10 16:31:26 -0700

    New todo's for sign on

--- tests/unit/views/Sign-on.spec.js
+++ tests/unit/views/Sign-on.spec.js
@@ -6,4 +6,7 @@ describe('@/views/Sign-on.vue', () => {
     const wrapper = shallow(Sign_on)
     expect(wrapper.element).toMatchSnapshot()
   })
+  it.todo('Returns to the page user was on when they decided to join')
+  it.todo('Asks new users for their name')
+  it.todo('Takes a new user to the account page')
 })
