83f879d479bf231c049f4f6c9927ca4da0be1e49
Scott Fryxell <sfryxell@me.com>
2020-10-10 17:50:15 -0700

    Moved tests over to te workers repo.
    This involves some setup over on the workers side but it's worth it to 
    make explicit that workers and realness online live in different 
    sandboxes

--- tests/unit/components/sync.spec.js
+++ tests/unit/components/sync.spec.js
@@ -170,26 +170,5 @@ describe('Syncing Edge data', () => {
       })
     })
   })
-  describe('/public/sync.js', () => {
-    it.todo('Syncs posters created while signed in but offline')
-    it.todo('Syncs directories')
-    describe('Syncronzing IndexDB:', () => {
-      // The application loads the data
-      // the syncronizer deletes what's stale
-      it.todo('Keeps track of how old items are')
-      it.todo('Checks if a resourse has been updated')
-      it.todo('Removes any items that are no longer in sync with what\'s current')
-    })
-    describe('Frequency:', () => {
-      it.todo('Checks every 5 minutes for updates from users who have posted recently')
-      it.todo('Check once per session for people who haven\'t posted in a while')
-      it.todo('will rely on peer to peer for updates sooner than five minutes')
-    })
-    describe('Peer syncing:', () => {
-      it.todo('contacts a signaling server to determine available peers')
-      it.todo('Connects to available peer')
-      it.todo('share indexes with peer')
-      it.todo('shares updates with peers')
-    })
-  })
+
 })
