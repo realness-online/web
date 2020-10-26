dd6e1caac31540557e6920ad20be5256cafb137a
Scott Fryxell <sfryxell@me.com>
2020-10-25 18:44:15 -0700

    Better testing sessionstorage.previous

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -1,10 +1,14 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import VueRouter from 'vue-router'
+
 import * as firebase from 'firebase/app'
 import App from '@/App'
 describe('@/App.vue', () => {
   let wrapper
   beforeEach(() => {
-    wrapper = shallowMount(App)
+    wrapper = shallowMount(App, {
+      stubs: ['router-link', 'router-view']
+    })
   })
   afterEach(() => {
     firebase.initializeApp.mockClear()
@@ -12,16 +16,18 @@ describe('@/App.vue', () => {
   it('Renders layout of the application', () => {
     expect(wrapper.element).toMatchSnapshot()
   })
-  it('Sets previously visited page in sessionStorage', () => {
-    const $route = {
-      path: '/relations'
-    }
-    wrapper = shallowMount(App, {
-      mocks: {
-        $route
-      }
+  it('Sets previously visited page in sessionStorage', async () => {
+    const localVue = createLocalVue()
+    localVue.use(VueRouter)
+    const router = new VueRouter({
+      routes: [
+        { path: '/relations', name: 'relations' },
+        { path: '/magic', name: 'magic' }
+      ]
     })
-    wrapper.setData({ $route: { path: '/magic' } })
+    wrapper = shallowMount(App, { localVue, router })
+    await router.push({ name: 'relations' })
+    await router.push({ name: 'magic' })
     expect(sessionStorage.previous).toBe('/relations')
   })
   it('Initialises firebase', () => {
