cdf7675ba69cdcd58a457ec3a6bc06df9c20756e
Scott Fryxell <sfryxell@me.com>
2020-11-01 12:27:57 -0800

    properly configured RecaptchaVerifier

--- tests/__mocks__/firebase/auth.js
+++ tests/__mocks__/firebase/auth.js
@@ -1,15 +1,16 @@
 import * as firebase from 'firebase/app'
+const RecaptchaVerifier = class {
+  verify () {
+    // console.log('RecaptchaVerifier.verify');
+  }
+}
 firebase.auth_mock = {
   get currentUser () {
     return firebase.user
   },
   onAuthStateChanged: jest.fn(state => state(firebase.user)),
-  signInWithPhoneNumber: jest.fn(() => Promise.resolve('success')),
-  RecaptchaVerifier: class {
-    verify () {
-      // console.log('RecaptchaVerifier.verify');
-    }
-  }
+  signInWithPhoneNumber: jest.fn(() => Promise.resolve('success'))
 }
 firebase.auth = jest.fn(() => firebase.auth_mock)
+firebase.auth.RecaptchaVerifier = RecaptchaVerifier
 firebase.user = null
