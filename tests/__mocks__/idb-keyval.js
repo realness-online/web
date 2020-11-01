983d37ef3b5641d4bcc23a9c578be7e46daa1a83
Scott Fryxell <sfryxell@me.com>
2020-11-01 11:40:03 -0800

    Stick with this old commit for just a second

--- tests/__mocks__/idb-keyval.js
+++ tests/__mocks__/idb-keyval.js
@@ -1,11 +1,28 @@
+// const get_mock = jest.fn(itemid => {
+//   return Promise.resolve({})
+// })
+// const set_mock = jest.fn((key, value) => {
+//   return Promise.resolve({})
+// })
+// const delete_mock = jest.fn((key) => {
+//   return Promise.resolve({})
+// })
+// const keys_mock = jest.fn(() => {
+//   return Promise.resolve([])
+// })
+//
+// module.exports.set = set_mock
+// module.exports.del = delete_mock
+// module.exports.get = get_mock
+// module.exports.keys = keys_mock
 const get_mock = jest.fn(itemid => {
-  return Promise.resolve({})
+  return 'some stufffff!!!!!!!!!!!!!!!'
 })
 const set_mock = jest.fn((key, value) => {
-  return Promise.resolve({})
+  return true
 })
 const delete_mock = jest.fn((key) => {
-  return Promise.resolve({})
+  return key
 })
 const keys_mock = jest.fn(() => {
   return Promise.resolve([])
