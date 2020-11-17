a7df56fc54065c9374e150ef268cca6568e4f69f
Scott Fryxell <sfryxell@me.com>
2020-11-17 14:08:35 -0800

    Add options to load sooner and throttle event triggering

--- src/mixins/intersection.js
+++ src/mixins/intersection.js
@@ -12,7 +12,10 @@ export default {
   methods: {
     intersect () {
       if (this.observer) this.observer.unobserve(this.$el)
-      this.observer = new IntersectionObserver(this.check_intersection)
+      this.observer = new IntersectionObserver(this.check_intersection, {
+        rootMargin: '20px',
+        delay: 50
+      })
       this.observer.observe(this.$el)
     },
     check_intersection (entries) {
