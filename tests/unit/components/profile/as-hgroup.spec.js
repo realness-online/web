ae31d4dd33f19b2e580bdee689501ea8d0386cbc
Scott Fryxell <sfryxell@me.com>
2020-12-05 10:48:02 -0800

    Refactor to pop events rather then modify props

--- tests/unit/components/profile/as-hgroup.spec.js
+++ tests/unit/components/profile/as-hgroup.spec.js
@@ -1,18 +1,58 @@
 import { shallowMount } from '@vue/test-utils'
 import as_hgroup from '@/components/profile/as-hgroup'
+const person = {
+  id: '/+14151234356',
+  first_name: 'Scott',
+  last_name: 'Fryxell',
+  mobile: '4151234356',
+  avatar: 'avatars/5553338945763'
+}
 describe('@/compontent/profile/as-hgroup.vue', () => {
   let wrapper
-  const person = {
-    id: '/+14151234356',
-    first_name: 'Scott',
-    last_name: 'Fryxell',
-    mobile: '4151234356',
-    avatar: 'avatars/5553338945763'
-  }
-  beforeEach(() => {
-    wrapper = shallowMount(as_hgroup, { propsData: { person } })
-  })
   it('Renders a person as a hgroup element', () => {
+    wrapper = shallowMount(as_hgroup, { propsData: { person } })
     expect(wrapper.element).toMatchSnapshot()
+  })
+  describe('methods:', () => {
+    beforeEach(() => {
+      wrapper = shallowMount(as_hgroup, {
+        propsData: {
+          person,
+          editable: true
+        }
+      })
+    })
+    describe('#save_first_name', () => {
+      it('Emits an event when first_name changes', async () => {
+        expect(wrapper.vm.person.first_name).toBe('Scott')
+        wrapper.vm.$refs.first_name.textContent = 'John'
+        await wrapper.vm.$nextTick()
+        wrapper.vm.save_first_name()
+        expect(wrapper.emitted('update:person')).toBeTruthy()
+      })
+      it('Stays quiet when first_name is consistent', async () => {
+        expect(wrapper.vm.person.first_name).toBe('Scott')
+        wrapper.vm.$refs.first_name.textContent = 'Scott'
+        await wrapper.vm.$nextTick()
+        wrapper.vm.save_first_name()
+        expect(wrapper.emitted('update:person')).toBeFalsy()
+      })
+    })
+    describe('#save_last_name', () => {
+      it('Emits an event when last_name changes', async () => {
+        expect(wrapper.vm.person.last_name).toBe('Fryxell')
+        wrapper.vm.$refs.last_name.textContent = 'Johannes'
+        await wrapper.vm.$nextTick()
+        wrapper.vm.save_last_name()
+        expect(wrapper.emitted('update:person')).toBeTruthy()
+      })
+      it('Stays quiet when last_name is consistent', async () => {
+        expect(wrapper.vm.person.last_name).toBe('Fryxell')
+        wrapper.vm.$refs.last_name.textContent = 'Fryxell'
+        await wrapper.vm.$nextTick()
+        wrapper.vm.save_last_name()
+        expect(wrapper.emitted('update:person')).toBeFalsy()
+      })
+    })
   })
 })
