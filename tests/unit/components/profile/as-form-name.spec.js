2380b62590311ae8e1bfc5be12bfe96fb1af2a35
Scott Fryxell <sfryxell@me.com>
2020-12-06 17:02:17 -0800

    Refactored to anounce changes to properties rather than modify them directly

--- tests/unit/components/profile/as-form-name.spec.js
+++ tests/unit/components/profile/as-form-name.spec.js
@@ -1,18 +1,72 @@
 import { shallowMount } from '@vue/test-utils'
 import as_form from '@/components/profile/as-form-name'
+const person = {
+  first_name: 'Yu',
+  last_name: 'G',
+  mobile: '4151234356'
+}
 describe('@/compontent/profile/as-form-name.vue', () => {
-  const person = {
-    first_name: 'Scott',
-    last_name: 'Fryxell',
-    mobile: '4151234356'
-  }
   let wrapper
   beforeEach(() => {
-    wrapper = shallowMount(as_form, { propsData: { person: person } })
+    wrapper = shallowMount(as_form, { propsData: { person } })
   })
   it('Render profile name form', () => {
     expect(wrapper.element).toMatchSnapshot()
   })
-  it.todo('requires at least 3 letters total for a name')
-  it.todo('only shows the submit button when input is valid')
+  describe('methods:', () => {
+    describe('#valid', () => {
+      it('3 character full names are valid', () => {
+        wrapper.vm.valid()
+        expect(wrapper.emitted('valid')).toBeTruthy()
+      })
+      it('2 character full names will fail', () => {
+        const changed = { ...person }
+        changed.first_name = 's'
+        changed.last_name = 'f'
+        wrapper.setProps({ person: changed })
+        wrapper.vm.valid()
+        expect(wrapper.emitted('valid')).toBeFalsy()
+      })
+      it('first_name is required', () => {
+        const changed = { ...person }
+        changed.first_name = null
+        wrapper.setProps({ person: changed })
+        wrapper.vm.valid()
+        expect(wrapper.emitted('valid')).toBeFalsy()
+      })
+      it('last_name is required', () => {
+        const changed = { ...person }
+        changed.last_name = null
+        wrapper.setProps({ person: changed })
+        wrapper.vm.valid()
+        expect(wrapper.emitted('valid')).toBeFalsy()
+      })
+    })
+    describe('#modified_check', () => {
+      it('Emits no updates when there are no changes', () => {
+        wrapper.vm.modified_check()
+        expect(wrapper.emitted('update:person')).toBeFalsy()
+      })
+      it('Emits updates when first name is changed', () => {
+        wrapper.vm.first_name = 'Joe'
+        wrapper.vm.modified_check()
+        expect(wrapper.emitted('update:person')).toBeTruthy()
+      })
+      it('Emits updates when last name is changed', () => {
+        wrapper.vm.last_name = 'Schmo'
+        wrapper.vm.modified_check()
+        expect(wrapper.emitted('update:person')).toBeTruthy()
+      })
+      it('Changes button availability when valid', () => {
+        wrapper.vm.modified_check()
+        expect(wrapper.vm.$refs.button.disabled).toBe(false)
+        const changed = { ...person }
+        changed.last_name = null
+        wrapper.setProps({ person: changed })
+        expect(wrapper.vm.is_valid).toBe(false)
+        wrapper.vm.modified_check()
+        expect(wrapper.vm.$refs.button.disabled).toBe(true)
+      })
+    })
+  })
 })
