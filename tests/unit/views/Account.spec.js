95f004b53cf073424974e3e6d4d27db598dd7715
Scott Fryxell <sfryxell@me.com>
2020-12-08 18:51:24 -0800

    Tests of intersection_thought

--- tests/unit/views/Account.spec.js
+++ tests/unit/views/Account.spec.js
@@ -16,7 +16,7 @@ describe('@/views/Account.vue', () => {
   let wrapper, load_spy, list_spy
   beforeEach(() => {
     firebase.user = user
-    localStorage.me = '/+'
+    localStorage.me = '/+16282281824'
     load_spy = jest.spyOn(itemid, 'load').mockImplementation(_ => Promise.resolve(me))
     list_spy = jest.spyOn(itemid, 'list').mockImplementation(_ => Promise.resolve([]))
     wrapper = shallowMount(Account)
@@ -25,22 +25,47 @@ describe('@/views/Account.vue', () => {
     localStorage.clear()
     firebase.user = null
   })
-  it('Renders account information', async () => {
-    await flushPromises()
-    expect(load_spy).toBeCalled()
-    expect(list_spy).toBeCalled()
-    expect(wrapper.element).toMatchSnapshot()
+
+  describe('Rendering', () => {
+    it('Renders account information', async () => {
+      await flushPromises()
+      expect(load_spy).toBeCalled()
+      expect(list_spy).toBeCalled()
+      expect(wrapper.element).toMatchSnapshot()
+    })
   })
-  describe('computed', () => {
-    describe('#itemid', () => {
+  describe('computed:', () => {
+    describe('#statements_id', () => {
       it('give the current users id', async () => {
-        expect(wrapper.vm.statements_id).toBe('/+/statements')
+        expect(wrapper.vm.statements_id).toBe('/+16282281824/statements')
       })
     })
   })
-  describe('#destroy', () => {
-    it('Removes the css variable', async () => {
-      wrapper.destroy()
+  describe('methods:', () => {
+    describe('#thought_shown', () => {
+      const statements = [
+        { id: '/+16282281824/statements/1569168047725' },
+        { id: '/+16282281824/statements/1569909292018' },
+        { id: '/+16282281824/statements/1569909311638' }
+      ]
+      it('Checks if it\'s time to load more thoughts', async () => {
+        jest.spyOn(itemid, 'as_directory').mockImplementation(_ => {
+          return { items: ['index', '1569909311638'] }
+        })
+        wrapper.vm.statements = statements
+        await wrapper.vm.thought_shown(statements)
+      })
+    })
+    describe('#slot_key', () => {
+      const item = { id: '/+16282281824/statements/1569168047725' }
+      it('determines the slot key of an item', () => {
+        const key = wrapper.vm.slot_key(item)
+        expect(key).toBe('/+16282281824/statements/1569168047725')
+      })
+      it('determines the slot key of an array of items', () => {
+        const key = wrapper.vm.slot_key([item])
+        expect(key).toBe('/+16282281824/statements/1569168047725')
+      })
     })
   })
 })
