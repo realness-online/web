761bfe3a08c7c7d34b15ae8b8d6a2d67505d0d90
Scott Fryxell <sfryxell@me.com>
2020-12-15 13:26:58 -0800

    Optimize posters as they are being created

--- tests/unit/views/Posters.spec.js
+++ tests/unit/views/Posters.spec.js
@@ -19,58 +19,85 @@ describe('@/views/Posters.vue', () => {
     wrapper = shallowMount(Posters)
     wrapper.vm.events = events
   })
-  describe('Rendering', () => {
-    it('Renders ui for posters', () => {
+  describe('rendering:', () => {
+    it('Renders UI for posters', () => {
       expect(wrapper.element).toMatchSnapshot()
     })
+    it('Unmounts the worker when destroyed', () => {
+      const mock = jest.fn()
+      wrapper.vm.vectorizer = { terminate: mock }
+      wrapper.vm.optimizer = { terminate: mock }
+      wrapper.destroy()
+      expect(mock).toHaveBeenCalledTimes(2)
+    })
   })
-  it('Unmounts the worker when destroyed', () => {
-    const mock = jest.fn()
-    wrapper.vm.worker = { terminate: mock }
-    wrapper.destroy()
-    expect(mock).toBeCalled()
+  describe('computed', () => {
+    describe('as_itemid', () => {
+      wrapper = shallowMount(Posters)
+      localStorage.me = '/+16282281824'
+      wrapper.vm.new_poster = { created_at: 'now' }
+      expect(wrapper.vm.as_itemid).toBe('/+16282281824/posters/now')
+    })
   })
-  describe('methods', () => {
+  describe('methods:', () => {
     describe('get_id', () => {
       it('gets the poster id from the directory listing on hte network', () => {
         wrapper.vm.get_id(`${poster.id}.html`)
       })
     })
-    describe('vectorize_image', () => {
+    describe('#vectorize', () => {
       it('executes the method', () => {
-        wrapper.vm.vectorize_image()
+        wrapper.vm.vectorize()
       })
     })
-    describe('get_poster_list', () => {
-      it('executes the method', async () => {
-        jest.spyOn(itemid, 'as_directory').mockImplementationOnce(_ => {
-            return { items: [] }
-        })
-        await wrapper.vm.get_poster_list({})
-      })
-    })
-    describe('brand_new_poster', () => {
+    describe('#vectorized', () => {
       it('gets the poster from the worker', () => {
         const event = {
           data: poster
         }
         wrapper.vm.working = true
-        wrapper.vm.brand_new_poster(event)
+        wrapper.vm.vectorized(event)
         expect(wrapper.vm.working).toBe(false)
         expect(wrapper.vm.new_poster.id).toBe(poster.id)
       })
     })
-    describe('cancel_poster', () => {
+    describe('#optimize', () => {
+      it('Gives the optimize worker a vector to work on', () => {
+        wrapper.vm.optimizer.postMessage = jest.fn()
+        wrapper.vm.optimize({ id: 'mock-vector' })
+        expect(wrapper.vm.optimizer.postMessage).toBeCalled()
+      })
+    })
+    describe('#optimized', () => {
+      it('updates new_poster with the optimized vector', () => {
+        wrapper.vm.optimized({ data: { vector: poster_html } })
+        expect(wrapper.vm.new_poster.id).toBe('/+16282281824/posters/559666932867')
+      })
+    })
+    describe('#get_poster_list', () => {
+      it('Adds posters', async () => {
+        jest.spyOn(itemid, 'as_directory').mockImplementationOnce(_ => {
+            return { items: ['1576588885385'] }
+        })
+        await wrapper.vm.get_poster_list({})
+        expect(wrapper.vm.posters.length).toBe(1)
+      })
+      it('Handles an empty poster list', async () => {
+        jest.spyOn(itemid, 'as_directory').mockImplementationOnce(_ => null)
+        await wrapper.vm.get_poster_list({})
+      })
+    })
+    describe('#cancel_poster', () => {
       it('executes the method', () => {
         wrapper.vm.cancel_poster(poster.id)
       })
     })
-    describe('add_poster', () => {
+    describe('#save_poster', () => {
       it('executes the method', async () => {
-        await wrapper.vm.add_poster(poster.id)
+        await wrapper.vm.save_poster(poster.id)
       })
     })
-    describe('remove_poster', () => {
+    describe('#remove_poster', () => {
       it('executes the method', async () => {
         await wrapper.vm.remove_poster(poster.id)
       })
