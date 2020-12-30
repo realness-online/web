2ab43a6602ee1a60c2fc328e86d4e2dc50b46d1e
Scott Fryxell <sfryxell@me.com>
2020-12-30 06:49:56 -0800

    Move relations to a link from within the phonebook

--- src/views/Relations.vue
+++ src/views/Relations.vue
@@ -1,11 +1,10 @@
 <template>
   <section id="relations" class="page">
     <header>
-      <router-link v-if="signed_in" to="/phone-book">
-        <icon name="heart" />
+      <icon name="nothing" />
+      <router-link to="/phone-book">
+        <icon name="finished" />
       </router-link>
-      <icon v-else name="nothing" />
-      <logo-as-link />
     </header>
     <hgroup>
       <h1>Relations</h1>
@@ -13,27 +12,17 @@
     <nav v-if="signed_in" class="profile-list">
       <as-figure v-for="person in relations" :key="person.id" :person="person" :relations.sync="relations" />
     </nav>
-    <hgroup v-else class="sign-on message">
-      <p>
-        <sign-on /> and you can check out <icon name="heart" /> who's on here
-      </p>
-      <h6><a>Watch</a> a video and learn some more</h6>
-    </hgroup>
   </section>
 </template>
 <script>
   import { list, load } from '@/helpers/itemid'
   import signed_in from '@/mixins/signed_in'
   import icon from '@/components/icon'
-  import logo_as_link from '@/components/logo-as-link'
   import as_figure from '@/components/profile/as-figure'
-  import sign_on from '@/components/profile/sign-on'
   export default {
     components: {
       icon,
-      'sign-on': sign_on,
-      'as-figure': as_figure,
-      'logo-as-link': logo_as_link
+      'as-figure': as_figure
     },
     mixins: [signed_in],
     data () {
@@ -54,11 +43,11 @@
 </script>
 <style lang='stylus'>
   section#relations
-    svg
-      &.heart
-        fill: blue
-      &.working
-        margin-bottom: base-line
     & > header
       margin: auto
+      svg
+        &.finished
+          fill: blue
+        &.working
+          margin-bottom: base-line
 </style>
