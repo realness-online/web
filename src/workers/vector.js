c49a1ead9b48208d17d546ecf2c44ebf8aacae14
Scott Fryxell <sfryxell@me.com>
2021-01-13 20:13:17 -0800

    Optimize posters that are way too large
    tweakes setting for poster creation

--- src/workers/vector.js
+++ src/workers/vector.js
@@ -1,5 +1,5 @@
 import { Jimp, as_paths } from '@realness.online/potrace'
-const jimp_options = {
+const potrace_options = {
   threshold: 255,
   turdSize: 125,
   optTolerance: 0.55,
@@ -16,16 +16,23 @@ export async function read (file) {
   const reader = new FileReaderSync()
   return await Jimp.read(reader.readAsArrayBuffer(file))
 }
-export async function prepare (image, size = 512) {
-  image.dither565().posterize(10).normalize().threshold(brighness_options)
+export async function size (image, size = 512) {
   if (image.bitmap.width > image.bitmap.height) image = image.resize(Jimp.AUTO, size)
   else image = image.resize(size, Jimp.AUTO)
   return image
 }
+export async function prepare (image) {
+  return image.dither565().posterize(10).normalize().threshold(brighness_options)
+}
 export async function make (image) {
-  const poster = await as_paths(image, jimp_options)
+  let poster = await as_paths(image, potrace_options)
+  console.log(`${to_kb(poster)}kb`)
+  if (to_kb(poster) > 700) {
+    image = await size(image, 416)
+    poster = await as_paths(image, potrace_options)
+  }
+  console.log(`${to_kb(poster)}kb`)
   return {
-    created_at: Date.now(),
     path: poster.paths,
     viewbox: `0 0 ${poster.width} ${poster.height}`
   }
@@ -33,7 +40,17 @@ export async function make (image) {
 export async function listen (message) {
   let image = await read(message.data.image)
   image = await prepare(image)
+  image = await size(image)
   const vector = await make(image)
   self.postMessage(vector)
 }
 self.addEventListener('message', listen)
+
+function to_kb (vector) {
+  console.log(vector)
+  let size_of = 0
+  vector.paths.forEach(path => {
+    size_of += path.length
+  })
+  return (size_of / 1024).toFixed(2)
+}
