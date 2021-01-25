f1a524b82932e0bb23dd1754e1a6eab275811db8
Scott Fryxell <sfryxell@me.com>
2021-01-24 17:25:36 -0800

    Update facicon and move service worker to the workers directory

--- vue.config.js
+++ vue.config.js
@@ -17,37 +17,7 @@ module.exports = {
   pwa: {
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
-      swSrc: 'src/service.worker.js'
-    },
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black-translucent',
-    iconPaths: {
-      appleTouchIcon: 'icons/apple-touch-icon.png'
-    },
-    manifestOptions: {
-      short_name: 'Realness',
-      description: 'realness.online – A chill vector space for the realness in all of us',
-      scope: '/',
-      orientation: 'portrait',
-      background_color: '#52a0d1',
-      icons: [
-        {
-          src: '/icons/logo.svg', // inside the scope!
-          sizes: '192x192',
-          type: 'image/svg+xml', // not image/svg which is still visible in web
-          purpose: 'any' // not 'maskable any' as you may see there in answers.
-        },
-        {
-          src: '/icons/android-chrome-192x192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: '/icons/android-chrome-512x512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        }
-      ]
+      swSrc: 'src/workers/service.worker.js'
     }
   },
   configureWebpack: {
