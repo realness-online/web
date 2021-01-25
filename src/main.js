f4c6fb1da67f9fa8de9152c0eaf8e1ed915c9ac5
Scott Fryxell <sfryxell@me.com>
2021-01-24 17:25:36 -0800

    Update facicon and move service worker to the workers directory

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './register_service_worker'
+import './workers/register_service_worker'
 const me = localStorage.me
 if (!me) localStorage.me = '/+'
 Vue.config.productionTip = false
