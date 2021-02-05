1e27e13b3c71b0b022d1c51d437b8987fb7ac864
Scott Fryxell <sfryxell@me.com>
2021-02-05 10:45:41 -0800

    Visit now updates if It's been over an hour since it last saved a visit

--- src/mixins/visit.js
+++ src/mixins/visit.js
@@ -2,20 +2,23 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import { load } from '@/helpers/itemid'
 import { from_e64 } from '@/helpers/profile'
-import { is_fresh } from '@/helpers/date'
+import { visit_interval } from '@/workers/sync'
 export default {
-  created () {
-    firebase.auth().onAuthStateChanged(this.update_visit)
-  },
+  created () { this.update_visit() },
   methods: {
-    async update_visit (user) {
-      if (navigator.onLine && user) {
-        const person = await load(from_e64(user.phoneNumber))
-        if (person && !is_fresh(person.visited)) {
-          person.visited = new Date().toISOString()
-          this.$emit('update:person', person)
+    update_visit () {
+      firebase.auth().onAuthStateChanged(async user => {
+        if (navigator.onLine && user) {
+          const person = await load(from_e64(user.phoneNumber))
+          if (!person) return // Do nothing until there is a person
+          const visit_digit = new Date(person.visited).getTime()
+          if (visit_interval() > visit_digit) {
+            const new_visit = new Date().toISOString()
+            person.visited = new_visit
+            this.$emit('update:person', person)
+          }
         }
-      }
+      })
     }
   }
 }
--- tests/unit/mixins/visit.spec.js
+++ tests/unit/mixins/visit.spec.js
@@ -1,17 +1,27 @@
 import { shallowMount } from '@vue/test-utils'
 import * as itemid from '@/helpers/itemid'
+import firebase from 'firebase/app'
+import 'firebase/auth'
 import vector_mock from './mixin_mock'
-const person = {
-  first_name: 'Scott',
-  last_name: 'Fryxell',
-  id: '/+14151234356',
-  avatar: '/+14151234356/avatars/1578929551564'
-}
-
+import flushPromises from 'flush-promises'
+const current_user = { phoneNumber: '+16282281824' }
 describe('@/mixins/visit', () => {
   let wrapper
+  let person
+  let load_spy
   beforeEach(() => {
+    firebase.user = current_user
+    person = {
+      first_name: 'Scott',
+      last_name: 'Fryxell',
+      id: '/+14151234356',
+      avatar: '/+14151234356/avatars/1578929551564',
+      visited: '2021-02-04T18:30:01.038Z'
+    }
     wrapper = shallowMount(vector_mock)
+  })
+  afterEach(() => {
+    firebase.user = null
   })
   describe('methods', () => {
     describe('#visit', () => {
@@ -20,13 +30,23 @@ describe('@/mixins/visit', () => {
       })
       it('updates the user with a visit', async () => {
         const load_spy = jest.spyOn(itemid, 'load').mockImplementation(_ => Promise.resolve(person))
-        await wrapper.vm.update_visit({ phoneNumber: '+16282281824' })
+        await wrapper.vm.update_visit()
+        await flushPromises()
         expect(load_spy).toBeCalled()
         expect(wrapper.emitted('update:person')).toBeTruthy()
       })
       it('Does nothing unless there is a person', async () => {
         const load_spy = jest.spyOn(itemid, 'load').mockImplementation(_ => Promise.resolve(null))
-        await wrapper.vm.update_visit({ phoneNumber: '+16282281824' })
+        await wrapper.vm.update_visit()
+        await flushPromises()
+        expect(load_spy).toBeCalled()
+        expect(wrapper.emitted('update:person')).not.toBeTruthy()
+      })
+      it('Waits a proper interval before update to visit', async () => {
+        person.visited = new Date().toISOString()
+        const load_spy = jest.spyOn(itemid, 'load').mockImplementation(_ => Promise.resolve(person))
+        await wrapper.vm.update_visit()
+        await flushPromises()
         expect(load_spy).toBeCalled()
         expect(wrapper.emitted('update:person')).not.toBeTruthy()
       })
