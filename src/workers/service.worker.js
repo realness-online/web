8d01234074a30c313d27ec62d0532744df25de5e
Scott Fryxell <sfryxell@me.com>
2021-03-07 14:05:49 -0800

    Remove faulty code and leave an article for a future fix

--- src/workers/service.worker.js
+++ src/workers/service.worker.js
@@ -4,7 +4,9 @@ self.__precacheManifest = [].concat(self.__precacheManifest || [])
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
 self.addEventListener('install', event => {
   self.skipWaiting().then(() => {
-    window.reload()
+    // https://redfin.engineering/how-to-fix-the-refresh-button-when-using-service-workers-a8e27af6df68
+    // Maybe one day we can handle updates to the service workers
+    // but It works pretty good right now that eventually the changes roll out
   })
 })
 self.addEventListener('activate', event => {
