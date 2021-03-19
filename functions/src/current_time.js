736bde784a4d6ac0cfcfc31cf3aadd546be2cc1a
Scott Fryxell <sfryxell@me.com>
2021-03-19 16:43:35 -0700

    Remove unused firebase function

--- functions/src/current_time.js
+++
@@ -1,8 +0,0 @@
-const functions = require('firebase-functions')
-const cors = require('cors')({ origin: true })
-exports.date = functions.https.onRequest((req, res) => {
-  if (req.method === 'PUT') return res.status(403).send('Forbidden!')
-  return cors(req, res, () => {
-    res.status(200).send(new Date().getTime())
-  })
-})
