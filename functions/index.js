fd83dd1bf1d992d8359255a7c6c699026eee8c8e
Scott Fryxell <sfryxell@me.com>
2021-03-19 16:43:35 -0700

    Remove unused firebase function

--- functions/index.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const functions = require('firebase-functions')
-const admin = require('firebase-admin')
-admin.initializeApp()
-exports.create_admin = functions.storage.object().onFinalize(file => {
-  const claims = {}
-  claims.admin = true
-  return admin.auth().setCustomUserClaims('user-id-goes-here', claims)
-})
