d7591440d60be6fa5731e07712c5a03accc03895
Scott Fryxell <sfryxell@me.com>
2021-03-30 08:21:06 -0700

    Support users in other timezones running tests

--- tests/unit/components/sync.spec.js
+++ tests/unit/components/sync.spec.js
@@ -187,7 +187,7 @@ describe('@/components/sync', () => {
       it('Only syncs when the network and local statements differ', async () => {
         jest.spyOn(sync_worker, 'fresh_metadata')
         .mockImplementation(_ => Promise.resolve({
-           customMetadata: { md5: 'MuWitqM+87rpdNO7nuoljg==' }
+           customMetadata: { md5: 'eL7hc0lLvyQjuL+5Gst2Aw==' }
         }))
         wrapper = await mount(sync, fake_props)
         await flushPromises()
