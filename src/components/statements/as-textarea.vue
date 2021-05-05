ceb05bff479dcc769b878893fa966d5df7016f3a
Scott Fryxell <sfryxell@me.com>
2021-05-05 14:05:53 -0700

    Adding spellcheck to text input

--- src/components/statements/as-textarea.vue
+++ src/components/statements/as-textarea.vue
@@ -2,6 +2,7 @@
   <textarea id="wat" v-model="new_statement"
             cols="1" rows="1"
             placeholder=">"
+            spellcheck="true"
             @focusout="prepare_statement"
             @focusin="focused" />
 </template>
