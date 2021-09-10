e28cbeec79742c6daf54a39f1db4e6e4764d5c6d
Scott Fryxell <scott.fryxell@hey.com>
2021-09-10 14:00:42 -0700

    Let's call them relations again

--- src/views/Navigation.vue
+++ src/views/Navigation.vue
@@ -14,7 +14,7 @@
         Feed
       </router-link>
       <router-link v-if="!posting" to="/phone-book" class="blue" tabindex="-1">
-        People
+        Relations
       </router-link>
       <button v-if="posting" tabindex="-1" @click="done_posting">Done</button>
       <statement-as-textarea class="red"
