37cde1e1c944adfd0db884bc242850c1f9720cd6
Scott Fryxell <scott.fryxell@hey.com>
2021-10-04 11:06:46 -0700

    Add Forms for editing the different interesting bits of a poster

---
+++ src/components/posters/as-grid-input.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <svg id="grid" viewBox="0 0 2 2">
+    <rect width="1" height="1" />
+    <rect width="1" height="1" />
+    <rect width="1" height="1" />
+    <rect width="1" height="1" />
+  </svg>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style lang="stylus">
+  svg#grid
+    width: 100%
+    height: 100%
+</style>
