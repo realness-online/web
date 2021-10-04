929eaa1379104d9c8a8f9b65753188f81cb03742
Scott Fryxell <scott.fryxell@hey.com>
2021-10-04 11:06:03 -0700

    Rename opacity to editor
    Will subdivide the view into components so that the browser history does 
    not become polluted

--- tests/unit/views/Editor.spec.js
+++ tests/unit/views/Editor.spec.js
@@ -1,15 +1,15 @@
 import { shallowMount } from '@vue/test-utils'
 import firebase from 'firebase/app'
 import 'firebase/auth'
-import Opacity from '@/views/Opacity'
+import Editor from '@/views/Editor'
 const user = { phoneNumber: '/+16282281824' }
 
-describe('@/views/Opacity.vue', () => {
+describe('@/views/Editor.vue', () => {
   describe('Renders', () => {
     it('Shows an editor for a poster or an avatar', async () => {
       firebase.user = user
       const $route = { params: { phone_number: '+14151231234' } }
-      const wrapper = await shallowMount(Opacity, {
+      const wrapper = await shallowMount(Editor, {
         mocks: { $route },
         stubs: ['router-link', 'router-view']
       })
