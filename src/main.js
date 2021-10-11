9434469f0debd35f0779fb5e6cadbc6dd955e528
Scott Fryxell <scott.fryxell@hey.com>
2021-10-11 06:31:30 -0700

    Upgrage app initialization to vue3

--- src/main.js
+++ src/main.js
@@ -1,24 +1,7 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import router from './views/router'
-import { register } from 'register-service-worker'
-import VueHotkey from 'v-hotkey'
+import router from './router'
+import './workers/register_service_worker'
 const me = localStorage.me
 if (!me) localStorage.me = '/+'
-if (process.env.NODE_ENV === 'production') {
-  register(`${process.env.BASE_URL}service.worker.js`, {
-    ready () { console.log('service worker is ready') },
-    registered () { console.log('Service worker has been registered.') },
-    cached () { console.log('Content has been cached for offline use.') },
-    updatefound () { console.log('New content is downloading.') },
-    updated () { console.log('New content is available; please refresh.') },
-    offline () { console.log('No internet connection found. App is running in offline mode.') },
-    error (error) { console.error('Error during service worker registration:', error) }
-  })
-}
-Vue.config.productionTip = false
-Vue.use(VueHotkey)
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#loading-realness')
+createApp(App).use(router).mount('body')
---
+++ src/router.js
@@ -0,0 +1,32 @@
+import { createWebHistory, createRouter } from 'vue-router'
+import Events from '@/views/Events'
+import Feed from '@/views/Feed'
+import Posters from '@/views/Posters'
+import Account from '@/views/Account'
+import Navigation from '@/views/Navigation'
+import Profile from '@/views/Profile'
+import Relations from '@/views/Relations'
+import PhoneBook from '@/views/PhoneBook'
+import Sign_on from '@/views/Sign-on'
+import Editor from '@/views/Editor'
+
+const routes = [
+  { path: '/', component: Navigation },
+  { path: '/sign-on', component: Sign_on },
+  { path: '/events', component: Events },
+  { path: '/feed', component: Feed },
+  { path: '/posters', component: Posters },
+  { path: '/posters/:id/editor', component: Editor },
+  { path: '/relations', component: Relations },
+  { path: '/phone-book', component: PhoneBook },
+  { path: '/profile', component: Profile },
+  { path: '/account', component: Account },
+  { path: '/:phone_number', component: Profile }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
