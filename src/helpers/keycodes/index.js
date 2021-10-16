91baf5a68f52dfc900741fe2a5f86669bc5c7d6c
Scott Fryxell <scott.fryxell@hey.com>
2021-10-16 12:08:48 -0700

    Map key commands into the application code.
    I am using code from v-hotkey as the keymap is easy to read
    
    I want to take full ownership of the kemap. I have an idea for a typing 
    app that would make good use of an eventing component of key commands

---
+++ src/helpers/keycodes/index.js
@@ -0,0 +1,86 @@
+import codes from './codes'
+import { splitCombination, returnCharCode } from '../helpers'
+
+const noop = () => {}
+
+const defaultModifiers = {
+  ctrlKey: false,
+  altKey: false,
+  shiftKey: false,
+  metaKey: false
+}
+
+const alternativeKeyNames = {
+  option: 'alt',
+  command: 'meta',
+  return: 'enter',
+  escape: 'esc',
+  plus: '+',
+  mod: /Mac|iPod|iPhone|iPad/.test(navigator.platform) ? 'meta' : 'ctrl'
+}
+
+/**
+ *
+ * @param {Object} combinations
+ * @param {Object} alias
+ * @returns {Object}
+ */
+export const getKeyMap = (combinations, alias) => {
+  const result = []
+
+  Object.keys(combinations).forEach(combination => {
+    const { keyup, keydown } = combinations[combination]
+    const callback = {
+      keydown: keydown || (keyup ? noop : combinations[combination]),
+      keyup: keyup || noop
+    }
+    const keys = splitCombination(combination)
+    const { code, modifiers } = resolveCodesAndModifiers(keys, alias)
+
+    result.push({
+      code,
+      modifiers,
+      callback
+    })
+  })
+
+  return result
+}
+
+/**
+ *
+ * @param {Array} keys
+ * @param {Object} alias
+ * @returns {Object}
+ */
+const resolveCodesAndModifiers = (keys, alias) => {
+  let modifiers = { ...defaultModifiers }
+  if (keys.length > 1) {
+    return keys.reduce((acc, key) => {
+      key = alternativeKeyNames[key] || key
+      if (defaultModifiers.hasOwnProperty(`${key}Key`)) {
+        acc.modifiers = { ...acc.modifiers, [`${key}Key`]: true }
+      } else {
+        acc.code = alias[key] || searchKeyCode(key)
+      }
+      return acc
+    }, { modifiers })
+  }
+
+  const key = alternativeKeyNames[keys[0]] || keys[0]
+  if (defaultModifiers.hasOwnProperty(`${key}Key`)) {
+    modifiers = { ...modifiers, [`${key}Key`]: true }
+  }
+  const code = alias[key] || searchKeyCode(key)
+
+  return {
+    modifiers,
+    code
+  }
+}
+
+/**
+ *
+ * @param {String} key
+ */
+const searchKeyCode = key => codes[key.toLowerCase()] || returnCharCode(key)
