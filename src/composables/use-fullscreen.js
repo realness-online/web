69b13abe22d2cefa31d9a46e0b68c798c122bba5
Scott Fryxell <scott.fryxell@hey.com>
2021-10-19 08:06:32 -0700

    use fullscreen composable

---
+++ src/composables/use-fullscreen.js
@@ -0,0 +1,16 @@
+import { ref } from "vue"
+
+export default function use_fullscreen () {
+  const  is_fullscreen = ref(false)
+  is_fullscreen.value = !is_fullscreen.value
+
+  try {
+    this.$el.requestFullscreen()
+  } catch (e) {
+    this.$el.webkitRequestFullScreen()
+  }
+
+  return {
+    is_fullscreen
+  }
+}
