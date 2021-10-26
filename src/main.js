e107d0c6920b9ae1d6124f88e22b026dee1a1835
Scott Fryxell <scott.fryxell@hey.com>
2021-10-25 19:35:25 -0700

    Upgrade to vue 3.2

--- src/main.js
+++ src/main.js
@@ -1,13 +1,8 @@
-import { createApp, configureCompat } from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './workers/register_service_worker'
 const me = localStorage.me
 if (!me) localStorage.me = '/+'
-
-// TODO: remove after 3.2
-configureCompat({
-  WATCH_ARRAY: false
-})
 
 createApp(App).use(router).mount('body')
