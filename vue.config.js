f3dd8b3ab79df935e683f505bd02d7f48cb082c5
Scott Fryxell <scott.fryxell@hey.com>
2021-10-25 19:35:25 -0700

    Upgrade to vue 3.2

--- vue.config.js
+++ vue.config.js
@@ -72,21 +72,5 @@ module.exports = {
         minSize: 200000
       }
     }
-  },
-  chainWebpack: config => {
-    config.resolve.alias.set('vue', '@vue/compat')
-    config.module
-      .rule('vue')
-      .use('vue-loader')
-      .tap(options => {
-        return {
-          ...options,
-          compilerOptions: {
-            compatConfig: {
-              MODE: 3
-            }
-          }
-        }
-      })
   }
 }
