8a9217e5a994f09721df11048a992e118f3728dc
Scott Fryxell <scott.fryxell@hey.com>
2021-11-02 14:32:35 -0700

    remove 'this'

--- src/use/color-editor.js
+++ src/use/color-editor.js
@@ -1,10 +1,10 @@
 export function change_color(id, color, type = 'fill') {
-  const path = this.get_path_element(id)
+  const path = get_path_element(id)
   path.setAttribute(type, color)
 }
 
 export function get_color(id, type = 'fill') {
-  return this.get_path_element(id).getAttribute(`${type}`)
+  return get_path_element(id).getAttribute(`${type}`)
 }
 
 export function get_path_element(id) {
@@ -12,7 +12,7 @@ export function get_path_element(id) {
 }
 
 export function get_path_attribute(id, type = 'fill') {
-  return this.get_path_element(id).getAttribute(`${type}`)
+  return get_path_element(id).getAttribute(`${type}`)
 }
 
 export default change_color
