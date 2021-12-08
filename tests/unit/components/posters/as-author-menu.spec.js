9e68f74a30342c29e806a145a870c6602865620c
Scott Fryxelo <sfryxell@me.com>
2021-12-08 15:14:31 -0800

    Remove need for migrate_poster in tests

--- tests/unit/components/posters/as-author-menu.spec.js
+++ tests/unit/components/posters/as-author-menu.spec.js
@@ -1,13 +1,11 @@
 import { shallowMount } from '@vue/test-utils'
 import as_author_menu from '@/components/posters/as-author-menu'
-import { migrate_poster } from '@/use/vector'
 import get_item from '@/use/item'
 const poster_html = require('fs').readFileSync(
   './tests/unit/html/poster.html',
   'utf8'
 )
 let poster = get_item(poster_html)
-poster = migrate_poster(poster)
 describe('@/components/posters/as-author-menu.vue', () => {
   describe('Renders', () => {
     it('a menu to edit a poster', () => {
