2fb0063de5c0609465ff785e9ac69ae2ab521636
Scott Fryxelo <sfryxell@me.com>
2021-12-08 15:14:31 -0800

    Remove need for migrate_poster in tests

--- tests/unit/components/posters/as-svg.spec.js
+++ tests/unit/components/posters/as-svg.spec.js
@@ -1,13 +1,11 @@
 import { shallowMount } from '@vue/test-utils'
 import as_svg from '@/components/posters/as-svg'
-import { migrate_poster } from '@/use/vector'
 import get_item from '@/use/item'
 const poster_html = require('fs').readFileSync(
   './tests/unit/html/poster.html',
   'utf8'
 )
 let poster = get_item(poster_html)
-poster = migrate_poster(poster)
 describe('@/components/posters/as-svg.vue', () => {
   describe('Renders', () => {
     it('A working icon initially', () => {
