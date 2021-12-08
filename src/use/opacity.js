cacf2fd7d5e47854f715c790893824f315c0a45b
Scott Fryxelo <sfryxell@me.com>
2021-12-08 12:58:21 -0800

    Fix up opacity

--- src/use/opacity.js
+++ src/use/opacity.js
@@ -1,14 +1,19 @@
-export function change(opacity = 0.5, direction = 'more', resolution = 0.025) {
-  if (typeof opacity != 'number') opacity = 0.5
+export function change(
+  opacity = 0.025,
+  direction = 'more',
+  resolution = 0.025
+) {
   opacity = parseFloat(opacity)
+
   opacity = opacity * 10000
   opacity = Math.round(opacity)
   opacity = opacity / 10000
 
-  if (direction === 'less') opacity += resolution
+  if (direction === 'more') opacity += resolution
   else opacity -= resolution
 
-  if (opacity > 0.9) opacity = 0.8
+  if (opacity > 0.9) opacity = 0.875
   else if (opacity < 0) opacity = 0.025
+
   return opacity
 }
