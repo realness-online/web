2f90a42d821dbdea0927d0f1a9fa9abc35994979
Scott Fryxelo <sfryxell@me.com>
2021-12-12 18:08:52 -0800

    don't need to pass more or less just pass positive or negative values

--- src/use/opacity.js
+++ src/use/opacity.js
@@ -1,16 +1,11 @@
-export function change(
-  opacity = 0.025,
-  direction = 'more',
-  resolution = 0.025
-) {
+export function change(opacity = 0.025, resolution = 0.025) {
   opacity = parseFloat(opacity)
 
   opacity = opacity * 10000
   opacity = Math.round(opacity)
   opacity = opacity / 10000
 
-  if (direction === 'more') opacity += resolution
-  else opacity -= resolution
+  opacity += resolution
 
   if (opacity > 0.9) opacity = 0.875
   else if (opacity < 0) opacity = 0.025
