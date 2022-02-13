d38afd9492332d817b3e6b10dd0b27500edc2eab
Scott Fryxelo <sfryxell@me.com>
2022-02-12 16:02:10 -0800

    A que that you have hit the wall

--- src/use/opacity.js
+++ src/use/opacity.js
@@ -1,4 +1,4 @@
-export function change(opacity = 0.025, resolution = 0.025) {
+export function change(opacity = 0.025, resolution = 0.05) {
   opacity = parseFloat(opacity)
 
   opacity = opacity * 10000
@@ -7,7 +7,7 @@ export function change(opacity = 0.025, resolution = 0.025) {
 
   opacity += resolution
 
-  if (opacity > 0.99) opacity = 1
+  if (opacity > 1) opacity = 0.9
   else if (opacity < 0) opacity = 0.025
 
   return opacity
