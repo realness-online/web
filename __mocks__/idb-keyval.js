cb7ae14af3c977aa6f357ebc694610dfc9d51d88
Scott Fryxelo <sfryxell@me.com>
2022-03-11 20:03:58 -0800

    tidy up

--- __mocks__/idb-keyval.js
+++ __mocks__/idb-keyval.js
@@ -1,4 +1,3 @@
-import { vi } from 'vitest'
 export const get = vi.fn(() => Promise.resolve(undefined))
 export const set = vi.fn(() => Promise.resolve())
 export const del = vi.fn(() => Promise.resolve())
