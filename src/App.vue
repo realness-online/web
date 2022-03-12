61de46b7382236ce1c828d376e06fb9992eebfef
Scott Fryxelo <sfryxell@me.com>
2022-03-11 17:21:52 -0800

    Convert to composition api

--- src/App.vue
+++ src/App.vue
@@ -9,66 +9,36 @@
     </aside>
   </main>
 </template>
-<script>
+<script setup>
+  import { onUnmounted as dismount } from 'vue'
   import firebase from 'firebase/compat/app'
   import { get, set } from 'idb-keyval'
   import sync from '@/components/sync'
-  export default {
-    components: { sync },
-    data() {
-      return {
-        working: true,
-        me: null,
-        statement: null,
-        status: null
-      }
-    },
-    computed: {
-      is_production() {
-        return import.meta.env.NODE_ENV === 'production'
-      }
-    },
-    async created() {
-      if (this.is_production) {
-        const response = await fetch('__/firebase/init.json')
-        await set('firebase-keys', await response.json())
-      } else {
-        const keys = {
-          apiKey: import.meta.env.VITE_API_KEY,
-          authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-          databaseUrl: import.meta.env.VITE_DATABASE_URL,
-          projectId: import.meta.env.VITE_PROJECT_ID,
-          storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-          messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID
-        }
-        await set('firebase-keys', keys)
-      }
-      firebase.initializeApp(await get('firebase-keys'))
-      window.addEventListener('online', this.online)
-      window.addEventListener('offline', this.offline)
-      this.working = false
-    },
-    beforeUnMount() {
-      window.removeEventListener('online', this.online)
-      window.removeEventListener('offline', this.offline)
-    },
-    methods: {
-      sync_active(active) {
-        if (active) this.status = 'working'
-        else this.status = null
-      },
-      online() {
-        const editable = document.querySelectorAll('[contenteditable]')
-        editable.forEach(e => e.setAttribute('contenteditable', true))
-        this.status = null
-      },
-      offline() {
-        const editable = document.querySelectorAll('[contenteditable]')
-        editable.forEach(e => e.setAttribute('contenteditable', false))
-        this.status = 'offline'
-      }
-    }
+  const working = ref(true)
+  const status = ref(null)
+  const me = ref(undefined)
+  const statement = ref(undefined)
+  const sync_active = active => {
+    if (active.value) status.value = 'working'
+    else status.value = null
   }
+  const online = () => {
+    const editable = document.querySelectorAll('[contenteditable]')
+    editable.forEach(e => e.setAttribute('contenteditable', true))
+    status.value = null
+  }
+  const offline = () => {
+    const editable = document.querySelectorAll('[contenteditable]')
+    editable.forEach(e => e.setAttribute('contenteditable', false))
+    status.value = 'offline'
+  }
+  window.addEventListener('online', online)
+  window.addEventListener('offline', offline)
+  working.value = false
+  dismount(() => {
+    window.removeEventListener('online', online)
+    window.removeEventListener('offline', offline)
+  })
 </script>
 <style src="@/style/index.styl" lang="stylus"></style>
 <style lang="stylus">
