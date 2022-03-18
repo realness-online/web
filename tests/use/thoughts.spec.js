b7fbbf2e110a0d9dd381afbdfb8d577f5c5f9e97
Scott Fryxelo <sfryxell@me.com>
2022-03-18 13:22:39 -0700

    Moving tests to proper locations

--- tests/use/thoughts.spec.js
+++ tests/use/thoughts.spec.js
@@ -20,5 +20,39 @@ describe('@/use/itemid', () => {
         expect(thoughts.length).toBe(3)
       })
     })
+    describe('#thought_shown', () => {
+      const statements = [
+        { id: '/+16282281824/statements/1569168047725' },
+        { id: '/+16282281824/statements/1569909292018' },
+        { id: '/+16282281824/statements/1569909311638' }
+      ]
+      it("Checks if it's time to load more thoughts", async () => {
+        vi.spyOn(itemid, 'as_directory').mockImplementation(() => {
+          return { items: ['index', '1569909311638'] }
+        })
+        wrapper.vm.statements = statements
+        await wrapper.vm.thought_shown(statements)
+      })
+    })
+    describe('#slot_key', () => {
+      const item = { id: '/+16282281824/statements/1569168047725' }
+      it('Determines the slot key of an item', () => {
+        const key = wrapper.vm.slot_key(item)
+        expect(key).toBe('/+16282281824/statements/1569168047725')
+      })
+      it('Determines the slot key of an array of items', () => {
+        const key = wrapper.vm.slot_key([item])
+        expect(key).toBe('/+16282281824/statements/1569168047725')
+      })
+    })
+    describe('#get_all_my_stuff', () => {
+      it('Handles empty person', () => {
+        load_spy = vi
+          .spyOn(itemid, 'load')
+          .mockImplementation(() => Promise.resolve(null))
+        wrapper.vm.get_all_my_stuff()
+        expect(load_spy).toBeCalled()
+      })
+    })
   })
 })
