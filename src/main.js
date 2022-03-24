3d79065c709d6b2bdafa9b17d5810a68f990de01
Scott Fryxelo <sfryxell@me.com>
2022-03-24 10:21:19 -0700

    Remove duplicative pwa stuff

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './workers/register_service_worker'
 const me = localStorage.me
 if (!me) localStorage.me = '/+'
 
