038194759f841aafbc612f92ff850f208a538715
Scott Fryxelo <sfryxell@me.com>
2022-03-25 21:38:38 -0700

    Move sync code into cloud.sync

--- src/components/sync.vue
+++ src/components/sync.vue
@@ -31,24 +31,7 @@
   import UnsyncedPoster from '@/components/posters/as-svg'
   import ThoughtAsArticle from '@/components/statements/as-article'
   import AsAddress from '@/components/profile/as-address'
-  import { current_user } from '@/use/serverless'
-  import { del, get } from 'idb-keyval'
-  import {
-    one_hour,
-    fresh_metadata,
-    hash_options,
-    prune,
-    sync_offline_actions
-  } from '@/persistance/Cloud.sync'
-  import { Statements, Events, Poster, Me } from '@/persistance/Storage'
-  import { from_e64 } from '@/use/profile'
-  import { list, load } from '@/use/itemid'
-  import get_item from '@/use/item'
-  import hash from 'object-hash'
-  import { watch, watchEffect, ref, nextTick as next_tick } from 'vue'
-
-  const sync = ref(null)
-  const eight_hours = one_hour * 4
+  import { use as use_sync, get_itemid } from '@/persistance/Cloud.sync'
   const props = defineProps({
     statement: {
       type: Object,
@@ -62,146 +45,10 @@
     }
   })
   const emit = defineEmits(['update:statement', 'update:person', 'active'])
-  const poster = ref(null)
-  const statements = ref([])
-  const events = ref(null)
+  const { sync, statements, poster, events, play } = use_sync(props, emit)
 
   const visibility_change = async () => {
     if (document.visibilityState === 'visible') await play()
   }
-  const get_itemid = type => {
-    if (type) return `${localStorage.me}/${type}`
-    else return localStorage.me
-  }
-  const play = async () => {
-    const me = await load(localStorage.me) // check if new user
-    if (!me || current_user.value === null) return null // let's wait to sync
-    await sync_offline_actions()
-    let synced
-    if (localStorage.sync_time) {
-      synced = Date.now() - new Date(localStorage.sync_time).getTime()
-    } else synced = eight_hours
-    const time_left = eight_hours - synced
-    if (time_left <= 0) {
-      // setTimeout(async () => {
-      emit('active', true)
-      await prune()
-      await sync_me()
-      await sync_statements()
-      await sync_events()
-      await sync_anonymous_posters()
-      await sync_happened()
-      emit('active', false)
-      // }, 1000)
-    }
-  }
-  const sync_happened = async () => {
-    const statements = new Statements()
-    await statements.optimize()
-    localStorage.sync_time = new Date().toISOString()
-    const me = await load(localStorage.me)
-    if (!me) return // new user let's wait
-    if (!me.visited) me.visited = null
-    const visit_gap = Date.now() - new Date(me.visited).getTime()
-    if (me && visit_gap > one_hour) {
-      me.visited = new Date().toISOString()
-      emit('update:person', me)
-    }
-  }
-  const sync_me = async () => {
-    const id = get_itemid()
-    const network = (await fresh_metadata(id)).customMetadata
-    let my_info = localStorage.getItem(id)
-    if (!my_info) my_info = await get(id)
-    if (!my_info || !network) return
-    const md5 = hash(my_info, hash_options)
-    if (md5 !== network.md5) {
-      localStorage.removeItem(id)
-      del(id)
-    }
-  }
-  const sync_statements = async () => {
-    const statements = new Statements()
-    const itemid = get_itemid('statements')
-    const network = (await fresh_metadata(itemid)).customMetadata
-    const elements = sync.value.querySelector(`[itemid="${itemid}"]`)
-    if (!elements || !elements.outerHTML) return null // nothing local so we'll let it load on request
-    const md5 = hash(elements.outerHTML, hash_options)
-    if (!network || network.md5 !== md5) {
-      statements.value = await statements.sync()
-      if (statements.value.length) {
-        await next_tick()
-        await statements.save(elements)
-        localStorage.removeItem('/+/statements')
-      }
-    }
-  }
-  const sync_events = async () => {
-    const events = new Events()
-    const itemid = get_itemid('events')
-    const network = (await fresh_metadata(itemid)).customMetadata
-    const elements = sync.value.querySelector(`[itemid="${itemid}"]`)
-    if (!elements) return
-    const md5 = hash(elements.outerHTML, hash_options)
-    if (!network || network.md5 !== md5) {
-      events.value = await events.sync()
-      if (events.value.length) {
-        await next_tick()
-        await events.save(elements)
-        localStorage.removeItem('/+/events')
-      }
-    }
-  }
-  const sync_anonymous_posters = async () => {
-    const offline_posters = await get('/+/posters/')
-    if (!offline_posters || !offline_posters.items) return
-    await Promise.all(
-      offline_posters.items.map(async created_at => {
-        const poster_string = await get(`/+/posters/${created_at}`)
-        save_poster({
-          id: `${localStorage.me}/posters/${created_at}`,
-          outerHTML: poster_string
-        })
-        await del(`/+/posters/${created_at}`)
-      })
-    )
-    await del('/+/posters/')
-  }
-  const save_poster = async poster => {
-    props.poster.value = get_item(poster.outerHTML)
-    props.poster.id = poster.id
-    await next_tick()
-    const new_poster = new Poster(poster.value.id)
-    await new_poster.save()
-    await del(`${localStorage.me}/posters/`)
-    poster.value = null
-  }
-
-  watchEffect(() => {
-    if (current_user.value && navigator.onLine) {
-      localStorage.me = from_e64(current_user.value.phoneNumber)
-      window.addEventListener('online', play)
-      play()
-    } else window.removeEventListener('online', play)
-  })
-  watchEffect(async () => {
-    if (props.statement) {
-      await next_tick()
-      const id = get_itemid('statements')
-      statements.value = await list(id)
-      statements.value.push(props.statement)
-      const data = new Statements()
-      await next_tick()
-      await data.save()
-      emit('update:statement', null)
-    }
-  })
-  watchEffect(async () => {
-    if (props.person) {
-      await next_tick()
-      const me = new Me()
-      await me.save()
-    }
-  })
   document.addEventListener('visibilitychange', visibility_change)
 </script>
--- src/persistance/Cloud.sync.js
+++ src/persistance/Cloud.sync.js
@@ -3,22 +3,24 @@ import 'firebase/compat/auth'
 import 'firebase/compat/storage'
 import hash from 'object-hash'
 import { get, del, set, keys } from 'idb-keyval'
-import { as_filename, as_author, list } from '@/use/itemid'
-import { Offline } from '@/persistance/Storage'
+import { as_filename, as_author, list, load } from '@/use/itemid'
+import { Offline, Statements, Events, Poster, Me } from '@/persistance/Storage'
+import { from_e64 } from '@/use/profile'
+import get_item from '@/use/item'
+import { ref, watchEffect as watch_effect, nextTick as next_tick } from 'vue'
+import { current_user } from '@/use/serverless'
 
 export const three_minutes = 180000
 export const five_minutes = 300000
 export const one_hour = 3600000
+export const eight_hours = one_hour * 8
 export const timeouts = []
 export const hash_options = { encoding: 'base64', algorithm: 'md5' }
 export const does_not_exist = { updated: null, customMetadata: { md5: null } } // Explicitly setting null to indicate that this file doesn't exist
 
 export async function sync_later(id, action) {
   const offline = (await get('sync:offline')) || []
-  offline.push({
-    id,
-    action
-  })
+  offline.push({ id, action })
   await set('sync:offline', offline)
 }
 export async function is_stranger(id) {
@@ -86,4 +88,153 @@ export async function fresh_metadata(itemid) {
 }
 export function visit_interval() {
   return Date.now() - one_hour
+}
+export const get_itemid = type => {
+  if (type) return `${localStorage.me}/${type}`
+  else return localStorage.me
+}
+
+export const use = (props, emit) => {
+  const container = ref(null)
+  const poster = ref(null)
+  const statements = ref([])
+  const events = ref(null)
+
+  const play = async () => {
+    console.log('play')
+    const me = await load(localStorage.me) // check if new user
+    if (!me || current_user.value === null) return null // let's wait to sync
+    await sync_offline_actions()
+    let synced
+    if (localStorage.sync_time) {
+      synced = Date.now() - new Date(localStorage.sync_time).getTime()
+    } else synced = eight_hours
+    const time_left = eight_hours - synced
+    if (time_left <= 0) {
+      // setTimeout(async () => {
+      emit('active', true)
+      await prune()
+      await sync_me()
+      await sync_statements()
+      await sync_events()
+      await sync_anonymous_posters()
+      await sync_happened()
+      emit('active', false)
+      // }, 1000)
+    }
+  }
+  const sync_happened = async () => {
+    const statements = new Statements()
+    await statements.optimize()
+    localStorage.sync_time = new Date().toISOString()
+    const me = await load(localStorage.me)
+    if (!me) return // new user let's wait
+    if (!me.visited) me.visited = null
+    const visit_gap = Date.now() - new Date(me.visited).getTime()
+    if (me && visit_gap > one_hour) {
+      me.visited = new Date().toISOString()
+      emit('update:person', me)
+    }
+  }
+  const sync_me = async () => {
+    const id = get_itemid()
+    const network = (await fresh_metadata(id)).customMetadata
+    let my_info = localStorage.getItem(id)
+    if (!my_info) my_info = await get(id)
+    if (!my_info || !network) return
+    const md5 = hash(my_info, hash_options)
+    if (md5 !== network.md5) {
+      localStorage.removeItem(id)
+      del(id)
+    }
+  }
+  const sync_statements = async () => {
+    const statements = new Statements()
+    const itemid = get_itemid('statements')
+    const network = (await fresh_metadata(itemid)).customMetadata
+    const elements = sync.value.querySelector(`[itemid="${itemid}"]`)
+    if (!elements || !elements.outerHTML) return null // nothing local so we'll let it load on request
+    const md5 = hash(elements.outerHTML, hash_options)
+    if (!network || network.md5 !== md5) {
+      statements.value = await statements.sync()
+      if (statements.value.length) {
+        await next_tick()
+        await statements.save(elements)
+        localStorage.removeItem('/+/statements')
+      }
+    }
+  }
+  const sync_events = async () => {
+    const events = new Events()
+    const itemid = get_itemid('events')
+    const network = (await fresh_metadata(itemid)).customMetadata
+    const elements = sync.value.querySelector(`[itemid="${itemid}"]`)
+    if (!elements) return
+    const md5 = hash(elements.outerHTML, hash_options)
+    if (!network || network.md5 !== md5) {
+      events.value = await events.sync()
+      if (events.value.length) {
+        await next_tick()
+        await events.save(elements)
+        localStorage.removeItem('/+/events')
+      }
+    }
+  }
+  const sync_anonymous_posters = async () => {
+    const offline_posters = await get('/+/posters/')
+    if (!offline_posters || !offline_posters.items) return
+    await Promise.all(
+      offline_posters.items.map(async created_at => {
+        const poster_string = await get(`/+/posters/${created_at}`)
+        save_poster({
+          id: `${localStorage.me}/posters/${created_at}`,
+          outerHTML: poster_string
+        })
+        await del(`/+/posters/${created_at}`)
+      })
+    )
+    await del('/+/posters/')
+  }
+  const save_poster = async poster => {
+    props.poster.value = get_item(poster.outerHTML)
+    props.poster.id = poster.id
+    await next_tick()
+    const new_poster = new Poster(poster.value.id)
+    await new_poster.save()
+    await del(`${localStorage.me}/posters/`)
+    poster.value = null
+  }
+  watch_effect(() => {
+    if (current_user.value && navigator.onLine) {
+      localStorage.me = from_e64(current_user.value.phoneNumber)
+      window.addEventListener('online', play)
+      play()
+    } else window.removeEventListener('online', play)
+  })
+  watch_effect(async () => {
+    if (props.statement) {
+      await next_tick()
+      const id = get_itemid('statements')
+      statements.value = await list(id)
+      statements.value.push(props.statement)
+      const data = new Statements()
+      await next_tick()
+      await data.save()
+      emit('update:statement', null)
+    }
+  })
+  watch_effect(async () => {
+    if (props.person) {
+      await next_tick()
+      const me = new Me()
+      await me.save()
+    }
+  })
+  return {
+    play,
+    container,
+    events,
+    statements,
+    poster
+  }
 }
