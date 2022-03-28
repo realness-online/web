14248dc7bc0c4da1f114d05703b9acff8884aa37
Scott Fryxelo <sfryxell@me.com>
2022-03-28 15:55:55 -0700

    Add a linear gradient component that will consume a set of stops

---
+++ src/components/posters/linear-gradient.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import is_stop from '@/use/gradient'
+
+  const props = defineProps({
+    stops: {
+      required:true,
+      type: Object,
+      validator: is_stops
+    }
+  })
+</script>
+<template>
+  <linearGradient id="gradient">
+   <stop v-for="stop in stops" offset="0%" />
+   <stop offset="100%" />
+ </linearGradient>
+</template>
+<style lang="stylus"></style>
