86d5fc560e1a62f7b4f6f3a7135cddc674889868
Scott Fryxelo <sfryxell@me.com>
2022-04-05 18:41:53 -0700

    Some kind of color

--- src/components/posters/as-effects.vue
+++ src/components/posters/as-effects.vue
@@ -1,41 +1,41 @@
 <template>
   <defs itemprop="effects">
-    <g class="background effect">
-      <filter :id="query('background-filter')">
-        <feColorMatrix
-          in="SourceGraphic"
-          type="matrix"
-          values="1 0 0 0 0
-                  0 1 0 0 0
-                  0 0 1 0 0
-                  0 0 0 1 0" />
-      </filter>
+    <g class="gradients">
       <linearGradient
-        :id="query('background-gradient')"
+        :id="query('height-gradient')"
         gradientUnits="userSpaceOnUse"
         x1="0%"
         x2="0"
         y1="0"
         y2="100%">
         <stop
-          v-for="stop in background"
-          :stop-color="stop.color"
+          v-for="stop in height"
+          :stop-color="stop.color.hsl"
           :offset="`${stop.percentage}%`" />
       </linearGradient>
-    </g>
-    <g class="light effect">
       <linearGradient
-        :id="query('light-gradient')"
+        :id="query('width-gradient')"
         gradientUnits="userSpaceOnUse"
         x1="0"
         x2="100%"
         y1="0"
         y2="0">
         <stop
-          v-for="stop in light"
-          :stop-color="stop.color"
+          v-for="stop in width"
+          :stop-color="stop.color.hsl"
           :offset="`${stop.percentage}%`" />
       </linearGradient>
+    </g>
+    <g class="filters">
+      <filter :id="query('background-filter')">
+        <feColorMatrix
+          in="SourceGraphic"
+          type="matrix"
+          values="1 0 0 0.5 0
+                  0 1 0 0.5 0
+                  0 0 1 0.5 0
+                  0 0 0 1 0" />
+      </filter>
       <filter :id="query('light-filter')">
         <feColorMatrix
           in="SourceGraphic"
@@ -45,81 +45,31 @@
                   0 0 1 0 0
                   0 0 0 1 0" />
       </filter>
-    </g>
-    <g class="regular effect">
-      <linearGradient
-        :id="query('regular-gradient')"
-        gradientUnits="userSpaceOnUse"
-        x1="0"
-        x2="100%"
-        y1="0"
-        y2="0">
-        <stop
-          v-for="stop in regular"
-          :stop-color="stop.color"
-          :offset="`${stop.percentage}%`" />
-      </linearGradient>
       <filter :id="query('regular-filter')">
-        <feTurbulence
-          type="turbulence"
-          baseFrequency="0.05"
-          numOctaves="2"
-          result="turbulence" />
-        <feDisplacementMap
-          in2="turbulence"
-          in="SourceGraphic"
-          scale="50"
-          xChannelSelector="R"
-          yChannelSelector="G" />
         <feColorMatrix
           in="SourceGraphic"
           type="matrix"
-          values="1 0 0 0 0
-                  0 1 0 0 0
-                  0 0 1 0 0
-                  0 0 0 1 0" />
+          values="1 0 0 -0.5 0
+                  0 1 0 -0.5 0
+                  0 0 1 -0.5 0
+                  0 0 0 1.5 0" />
       </filter>
-    </g>
-    <g class="bold effect">
-      <linearGradient
-        :id="query('bold-gradient')"
-        gradientUnits="userSpaceOnUse"
-        x1="0"
-        x2="100%"
-        y1="0"
-        y2="0">
-        <stop
-          v-for="stop in bold"
-          :stop-color="stop.color"
-          :stop-opacity="stop.opacity"
-          :offset="`${stop.percentage}%`" />
-      </linearGradient>
       <filter :id="query('bold-filter')">
-        <feTurbulence
-          type="turbulence"
-          baseFrequency="0.05"
-          numOctaves="2"
-          result="turbulence" />
-        <feDisplacementMap
-          in2="turbulence"
-          in="SourceGraphic"
-          scale="50"
-          xChannelSelector="R"
-          yChannelSelector="G" />
         <feColorMatrix
           in="SourceGraphic"
           type="matrix"
-          values="1 0 0 0.75 0
-                  0 1 0 0 0
-                  0 0 1 0 0
-                  0 0 0 1 0" />
+          values="1 0 0 -0.99 0
+                  0 1 0 -0.99 0
+                  0 0 1 -0.99 0
+                  0 0 0  3 0" />
       </filter>
     </g>
   </defs>
 </template>
 <script setup>
-  import { ref } from 'vue'
+  import { ref, watchEffect as watch_effect } from 'vue'
   import { is_vector, use_poster } from '@/use/vector'
+  import { use as use_vectorize } from '@/use/vectorize'
   const props = defineProps({
     vector: {
       type: Object,
@@ -128,8 +78,12 @@
     }
   })
   const { query } = use_poster({ poster: props.vector })
-  const background = ref(props.vector.effects?.background)
-  const light = ref(props.vector.effects?.light)
-  const regular = ref(props.vector.effects?.regular)
-  const bold = ref(props.vector.effects?.bold)
+  const { new_gradients: gradients } = use_vectorize()
+  const width = ref([])
+  const height = ref([])
+  watch_effect(() => {
+    if (!gradients.value) return
+    width.value = gradients.value.width
+    height.value = gradients.value.height
+  })
 </script>
