ca6dbcf99a42dd3b5b331a7b64951f7d605a027d
Scott Fryxelo <sfryxell@me.com>
2022-04-11 19:55:44 -0700

    Optimized vector every time it's loaded into the editor

--- src/use/optimize.js
+++ src/use/optimize.js
@@ -1,26 +1,27 @@
-import { ref, onMounted as mounted, onUnmounted as dismount } from 'vue'
+import {
+  ref,
+  onMounted as mounted,
+  onUnmounted as dismount,
+  nextTick as next_tick
+} from 'vue'
 import get_item from '@/use/item'
-
-export const use = () => {
-  const vector = ref(null)
+import { as_query_id } from '@/use/itemid'
+export const use = vector => {
   const optimizer = ref(null)
-  const optimize = vector => {
-    console.log('calling optmize', vector)
-    optimizer.value.postMessage({ vector })
+  const optimize = async () => {
+    await next_tick()
+    optimizer.value = new Worker('/optimize.worker.js')
+    optimizer.value.addEventListener('message', optimized)
+    const element = document.getElementById(as_query_id(vector.value.id))
+    optimizer.value.postMessage({ vector: element.outerHTML })
   }
   const optimized = message => {
     const optimized = get_item(message.data.vector)
     vector.value = optimized
-  }
-  mounted(() => {
-    console.log('mount optimizer')
-    optimizer.value = new Worker('/optimize.worker.js')
-    optimizer.value.addEventListener('message', optimized)
-  })
-  dismount(() => {
+    vector.value.optimized = true
+    optimizer.value.removeEventListener('message', optimized)
     optimizer.value.terminate()
-    console.log('terminate optimizer')
-  })
+  }
   return {
     optimize,
     vector
