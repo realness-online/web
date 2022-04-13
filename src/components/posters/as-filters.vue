b8bb948e114aa517315c1db3bc02da33fbc35002
Scott Fryxell <sfryxell@me.com>
2022-04-13 12:08:52 -0700

    Setup filters for each layer

--- src/components/posters/as-filters.vue
+++ src/components/posters/as-filters.vue
@@ -1,14 +1,34 @@
 <template>
   <defs>
+    <filter id="light-filter">
+      <feColorMatrix
+        in="SourceGraphic"
+        type="matrix"
+        values="1 0 0 0 0
+                0 1 0 0 0
+                0 0 1 0 0
+                0 0 0 1 0" />
+    </filter>
+    <filter id="regular-filter">
+      <feColorMatrix
+        in="SourceGraphic"
+        type="matrix"
+        values="1 0 0 0 0
+                0 1 0 0 0
+                0 0 1 0 0
+                0 0 0 1 0" />
+    </filter>
+
     <filter id="bold-filter">
       <feColorMatrix
         in="SourceGraphic"
         type="matrix"
-        values="1 0 0 0.00 0
-                0 1 0 0.00 0
-                0 0 1 0.00 0
-                0 0 0 2.75 0" />
+        values="1 0 0 0 0
+                0 1 0 0 0
+                0 0 1 0 0
+                0 0 0 3 0" />
     </filter>
+
     <filter id="emboss">
       <feConvolveMatrix
         kernelMatrix="3 0 0
