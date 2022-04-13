41889d7d11280a67e37b6161bdb15dce68f2c24f
Scott Fryxell <sfryxell@me.com>
2022-04-13 12:18:17 -0700

    Remove setting and deleting directories locally.

--- src/persistance/Large.js
+++ src/persistance/Large.js
@@ -5,30 +5,31 @@ const Large = superclass =>
   class extends superclass {
     async save(items = document.querySelector(`[itemid="${this.id}"]`)) {
       if (!items) return
-      const exist = await get(this.id)
+      // const exist = await get(this.id)
       await set(this.id, items.outerHTML)
-      if (!exist) {
-        const path = as_directory_id(this.id)
-        let directory = await get(path)
-        if (directory && directory.items)
-          directory.items.push(as_path_parts(this.id)[2])
-        else directory = { items: [as_path_parts(this.id)[2]] }
-        if (items) {
-          await set(path, directory)
-        }
-      }
+      // if (!exist) {
+      //   const path = as_directory_id(this.id)
+      //   let directory = await get(path)
+      //   if (directory && directory.items)
+      //     directory.items.push(as_path_parts(this.id)[2])
+      //   else directory = { items: [as_path_parts(this.id)[2]] }
+      //   if (items) {
+      //     await set(path, directory)
+      //   }
+      // }
       if (super.save) await super.save(items)
     }
     async delete() {
       const path = as_directory_id(this.id)
-      const directory = await get(path)
+      // const directory = await get(path)
       await del(this.id)
-      if (directory.items) {
-        directory.items = directory.items.filter(
-          id => parseInt(id) !== as_created_at(this.id)
-        )
-        await set(path, directory)
-      }
+      await del(path)
+      // if (directory.items) {
+      //   directory.items = directory.items.filter(
+      //     id => parseInt(id) !== as_created_at(this.id)
+      //   )
+      //   await set(path, directory)
+      // }
       if (super.delete) await super.delete()
     }
   }
