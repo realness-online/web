3c6e7d573190673597dae9038427352019e633f0
Scott Fryxell <sfryxell@me.com>
2022-04-15 14:28:22 -0700

    Move is_path to use/path, and use type narrowing in regular as javascript

--- src/use/path.js
+++ src/use/path.js
@@ -1,8 +1,14 @@
 import { useActiveElement as active_element } from '@vueuse/core'
-import { ref } from 'vue'
+import { ref, onMounted as mounted } from 'vue'
 import { change } from '@/use/opacity'
 import { luminosity } from '@/use/colors'
 export const svg_ns = 'http://www.w3.org/2000/svg'
+
+export const is_path = path => {
+  if (typeof path != 'object') return false
+  if (path instanceof SVGPathElement) return true
+  else return false
+}
 
 export const itemprop_query = name => {
   return document.querySelector(`[itemprop="${name}"]`)
@@ -39,15 +45,22 @@ export const use = () => {
   }
 
   const get_active_path = () => {
-    const id = active_element().value.href.baseVal.slice(1)
-    const symbol = document.getElementById(id)
-    const path = symbol.firstChild
-    console.log('howdy', path)
+    let path = active_element().value
+    if (!is_path(path)) {
+      const id = active_element().value.href.baseVal.slice(1)
+      const symbol = document.getElementById(id)
+      path = symbol.firstChild
+    }
+
     selected_path.value = path.getAttribute('itemprop')
     opacity_percentage.value = path.style.fillOpacity
     return path
   }
 
+  mounted(() => {
+    selected_path.value = null
+    opacity_percentage.value = null
+  })
   return {
     fill_opacity,
     opacity_percentage,
