53faff691c0b2b7374f8456bd20e96ae3ce96b61
Scott Fryxell <sfryxell@me.com>
2022-05-15 12:00:48 -0700

    improve emboss look

--- src/components/posters/as-emboss.vue
+++ src/components/posters/as-emboss.vue
@@ -9,13 +9,13 @@
     class="emboss regular"
     tabindex="-1"
     :href="fragment('regular')"
-    fill="DimGray"
+    fill="gray"
     filter="url(#emboss)" />
   <use
     class="emboss bold"
     tabindex="-1"
     :href="fragment('bold')"
-    fill="gray"
+    fill="dimgray"
     filter="url(#emboss)" />
 </template>
 <script setup>
@@ -32,12 +32,3 @@
     return `${as_fragment_id(props.itemid)}-${add}`
   }
 </script>
-<style lang="stylus">
-  // svg > use.emboss.regular
-  //   opacity: 0.5
-  //   visibility: hidden
-  //   transition-delay: 0s, 0.5s
-  // svg:focus > use.emboss
-  //   opacity: 1
-  //   visibility: visible;
-</style>
