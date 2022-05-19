15efc356b48beeeb0c45e5b40a4814f627e11e92
Scott Fryxell <sfryxell@me.com>
2022-05-18 20:04:20 -0700

    Go with original code to manage directory

--- src/persistance/Large.js
+++ src/persistance/Large.js
@@ -1,38 +1,36 @@
 // https://developers.caffeina.com/object-composition-patterns-in-javascript-4853898bb9d0
-// import { set, get, del } from 'idb-keyval'
-// import { as_directory_id, as_path_parts, as_created_at } from '@/use/itemid'
-import { set, del } from 'idb-keyval'
-import { as_directory_id } from '@/use/itemid'
+import { set, get, del } from 'idb-keyval'
+import { as_directory_id, as_path_parts, as_created_at } from '@/use/itemid'
 
 const Large = superclass =>
   class extends superclass {
     async save(items = document.querySelector(`[itemid="${this.id}"]`)) {
       if (!items) return
-      // const exist = await get(this.id)
+      const exist = await get(this.id)
       await set(this.id, items.outerHTML)
-      // if (!exist) {
-      //   const path = as_directory_id(this.id)
-      //   let directory = await get(path)
-      //   if (directory && directory.items)
-      //     directory.items.push(as_path_parts(this.id)[2])
-      //   else directory = { items: [as_path_parts(this.id)[2]] }
-      //   if (items) {
-      //     await set(path, directory)
-      //   }
-      // }
+      if (!exist) {
+        const path = as_directory_id(this.id)
+        let directory = await get(path)
+        if (directory && directory.items)
+          directory.items.push(as_path_parts(this.id)[2])
+        else directory = { items: [as_path_parts(this.id)[2]] }
+        if (items) {
+          await set(path, directory)
+        }
+      }
       if (super.save) await super.save(items)
     }
     async delete() {
       const path = as_directory_id(this.id)
-      // const directory = await get(path)
+      const directory = await get(path)
       await del(this.id)
       await del(path)
-      // if (directory.items) {
-      //   directory.items = directory.items.filter(
-      //     id => parseInt(id) !== as_created_at(this.id)
-      //   )
-      //   await set(path, directory)
-      // }
+      if (directory.items) {
+        directory.items = directory.items.filter(
+          id => parseInt(id) !== as_created_at(this.id)
+        )
+        await set(path, directory)
+      }
       if (super.delete) await super.delete()
     }
   }
