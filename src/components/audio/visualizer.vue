1cc7d4fb466844bcaab6e26f5c3df9d49304d20d
Scott Fryxell <sfryxell@me.com>
2022-06-06 07:22:16 -0700

    add Visualizer to editor

---
+++ src/components/audio/visualizer.vue
@@ -0,0 +1,14 @@
+<template></template>
+<script setup>
+  if (navigator.mediaDevices) {
+    const AudioContext = window.AudioContext || window.webkitAudioContext
+    const audio_context = new AudioContext()
+    const stream = await navigator.mediaDevices.getUserMedia ({audio: true, video: false})
+    console.log(stream.getAudioTracks().stop());
+    let analyser = audio_context.createAnalyser()
+    source = audio_context.createMediaStreamSource(stream)
+    source.connect(analyser)
+  }
+
+</script>
+<style lang="stylus"></style>
