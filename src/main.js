91952e32c3ddefc19ad38169ea09eb383d6e2319
Scott Fryxell <sfryxell@me.com>
2022-06-21 12:15:43 -0700

    Check for service worker updates every 8 hours.

--- src/main.js
+++ src/main.js
@@ -1,15 +1,22 @@
 import { createApp } from 'vue'
-import { registerSW } from 'virtual:pwa-register'
+import { registerSW as register_service_worker } from 'virtual:pwa-register'
+import { eight_hours } from '@/use/sync'
 
 import App from './App.vue'
 import router from './router'
 const me = localStorage.me
 if (!me) localStorage.me = '/+'
 
-const update_servie_worker = registerSW({
+const update_servie_worker = register_service_worker({
   onOfflineReady() {
-    console.info('offline-ready', update_servie_worker)
+    console.info('sw-offline-ready', update_servie_worker)
+  },
+  onRegistered(r) {
+    console.info('sw-registered', r)
+    r && setInterval(() => {
+      console.info('sw-interval-check', r)
+      r.update()
+    }, eight_hours)
   }
 })
-
 createApp(App).use(router).mount('body')
