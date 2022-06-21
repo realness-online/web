3665a1f61ad9ad9412030a87417b4b9f7a213ac5
Scott Fryxell <sfryxell@me.com>
2022-06-21 10:13:13 -0700

    Read input from microphone

--- src/components/audio/visualizer.vue
+++ src/components/audio/visualizer.vue
@@ -1,66 +1,37 @@
 <template>
-  <button v-if="can_visualize && !audio_playing" @click="start">
+  <button v-if="can_visualize && !analyzing" @click="start">
     Visualize
   </button>
-  <button v-if="audio_playing" @click="stop">Stop</button>
+  <button v-if="analyzing" @click="stop">Stop</button>
 </template>
 <script setup>
+  import { ref } from 'vue'
+  const analyser = ref(null)
+  const analyzing = ref(false)
   const can_visualize = () => navigator.mediaDevices
-  let audio_context
-  let source_node
-  let analyser_node
-  let javascript_node
-  // let audio_data = null
-  let audio_playing = false
-  let sample_size = 1024 // number of samples to collect before analyzing data
-  let amplitude_array // array to hold time domain data
 
-  const setup_audio = async () => {
-    const audio_context = new AudioContext()
-    const stream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: false
-    })
-    const analyser = audio_context.createAnalyser()
-    const source = audio_context.createMediaStreamSource(stream)
-    source.connect(analyser)
-    console.log(source, analyser)
-
-    source_node = audio_context.createBufferSource()
-    analyser_node = audio_context.createAnalyser()
-    javascript_node = audio_context.createScriptProcessor(sample_size, 1, 1)
-    // Create the array for the data values
-    amplitude_array = new Uint8Array(analyser_node.frequencyBinCount)
-    // Now connect the nodes together
-    source_node.connect(audio_context.destination)
-    source_node.connect(analyser_node)
-    analyser_node.connect(javascript_node)
-    javascript_node.connect(audio_context.destination)
-  }
-  const draw_time_domain = () => {
-    // console.log(draw_time_domain)
-    // for (let i = 0; i < amplitude_array.length; i++) {
-    //   let value = amplitude_array[i] / 256
-    //   // let y = canvasHeight - canvasHeight * value - 1
-    //   // this is where you tweak the svg params
-    //   // canvas.fillStyle = '#ffffff'
-    //   // canvas.fillRect(i, y, 1, 1)
-    // }
-  }
   const start = async () => {
-    if (!audio_context) audio_context = new AudioContext()
-    await setup_audio()
-
-    audio_playing = true
-    console.log('audio_playing', audio_playing)
-    javascript_node.onaudioprocess = () => {
-      analyser_node.getByteTimeDomainData(amplitude_array)
-      if (audio_playing === true) requestAnimationFrame(draw_time_domain)
-    }
+    const context = new AudioContext()
+    analyser.value = context.createAnalyser()
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+    const input = context.createMediaStreamSource(stream)
+    input.connect(analyser.value)
+    analyzing.value = true
+    requestAnimationFrame(visualize)
   }
-  const stop = () => {
-    source_node.stop(0)
-    audio_playing = false
+
+  const stop = () => analyzing.value = false
+  const visualize = () => {
+    const times = new Uint8Array(analyser.value.frequencyBinCount)
+    analyser.value.getByteTimeDomainData(times)
+    console.log(times.length)
+    for (var i = 0; i < times.length; i++) {
+      const value = times[i]
+      const percent = value / 256
+      console.log(value, percent)
+    }
+    if (analyzing.value) requestAnimationFrame(visualize)
   }
 </script>
+
 <style lang="stylus"></style>
