f42724d2a4f77add6d3a345e8569afc58ef40ba0
Scott Fryxell <sfryxell@me.com>
2022-09-17 12:42:33 -0700

    Turn secret emboss feature back on

--- src/components/posters/as-emboss.vue
+++ src/components/posters/as-emboss.vue
@@ -1,22 +1,42 @@
 <template>
+  <defs>
+    <filter id="emboss-light">
+      <feConvolveMatrix
+        kernelMatrix="2 0 0
+                      0 0 0
+                      0 0 -2"></feConvolveMatrix>
+    </filter>
+    <filter id="emboss-regular">
+      <feConvolveMatrix
+        kernelMatrix="4 0 0
+                      0 0 0
+                      0 0 -4"></feConvolveMatrix>
+    </filter>
+    <filter id="emboss-bold">
+      <feConvolveMatrix
+        kernelMatrix="19 0 0
+                      0 0 0
+                      0 0 -19"></feConvolveMatrix>
+    </filter>
+  </defs>
   <use
     class="emboss light"
     tabindex="-1"
     :href="fragment('light')"
     fill="gray"
-    filter="url(#emboss)" />
+    filter="url(#emboss-light)" />
   <use
     class="emboss regular"
     tabindex="-1"
     :href="fragment('regular')"
     fill="gray"
-    filter="url(#emboss)" />
+    filter="url(#emboss-regular)" />
   <use
     class="emboss bold"
     tabindex="-1"
     :href="fragment('bold')"
-    fill="dimgray"
-    filter="url(#emboss)" />
+    fill="white"
+    filter="url(#emboss-bold)" />
 </template>
 <script setup>
   import { as_fragment_id } from '@/use/itemid'
@@ -29,6 +49,6 @@
     }
   })
   const fragment = add => {
-    return `${as_fragment_id(props.vector.value.id)}-${add}`
+    return `${as_fragment_id(props.vector.id)}-${add}`
   }
 </script>
