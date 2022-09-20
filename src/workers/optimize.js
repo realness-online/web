2121f6bd5f105e806c14bca3b007850bb1e2c0ed
Scott Fryxell <sfryxell@me.com>
2022-09-20 12:53:34 -0700

    Optimize supports adobe animate

--- src/workers/optimize.js
+++ src/workers/optimize.js
@@ -1,31 +1,39 @@
-import SVGO from 'svgo'
+import { optimize } from 'svgo/dist/svgo.browser.js'
 const options = {
   multipass: true,
   full: true,
+  js2svg: {
+    indent: 2,
+    pretty: true
+  },
   plugins: [
     {
-      convertPathData: {
-        floatPrecision: 0,
-        transformPrecision: 0,
-        makeArcs: {
-          threshold: 0.5, // coefficient of rounding error
-          tolerance: 25.0 // percentage of radius
+      name: 'preset-default',
+      params: {
+        overrides: {
+          removeUnknownsAndDefaults: false,
+          cleanupAttrs: true,
+          cleanupNumericValues: true,
+          removeViewBox: false,
+          removeEmptyAttrs: false,
+          removeUselessStrokeAndFill: true,
+          mergePaths: false,
+          convertPathData: {
+            floatPrecision: 0,
+            transformPrecision: 0,
+            makeArcs: {
+              threshold: 0.5, // coefficient of rounding error
+              tolerance: 25.0 // percentage of radius
+            }
+          }
         }
       }
-    },
-    { cleanupNumericValues: true },
-    { cleanupAttrs: true },
-    { removeEmptyAttrs: false },
-    { removeViewBox: false },
-    { removeUselessStrokeAndFill: true },
-    { sortAttrs: false },
-    { mergePaths: false }
+    }
   ]
 }
 export async function listen(message) {
-  const svgo = new SVGO(options)
   console.info('before', `${to_kb(message.data.vector)}kb`)
-  const optimized = await svgo.optimize(message.data.vector)
+  const optimized = optimize(message.data.vector, options)
   console.info('after', `${to_kb(optimized.data)}kb`)
   self.postMessage({ vector: optimized.data })
 }
