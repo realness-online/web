dce2ef142b26d189bc6f324e99982b9c2283a142
Scott Fryxell <sfryxell@me.com>
2022-12-02 16:24:10 -0800

    fine tune gradient animation

--- src/components/animations/gradient-transform.vue
+++ src/components/animations/gradient-transform.vue
@@ -1,7 +1,7 @@
 <template>
   <animate
     attributeName="gradientTransform"
-    dur="12s"
-    values="rotate(-90); rotate(0); rotate(56); rotate(0);"
+    dur="5s"
+    values="rotate(-90); rotate(0); rotate(56); rotate(0); rotate(-10); rotate(90); rotate(0);"
     repeatCount="indefinite" />
 </template>
