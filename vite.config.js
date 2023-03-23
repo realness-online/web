300f616ff25c8d6ed911cdafd8c357a07fea7e87
Scott Fryxell <sfryxell@me.com>
2023-03-22 21:44:23 -0700

    setting to fullscreen

--- vite.config.js
+++ vite.config.js
@@ -49,7 +49,7 @@ export default defineConfig({
         'icons.svg'
       ],
       manifest: {
-        display: 'standalone',
+        display: 'fullscreen',
         background_color: '#151518',
         name: 'Realness',
         short_name: 'Realness',
