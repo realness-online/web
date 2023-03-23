5eaff543d136f49f8cd29962b108012760b31ca4
Scott Fryxell <sfryxell@me.com>
2023-03-23 10:35:10 -0700

    fine tune opacity and embossing

--- src/components/posters/as-emboss.vue
+++ src/components/posters/as-emboss.vue
@@ -1,8 +1,5 @@
 <template>
   <defs>
-    <symbol :id="query('emboss')">
-      <use :href="fragment('light')" filter="url(#emboss)" />
-    </symbol>
     <filter id="emboss">
       <feConvolveMatrix
         kernelMatrix="2 0 0
@@ -10,7 +7,7 @@
                       0 0 -2" />
     </filter>
   </defs>
-  <use tabindex="-1" class="emboss" opacity="0.80" :href="fragment('emboss')" />
+  <use tabindex="-1" class="emboss" opacity=".66"  :href="fragment('light')" filter="url(#emboss)" />
 </template>
 <script setup>
   import { as_fragment_id, as_query_id } from '@/use/itemid'
