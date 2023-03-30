e5e7e121103f36c79bb50002705d95d53c0d88bb
Scott Fryxell <sfryxell@me.com>
2023-03-30 15:18:01 -0700

    Flatten file structure

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@/': new URL('./src/', import.meta.url).pathname,
+      '@/': new URL('./', import.meta.url).pathname,
       '@@/': new URL('./test/', import.meta.url).pathname
     },
     extensions: ['.js', '.json', '.vue']
@@ -28,9 +28,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       stylus: {
-        imports: [
-          new URL('./src/style/variables.styl', import.meta.url).pathname
-        ]
+        imports: [new URL('./style/variables.styl', import.meta.url).pathname]
       }
     }
   },
