86edeb7014df9c527bf6fd6c63519fe54da5f3fa
Scott Fryxell <sfryxell@me.com>
2023-07-19 10:41:10 -0700

    fine tuning emboss

--- components/posters/as-emboss.vue
+++ components/posters/as-emboss.vue
@@ -12,20 +12,20 @@
 
     <filter id="emboss">
       <feConvolveMatrix
-        kernelMatrix="1 0 0
+        kernelMatrix="1.5 0 0
                       0 0 0
-                      0 0 -1" />
+                      0 0 -1.5" />
     </filter>
     <filter id="emboss-opposite">
       <feConvolveMatrix
-        kernelMatrix="0 0 2
+        kernelMatrix="0 0 2.5
                       0 0 0
-                     -2 0 0" />
+                     -2.5 0 0" />
     </filter>
     <filter id="emboss-straight">
       <feConvolveMatrix
         kernelMatrix="0 0  0
-                      2 0 -2
+                      2.5 0 -2.5
                       0 0  0" />
     </filter>
   </defs>
