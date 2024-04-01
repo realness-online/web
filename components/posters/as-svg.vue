4c8bc45db04dbd7cc34330b085a05c5d199aa4ce
Scott Fryxell <sfryxell@me.com>
2024-04-01 13:16:01 -0700

    refactor: Semove setting in favor of performant on all devices

--- components/posters/as-svg.vue
+++ components/posters/as-svg.vue
@@ -157,7 +157,7 @@
   const trigger = ref(null)
   const show_emboss = computed(() => localStorage.emboss && intersecting.value)
   const animate = computed(() => localStorage.animate && intersecting.value)
-  const mask = computed(() => localStorage.mask && intersecting.value)
+  const mask = computed(() => intersecting.value)
   const new_poster = inject('new-poster', false)
   if (new_poster) {
     const { new_vector } = use_vectorize()
