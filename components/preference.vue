3c92b0f0f2a1263f2c71055c9fb877a0de7aad52
Scott Fryxell <sfryxell@me.com>
2024-05-30 12:41:02 -0700

    Map out the keyboard

--- components/preference.vue
+++ components/preference.vue
@@ -81,3 +81,40 @@
             -webkit-transition: .4s
             transition: .4s
 </style>
+<!-- ********************** left pad  ************************** -->
+<!-- scroll wheel -->
+<!-- "MouseUp" -->
+<!-- "MouseRight" -->
+<!-- "MouseLeft" -->
+<!-- "MouseDown" -->
+
+<!-- function row  -->
+<!-- ["Escape", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12"] -->
+
+<!-- digit row -->
+<!-- ["Backquote", "Digit1", "Digit2", "Digit3", "Digit4", "Digit5", "Digit6", "Digit7", "Digit8", "Digit9", "Digit0", "Minus", "Equal", "Backspace"] -->
+
+<!-- 1st row! -->
+<!-- ["Tab", "KeyQ", "KeyW", "KeyE", "KeyR", "KeyT", "KeyY", "KeyU", "KeyI", "KeyO","KeyP", "BracketLeft", "BracketRight", "Backslash"] -->
+
+<!-- home row! -->
+<!-- ["CapsLock", "KeyA", "KeyS", "KeyD", "KeyF", "KeyG", "KeyH", "KeyJ", "KeyK", "KeyL", "Semicolon", "Quote", "Enter"] -->
+
+<!-- shift row -->
+<!-- ["ShiftLeft", "KeyZ", "KeyX", "KeyC", "KeyV", "KeyB", "KeyN" "KeyM", "Comma", "Period", "Slash", "ShiftRight"] -->
+
+<!-- ********************** bottom row ************************** -->
+
+<!-- ["ControlLeft", "AltLeft", "MetaLeft", "Space", "MetaRight", "AltRight", "Fn", "ControlRight"] -->
+
+<!-- ********************** Right pad ************************** -->
+<!-- ["Insert" ,"Home", "PageUp"] -->
+<!-- ["Delete", "End", "PageDown"]  -->
+<!-- ["ArrowUp", "ArrowLeft", "ArrowDown", "ArrowRight"] -->
+
+<!-- ********************** numpad ************************** -->
+<!-- ["NumLock", "NumpadDivide", "NumpadMultiply", "NumpadSubtract"] -->
+<!-- ["Numpad7", "Numpad8", "Numpad9"] -->
+<!-- ["Numpad4", "Numpad5", "Numpad6", "NumpadAdd"] -->
+<!-- ["Numpad1", "Numpad2", "Numpad3"] -->
+<!-- ["Numpad0", "NumpadDecimal", "NumpadEnter"] -->
