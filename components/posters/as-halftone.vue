f6dfa3af7868297ffc971f922f3cd7c9b0640802
Scott Fryxell <1165347+scott-fryxell@users.noreply.github.com>
2024-12-15 11:14:21 -0800

    feature: Beginings of a halftone feature

---
+++ components/posters/as-halftone.vue
@@ -0,0 +1,10 @@
+<template></template>
+<style>
+  svg[itemtype='/posters'] {
+    background:
+      radial-gradient(closest-side, #777, #fff) 0/ 1em 1em space,
+      linear-gradient(91deg, #888, #fff);
+    filter: contrast(16);
+    background-blend-mode: multiply;
+  }
+</style>
