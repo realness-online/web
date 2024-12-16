4a7cc7257fce423e0b61c1cb459a5ad9756a4129
Scott Fryxell <1165347+scott-fryxell@users.noreply.github.com>
2024-12-16 12:05:35 -0800

    refactor: Group the emboss work together

--- components/posters/as-svg.vue
+++ components/posters/as-svg.vue
@@ -12,121 +12,6 @@
     :tabindex="focusable"
     :class="{ animate, landscape }"
     @click="click">
-    <defs v-if="emboss" class="emboss">
-      <defs class="static filters">
-        <filter id="emboss">
-          <feConvolveMatrix
-            kernelMatrix="1.5 0 0
-                          0   0 0
-                          0   0 -1.5" />
-        </filter>
-        <filter id="emboss-opposite">
-          <feConvolveMatrix
-            kernelMatrix="0   0 1.5
-                          0   0 0
-                         -1.5 0 0" />
-        </filter>
-        <filter id="emboss-horizontal">
-          <feConvolveMatrix
-            kernelMatrix="0   0 0
-                          1.5 0 -1.5
-                          0   0 0" />
-        </filter>
-        <filter id="emboss-vertical">
-          <feConvolveMatrix
-            kernelMatrix="0    1.5 0
-                          0    0   0
-                          0   -1.5 0" />
-        </filter>
-      </defs>
-      <filter
-        :id="query('composite')"
-        color-interpolation-filters="sRGB"
-        y="0"
-        x="0"
-        width="100%"
-        height="100%">
-        <feImage :href="fragment('emboss-render-light')" result="light" />
-        <feImage :href="fragment('emboss-render-regular')" result="regular" />
-        <feImage :href="fragment('emboss-render-medium')" result="medium" />
-        <feImage :href="fragment('emboss-render-bold')" result="bold" />
-        <feImage :href="fragment('pattern-render')" result="framing" />
-        <feMerge>
-          <feMergeNode in="framing" />
-          <feMergeNode in="bold" />
-          <feMergeNode in="medium" />
-          <feMergeNode in="regular" />
-          <feMergeNode in="light" />
-        </feMerge>
-      </filter>
-
-      <pattern
-        :id="query('pattern-emboss-light')"
-        :width="vector.width"
-        :height="vector.height"
-        :viewBox="vector.viewbox"
-        patternUnits="userSpaceOnUse"
-        :preserveAspectRatio="aspect_ratio">
-        <use :href="fragment('light')" filter="url(#emboss)" opacity="0.45" />
-      </pattern>
-      <pattern
-        :id="query('pattern-emboss-regular')"
-        :width="vector.width"
-        :height="vector.height"
-        :viewBox="vector.viewbox"
-        patternUnits="userSpaceOnUse"
-        :preserveAspectRatio="aspect_ratio">
-        <use :href="fragment('regular')" filter="url(#emboss-vertical)" />
-      </pattern>
-      <pattern
-        :id="query('pattern-emboss-medium')"
-        :width="vector.width"
-        :height="vector.height"
-        :viewBox="vector.viewbox"
-        patternUnits="userSpaceOnUse"
-        :preserveAspectRatio="aspect_ratio">
-        <use :href="fragment('medium')" filter="url(#emboss-opposite)" />
-      </pattern>
-      <pattern
-        :id="query('pattern-emboss-bold')"
-        :width="vector.width"
-        :height="vector.height"
-        :viewBox="vector.viewbox"
-        patternUnits="userSpaceOnUse"
-        :preserveAspectRatio="aspect_ratio">
-        <use :href="fragment('bold')" filter="url(#emboss-horizontal)" />
-      </pattern>
-      <rect
-        :id="query('emboss-render')"
-        :fill="`url(${fragment('pattern-emboss')})`"
-        width="100%"
-        height="100%" />
-      <rect
-        :id="query('emboss-render-light')"
-        :fill="`url(${fragment('pattern-emboss-light')})`"
-        width="100%"
-        height="100%" />
-      <rect
-        :id="query('emboss-render-regular')"
-        :fill="`url(${fragment('pattern-emboss-regular')})`"
-        width="100%"
-        height="100%" />
-      <rect
-        :id="query('emboss-render-medium')"
-        :fill="`url(${fragment('pattern-emboss-medium')})`"
-        width="100%"
-        height="100%" />
-      <rect
-        :id="query('emboss-render-bold')"
-        :fill="`url(${fragment('pattern-emboss-bold')})`"
-        width="100%"
-        height="100%" />
-      <rect
-        id="lightbar-rect"
-        fill="url(#lightbar)"
-        width="100%"
-        height="100%" />
-    </defs>
     <defs class="graphic">
       <pattern
         :id="query('pattern')"
@@ -194,11 +79,127 @@
     <as-gradients :vector="vector" />
     <as-masks v-if="mask" :itemid="itemid" />
     <use :href="fragment('pattern')" />
-    <rect
-      v-if="emboss"
-      :filter="`url(${fragment('composite')})`"
-      width="100%"
-      height="100%" />
+    <g v-if="emboss">
+      <defs class="emboss">
+        <defs class="static filters">
+          <filter id="emboss">
+            <feConvolveMatrix
+              kernelMatrix="1.5 0 0
+                          0   0 0
+                          0   0 -1.5" />
+          </filter>
+          <filter id="emboss-opposite">
+            <feConvolveMatrix
+              kernelMatrix="0   0 1.5
+                          0   0 0
+                         -1.5 0 0" />
+          </filter>
+          <filter id="emboss-horizontal">
+            <feConvolveMatrix
+              kernelMatrix="0   0 0
+                          1.5 0 -1.5
+                          0   0 0" />
+          </filter>
+          <filter id="emboss-vertical">
+            <feConvolveMatrix
+              kernelMatrix="0    1.5 0
+                          0    0   0
+                          0   -1.5 0" />
+          </filter>
+        </defs>
+        <filter
+          :id="query('composite')"
+          color-interpolation-filters="sRGB"
+          y="0"
+          x="0"
+          width="100%"
+          height="100%">
+          <feImage :href="fragment('emboss-render-light')" result="light" />
+          <feImage :href="fragment('emboss-render-regular')" result="regular" />
+          <feImage :href="fragment('emboss-render-medium')" result="medium" />
+          <feImage :href="fragment('emboss-render-bold')" result="bold" />
+          <feImage :href="fragment('pattern-render')" result="framing" />
+          <feMerge>
+            <feMergeNode in="framing" />
+            <feMergeNode in="bold" />
+            <feMergeNode in="medium" />
+            <feMergeNode in="regular" />
+            <feMergeNode in="light" />
+          </feMerge>
+        </filter>
+
+        <pattern
+          :id="query('pattern-emboss-light')"
+          :width="vector.width"
+          :height="vector.height"
+          :viewBox="vector.viewbox"
+          patternUnits="userSpaceOnUse"
+          :preserveAspectRatio="aspect_ratio">
+          <use :href="fragment('light')" filter="url(#emboss)" opacity="0.45" />
+        </pattern>
+        <pattern
+          :id="query('pattern-emboss-regular')"
+          :width="vector.width"
+          :height="vector.height"
+          :viewBox="vector.viewbox"
+          patternUnits="userSpaceOnUse"
+          :preserveAspectRatio="aspect_ratio">
+          <use :href="fragment('regular')" filter="url(#emboss-vertical)" />
+        </pattern>
+        <pattern
+          :id="query('pattern-emboss-medium')"
+          :width="vector.width"
+          :height="vector.height"
+          :viewBox="vector.viewbox"
+          patternUnits="userSpaceOnUse"
+          :preserveAspectRatio="aspect_ratio">
+          <use :href="fragment('medium')" filter="url(#emboss-opposite)" />
+        </pattern>
+        <pattern
+          :id="query('pattern-emboss-bold')"
+          :width="vector.width"
+          :height="vector.height"
+          :viewBox="vector.viewbox"
+          patternUnits="userSpaceOnUse"
+          :preserveAspectRatio="aspect_ratio">
+          <use :href="fragment('bold')" filter="url(#emboss-horizontal)" />
+        </pattern>
+        <rect
+          :id="query('emboss-render')"
+          :fill="`url(${fragment('pattern-emboss')})`"
+          width="100%"
+          height="100%" />
+        <rect
+          :id="query('emboss-render-light')"
+          :fill="`url(${fragment('pattern-emboss-light')})`"
+          width="100%"
+          height="100%" />
+        <rect
+          :id="query('emboss-render-regular')"
+          :fill="`url(${fragment('pattern-emboss-regular')})`"
+          width="100%"
+          height="100%" />
+        <rect
+          :id="query('emboss-render-medium')"
+          :fill="`url(${fragment('pattern-emboss-medium')})`"
+          width="100%"
+          height="100%" />
+        <rect
+          :id="query('emboss-render-bold')"
+          :fill="`url(${fragment('pattern-emboss-bold')})`"
+          width="100%"
+          height="100%" />
+        <rect
+          id="lightbar-rect"
+          fill="url(#lightbar)"
+          width="100%"
+          height="100%" />
+      </defs>
+      <rect
+        :filter="`url(${fragment('composite')})`"
+        width="100%"
+        height="100%" />
+    </g>
     <g v-else>
       <use :href="fragment('background')" @focus="focus('bold')" />
       <use :href="fragment('light')" @focus="focus('light')" />
