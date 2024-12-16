1895c29d49041f6721edb169c4bb49eb57fd9eee
Scott Fryxell <1165347+scott-fryxell@users.noreply.github.com>
2024-12-16 11:17:19 -0800

    feature: paramatize halftone

--- components/posters/as-halftone.vue
+++ components/posters/as-halftone.vue
@@ -1,10 +1,16 @@
 <template></template>
 <style>
-  svg[itemtype='/posters'] {
-    background:
-      radial-gradient(closest-side, #777, #fff) 0/ 1em 1em space,
-      linear-gradient(91deg, #888, #fff);
-    filter: contrast(16);
+  svg[itemtype='/posters'] halftone {
+    font-size: 17px;
+    --s: 2em 2em;
+    --c: 21;
+    --dot: radial-gradient(closest-side, #777, #fff calc(100% / sqrt(2)));
+    --pattern: var(--dot) 0 0 / var(--s), var(--dot) 1em 1em / var(--s);
+
+    --map: linear-gradient(90deg, #888, #fff);
+
+    filter: contrast(var(--c));
     background-blend-mode: multiply;
+    background: var(--pattern), var(--map);
   }
 </style>
