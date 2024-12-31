ef55ae62249991a5d1f69ee40335af40773b31d2
Scott Fryxell <1165347+scott-fryxell@users.noreply.github.com>
2024-12-30 18:52:53 -0800

    Move code into a src directory.
    I give up the root of every dev project is a file nightmare
    
    eslint forces the issue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import { registerSW } from 'virtual:pwa-register'
 
-import App from './App.vue'
-import router from './router'
+import App from '@/App.vue'
+import router from '@/router'
 const me = localStorage.me
 if (!me) localStorage.me = '/+'
 
