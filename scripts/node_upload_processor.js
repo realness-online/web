8b05ce6179d2d26f900a60f7d6ed5f17a487f7a0
Scott Fryxell <1165347+scott-fryxell@users.noreply.github.com>
2025-01-01 13:53:05 -0800

    Compress files

---
+++ scripts/node_upload_processor.js
@@ -0,0 +1,38 @@
+import { gzip } from 'zlib'
+import { promisify } from 'util'
+import crypto from 'crypto'
+
+const gzip_async = promisify(gzip)
+
+const compress_data = async (data, filename = 'data') => {
+  return await gzip_async(data)
+}
+
+const create_hash = async (data, algorithm = 'SHA-256') => {
+  const hash = crypto.createHash(algorithm.toLowerCase())
+  hash.update(data)
+  return hash.digest('base64')
+}
+
+export const prepare_upload_data = async (items, path) => {
+  const data = await compress_data(items, path)
+  const content_hash = await create_hash(items)
+  const md5_hash = await create_hash(items, 'MD5')
+
+  return {
+    data,
+    content_hash,
+    md5_hash,
+    metadata: {
+      cacheControl: 'private, max-age=18000',
+      contentType: 'text/html; charset=utf-8',
+      contentLanguage: 'en-US', // Note: Using default since Node doesn't have navigator
+      contentEncoding: 'gzip',
+      contentDisposition: 'inline',
+      md5Hash: md5_hash,
+      customMetadata: {
+        ETag: `"${content_hash}"`
+      }
+    }
+  }
+}
