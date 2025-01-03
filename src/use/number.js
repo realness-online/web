58ab04fbfdf6e384cc99ba25147796abd26cff2e
Scott Fryxell <1165347+scott-fryxell@users.noreply.github.com>
2025-01-02 19:02:11 -0800

    feature: Building up our numbers system

--- src/use/number.js
+++
@@ -1,5 +0,0 @@
-export const to_kb = obj => {
-  const as_string = JSON.stringify(obj)
-  const size_of = new Blob([as_string]).size
-  return (size_of / 1024).toFixed(2)
-}
--- src/utils/numbers.js
+++ src/utils/numbers.js
@@ -8,3 +8,8 @@ export const format_bytes = bytes => {
   if (bytes < MB) return `${(bytes / KB).toFixed(1)} KB`
   return `${(bytes / MB).toFixed(1)} MB`
 }
+export const to_kb = obj => {
+  const as_string = JSON.stringify(obj)
+  const size_of = new Blob([as_string]).size
+  return (size_of / 1024).toFixed(2)
+}
