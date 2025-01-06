7c1de27575a993a75274d67d39de8b5f97518abd
Scott Fryxell <1165347+scott-fryxell@users.noreply.github.com>
2025-01-05 18:22:47 -0800

    fix: Properly handle and upload paged data
    This improves the history functinality by not writing the index file before confirming history is properly written.
    
    will batch the history creation of older statements and make apropriatly sized history (about 5kb) files

--- src/persistance/Storage.js
+++ src/persistance/Storage.js
@@ -3,9 +3,10 @@ import Local from '@/persistance/Local'
 import Large from '@/persistance/Large'
 import Cloud from '@/persistance/Cloud'
 import Paged from '@/persistance/Paged'
-import { current_user } from '@/use/serverless'
-import { as_type } from '@/utils/itemid'
+import { current_user, upload } from '@/use/serverless'
+import { as_type, as_filename } from '@/utils/itemid'
 import { get } from 'idb-keyval'
+import { prepare_upload_html } from '@/utils/upload_processor'
 export default class Storage {
   constructor(itemid) {
     this.id = itemid
@@ -44,10 +45,17 @@ export class Offline extends Cloud(Storage) {
 export class History extends Cloud(Storage) {
   async save(items) {
     // on purpose doesn't call super.save
-    if (!items) return
+    if (!items) return false
     if (current_user.value && navigator.onLine) {
-      const path = `/people/${this.id}.html`
-      await location(path).putString(items.outerHTML, 'raw', this.metadata)
+      const { compressed, metadata } = await prepare_upload_html(items)
+      try {
+        await upload(as_filename(this.id), compressed, metadata)
+      } catch (e) {
+        console.error(e)
+        return false
+      }
+      return true
     }
+    return false
   }
 }
