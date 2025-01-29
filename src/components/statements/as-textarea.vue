d16b422b3be97e8e463f5ffda89cfbeb5bbe6a05
Scott Fryxell <1165347+scott-fryxell@users.noreply.github.com>
2025-01-28 18:10:16 -0800

    fix: for display of statements form

--- src/components/statements/as-textarea.vue
+++ src/components/statements/as-textarea.vue
@@ -1,90 +1,166 @@
 <script setup>
-  import { ref } from 'vue'
+  import Icon from '@/components/icon'
+  import { ref, nextTick as next_tick, inject } from 'vue'
+
   import { use } from '@/use/statement'
 
   const emit = defineEmits(['toggle-keyboard'])
   const { save } = use()
+  const show_regular = inject('show_utility_components')
 
   /** @type {import('vue').Ref<string | null>} */
   const statement_text = ref(null)
 
-  const focused = () => emit('toggle-keyboard')
+  const focused = async () => {
+    emit('toggle-keyboard')
+    show_regular.value = false
+    const textarea = document.querySelector('textarea#wat')
+    textarea.focus()
+    await next_tick()
+    setTimeout(() => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      })
+      textarea.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+        inline: 'center'
+      })
+    }, 100)
+  }
 
   const prepare_statement = async () => {
+    console.info('prepare_statement')
+    show_regular.value = true
+    emit('toggle-keyboard')
+    const textarea = document.querySelector('textarea#wat')
+    textarea.style.height = ''
     const text = statement_text.value
+    statement_text.value = null
     if (typeof text !== 'string') return
 
     const trimmed = text.trim()
     if (!trimmed) return
 
-    emit('toggle-keyboard')
     await save(trimmed)
 
     if (statement_text.value === text) statement_text.value = null
 
     console.info('create:Statement')
   }
+
+  const adjust_height = event => {
+    const textarea = event.target
+    textarea.style.height = 'auto'
+    textarea.style.height = `${textarea.scrollHeight}px`
+  }
+
 </script>
 
 <template>
   <textarea
     id="wat"
+    v-bind="$attrs"
     v-model="statement_text"
     cols="1"
     rows="1"
     placeholder=">"
     :spellcheck="true"
+    @input="adjust_height"
     @focusout="prepare_statement"
     @focusin="focused" />
+  <button v-if="!show_regular" id="done">
+    <icon name="finished" />
+  </button>
 </template>
 
 <style lang="stylus">
   section#navigation.page
-    & textarea#wat
-      padding: base-line
-      border-radius: base-line
-      text-align: right
-      resize: none
-      appearance: none
-      transition-duration: 0.3s
-      user-select: text
-      border-style: solid
-      caret-color: red
-      cursor: pointer
-      transition-property: color, border-radius, text-align
-      color: red
-      @media (prefers-color-scheme: dark)
-        color: white
-      &::placeholder
-        transition-property: all
-        font-family: inherit
-        color: white
-        @media (prefers-color-scheme: light)
-          color: #fff
-    &.posting textarea#wat
-      font-size: 1.25em
-      font-weight: normal
-      padding: 0
-      grid-column: 1 / span 2
-      grid-row: 1 / span 3
-      text-align: left
-      border-top: none
-      border-radius: 0
-      border-width: 0
-      background-color: transparent
-      outline: 0
-      transition-duration: .3s
-      transition-property: border-radius, text-align
-      transition-timing-function: ease-out
-      line-height: base-line
-      height: auto
-      text-align: inherit
-      margin-top: base-line
-      padding: 0
-      border-radius: 0
-      &::placeholder
-        color: red
-        text-align: left
-        @media (prefers-color-scheme: light)
-          color: red
+    & textarea#wat {
+      padding: base-line;
+      border-radius: base-line;
+      text-align: right;
+      resize: none;
+      appearance: none;
+      transition-duration: 0.3s;
+      user-select: text;
+      border-style: solid;
+      caret-color: red;
+      cursor: pointer;
+      transition-property: color, border-radius, text-align;
+      color: red;
+      background-color: red;
+      min-height: base-line;
+      @media (prefers-color-scheme: dark) {
+        color: white-text;
+      }
+      &::placeholder {
+        transition-property: all;
+        font-family: inherit;
+        color: white-text;
+        @media (prefers-color-scheme: light) {
+          color: black-background;
+        }
+      }
+    }
+    & button#done {
+      color: white-text;
+      border-color: red;
+      border-radius: base-line;
+      padding: base-line;
+      position: fixed;
+      bottom: base-line;
+      right: base-line;
+      z-index: 4;
+      @media (min-width: pad-begins) {
+        position: inherit;
+      }
+    }
+    & button#done {
+      display: none;
+      border: none;
+      position: absolute;
+      top: base-line;
+      right: base-line;
+      width: fit-content;
+      height: fit-content;
+      padding: 0;
+      svg {
+        fill: red;
+      }
+    }
+    &.posting button#done {
+      display: block;
+    }
+    &.posting textarea#wat {
+      font-size: 1.25em;
+      font-weight: normal;
+      padding: 0;
+      grid-column: 1 / span 2;
+      grid-row: 1 / span 3;
+      text-align: left;
+      border-top: none;
+      border-radius: 0;
+      border-width: 0;
+      background-color: transparent;
+      outline: 0;
+      transition-duration: .3s;
+      transition-property: border-radius, text-align;
+      transition-timing-function: ease-out;
+      line-height: base-line;
+      min-height: base-line * 3;
+      height: auto;
+      text-align: inherit;
+      margin-top: base-line;
+      padding: 0;
+      border-radius: 0;
+      &::placeholder {
+        color: red;
+        text-align: left;
+        @media (prefers-color-scheme: light) {
+          color: red;
+        }
+      }
+    }
 </style>
--- src/views/Navigation.vue
+++ src/views/Navigation.vue
@@ -1,35 +1,21 @@
 <script setup>
   import Icon from '@/components/icon'
   import StatementAsTextarea from '@/components/statements/as-textarea'
-  import { ref, onMounted as mounted, inject, nextTick as next_tick } from 'vue'
+  import { ref, onMounted as mounted } from 'vue'
   import { use as use_vectorize } from '@/use/vectorize'
   import AccountDialog from '@/components/profile/as-dialog-account'
   const version = import.meta.env.PACKAGE_VERSION
 
-  const show_utility_components = inject('show_utility_components')
   const posting = ref(false)
   const nav = ref()
 
-  const toggle_keyboard = async () => {
+  const toggle_keyboard = () => {
     posting.value = !posting.value
-    if (show_utility_components) show_utility_components.value = !posting.value
-
-    if (posting.value) {
-      await next_tick()
-      document
-        .querySelector('textarea#wat')
-        .scrollIntoView({ behavior: 'smooth', block: 'center' })
-    }
-  }
-
-  const done_posting = () => {
-    document.querySelector('textarea#wat').blur()
-    posting.value = false
-    if (show_utility_components) show_utility_components.value = true
   }
 
   const { vVectorizer, image_picker, open_camera, mount_workers } =
     use_vectorize()
+
   mounted(() => mount_workers())
 </script>
 
@@ -55,10 +41,9 @@
         Thoughts
       </router-link>
       <statement-as-textarea class="red" @toggle-keyboard="toggle_keyboard" />
-      <button v-if="posting" @click="done_posting">Done</button>
     </nav>
     <footer v-if="!posting">
-      <button>{{ version }}</button>
+      <span>{{ version }}</span>
       <a id="camera" @click="open_camera">
         <icon name="camera" />
       </a>
@@ -77,120 +62,136 @@
 <style lang="stylus">
   section#navigation.page
     display: flex
-    align-items: center
-    flex-direction: column
-    justify-content: center
-    max-width: page-width
-    @media (max-width: pad-begins) and (orientation: portrait)
-      padding: 0 base-line
-    @media (max-height: pad-begins) and (orientation: landscape)
-      height: auto
-      max-width: none
-    a#toggle-name
-      position: fixed
-      top: inset(top,  base-line )
-      left: base-line
-    & > header
-      opacity: 0.66
-      position: fixed
-      bottom: 0
-      left: 0
-      @media (min-width: pad-begins)
-        top: env(safe-area-inset-top) !important
-      &:hover, &:active
-        opacity: 1
-    &.posting
-      align-self: start
-      margin-top: inset(top)
-      height: inherit
-      @media (max-width: pad-begins)
-        align-items: flex-start
-      & > footer
-        height: 50dvh
-      & > nav
-        width: 100%
-        transition-duration: 0.5s
-        min-height: inherit
-        & > button
-          width: base-line * 3
-          height: base-line * 1.66
-          line-height: 0
-          padding: 0
-          display: block
-    & > nav
-      transition-duration: 0s
-      display: grid
-      grid-gap: base-line
-      grid-template-columns: 1fr 1fr
-      align-items: stretch
-      min-height: round(base-line * 18, 2)
-      max-height: page-width
-      min-width: 33dvw
-      margin-bottom: base-line * 2
-      margin-top: base-line * 2
-      @media (orientation: landscape) and (display-mode: standalone) and (max-height: pad-begins)
-        display:none
-      @media (max-height: pad-begins) and (orientation: landscape)
-        min-height: auto
-        padding: base-line (base-line * 4)
-      & > a
-        text-transform: capitalize
-        text-align: left
-        border-width: 1px
-        border-style: solid
-        &:focus
-          color: transparent
-          transition-duration: 0.6s
-          transition: all
-          outline: none
-        &:nth-child(odd)
-         text-align: left
-        &:active
-          border-width: 1vmax
-          color:transparent
-      & > a
-      & > textarea
-        padding: base-line
-        border-radius: base-line
-        text-align: right
-        &::placeholder
-          @media (prefers-color-scheme: light)
-            color: #fff
-      & > a
-        @media (prefers-color-scheme: light)
-          color: #fff
-    & > footer
-      display: flex
-      justify-content: space-between
-      align-items: center
-      min-width: 33dvw
-      padding: 0
-      & > button
-      & > a
-      & > textarea
-        margin: 0
-        padding: 0
-        bottom: base-line
-      &#camera
-        border-radius: base-line
-        padding: base-line * 0.5
-        position: fixed
-        bottom: base-line
-        right: s('calc( 50% - %s)', (base-line * 1.75) )
-        z-index: 4
-        @media (min-width: pad-begins)
-          position:inherit
-        svg
-          fill: red
-      & > button
-        left: base-line
-        border: none
-        font-size: 0.66em
-      & > a
-        color: yellow
-        right: base-line
-      & > menu
-        display: flex
-        align-items: center
-        justify-content: space-between
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    max-width: page-width;
+    @media (max-width: pad-begins) and (orientation: portrait) {
+      padding: 0 base-line;
+    }
+    @media (max-height: pad-begins) and (orientation: landscape) {
+      height: auto;
+      max-width: none;
+    }
+
+    & > header {
+      opacity: 0.66;
+      position: fixed;
+      bottom: 0;
+      left: 0;
+      @media (min-width: pad-begins) {
+        top: env(safe-area-inset-top) !important;
+      }
+      &:hover, &:active {
+        opacity: 1;
+      }
+    }
+    &.posting {
+      align-self: start;
+      margin-top: inset(top);
+      height: inherit;
+      @media (max-width: pad-begins) {
+        align-items: flex-start;
+      }
+      & > footer {
+        height: 50dvh;
+      }
+      & > nav {
+        width: 100%;
+        transition-duration: 0.5s;
+        min-height: inherit;
+      }
+    }
+    & > nav {
+      transition-duration: 0s;
+      display: grid;
+      grid-gap: base-line;
+      grid-template-columns: 1fr 1fr;
+      align-items: stretch;
+      min-height: round(base-line * 18, 2);
+      max-height: page-width;
+      min-width: 40dvw;
+      width: 100%;
+      margin-bottom: base-line * 2;
+      margin-top: base-line * 2;
+      @media (orientation: landscape) and (display-mode: standalone) and (max-height: pad-begins) {
+        display: none;
+      }
+      @media (max-height: pad-begins) and (orientation: landscape) {
+        min-height: auto;
+        padding: base-line (base-line * 4);
+      }
+      & > a {
+        text-transform: capitalize;
+        text-align: left;
+        border-width: 1px;
+        border-style: solid;
+        &:focus {
+          color: transparent;
+          transition-duration: 0.6s;
+          transition: all;
+          outline: none;
+        }
+        &:nth-child(odd) {
+          text-align: left;
+        }
+        &:active {
+          border-width: 1vmax;
+          color: transparent;
+        }
+      }
+      & > a, & > textarea {
+        padding: base-line;
+        border-radius: base-line;
+        text-align: right;
+        &::placeholder {
+          @media (prefers-color-scheme: light) {
+            color: #fff;
+          }
+        }
+      }
+      & > a {
+        @media (prefers-color-scheme: light) {
+          color: #fff;
+        }
+      }
+    }
+    & > footer {
+      display: flex;
+      justify-content: space-between;
+      width: 100%;
+      align-items: center;
+      padding: 0 base-line;
+      & > a {
+        margin: 0;
+        padding: 0;
+      }
+      #camera {
+        border-radius: base-line;
+        padding: base-line * 0.5;
+        position: fixed;
+        bottom: base-line;
+        right: s('calc( 50% - %s)', (base-line * 1.5));
+        z-index: 4;
+        svg {
+          fill: red;
+        }
+      }
+      & > span {
+        color: blue;
+        padding: 0;
+        margin: 0;
+        border: none;
+        font-size: 0.66em;
+      }
+      & > a {
+        color: blue;
+        right: base-line;
+      }
+      & > menu {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+      }
+    }
 </style>
