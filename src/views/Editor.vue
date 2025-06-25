76a0ad4b57c5bd61d70af097a1fd95633b29ccf8
Scott Fryxell <1165347+scott-fryxell@users.noreply.github.com>
2025-06-24 20:26:43 -0700

    refactor: remove all the editor options

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -1,33 +1,14 @@
 <script setup>
+
   import Icon from '@/components/icon'
   import AsFill from '@/components/posters/as-figure-fill'
-  import AsAnimation from '@/components/posters/as-animation'
-  import AsGrid from '@/components/posters/as-grid'
   import { Poster } from '@/persistance/Storage'
-  import {
-    useFullscreen as use_fullscreen,
-    useMagicKeys as use_Keyboard
-  } from '@vueuse/core'
   import { useRoute as use_route, useRouter as use_router } from 'vue-router'
-  import { watch, computed, ref, provide } from 'vue'
+  import { provide } from 'vue'
   import { use as use_vectorize } from '@/use/vectorize'
-  const fill = ref(true)
-  const stroke = ref(false)
-  const animation = ref(false)
-  const grid = ref(false)
-  const menu = ref(true)
   const route = use_route()
   const router = use_router()
-  const itemid = `${localStorage.me}/posters/${route.params.id}`
-
-  const toggle_stroke = () => {
-    stroke.value = !stroke.value
-    fill.value = !fill.value
-  }
-  const color = computed(() => {
-    if (stroke.value || fill.value) return true
-    return false
-  })
+  const itemid = /** @type {import('@/types').Id} */ (`${localStorage.me}/posters/${Number(route.params.id)}`)
 
   const back = () => {
     const me = localStorage.me.substring(2)
@@ -43,52 +24,19 @@
     if (new_gradients.value) new_gradients.value = null
     back()
   }
-  const just_poster = () => {
-    if (fullscreen_supported.value) fullscreen()
-    else menu.value = false
-  }
-  const {
-    toggle: fullscreen,
-    isFullscreen: is_fullscreen,
-    isSupported: fullscreen_supported
-  } = use_fullscreen()
-  const { f, enter, escape } = use_Keyboard()
 
   const { new_vector, new_gradients } = use_vectorize()
   if (new_vector.value) provide('new-poster', true)
 
-  watch(enter, v => {
-    if (v) save()
-  })
-  watch(escape, v => {
-    if (v) back()
-  })
-  watch(is_fullscreen, v => {
-    if (!v) return
-    if (!is_fullscreen.value) menu.value = true
-  })
-  watch(f, v => {
-    if (!v) return
-    just_poster()
-  })
 </script>
 
 <template>
   <section id="editor" class="page">
-    <header v-if="menu" ref="header">
+    <header ref="header">
       <a @click="back"><icon name="remove" /></a>
       <a @click="save"><icon name="finished" /></a>
     </header>
-    <as-fill v-if="fill || stroke" :itemid="itemid" @toggle="toggle_stroke" />
-    <as-grid v-if="grid" :itemid="itemid" />
-    <as-animation v-if="animation" :itemid="itemid" />
-    <footer v-if="!is_fullscreen || !menu" hidden>
-      <menu>
-        <icon :class="{ selected: color }" name="edit-color" />
-        <icon :class="{ selected: grid }" name="grid" />
-        <icon :class="{ selected: animation }" name="animation" />
-      </menu>
-    </footer>
+    <as-fill :itemid="itemid"/>
   </section>
 </template>
 
@@ -112,7 +60,6 @@
         z-index: 2
         text-shadow: 1px 1px 1px black-background
     & > header > a  > svg
-    & > footer > menu > svg
       cursor: pointer
       fill: green
       .selected
@@ -127,15 +74,7 @@
       &.fullscreen
       &.finished
         fill-opacity: inherit
-    & > footer > menu
-      background-color: black-transparent
-      position: fixed
-      padding: base-line
-      bottom: 0
-      left: 0
-      right: 0
-      display: flex
-      justify-content: space-between
+
       & > svg
         z-index: 2
         &.selected
