4bb466acdf6db489f1d07c4dee03f51115f4d31b
Scott Fryxell <1165347+scott-fryxell@users.noreply.github.com>
2025-09-30 15:24:48 -0700

    chore: formating

--- tests/mocks/browser/svg.js
+++ tests/mocks/browser/svg.js
@@ -1,12 +1,6 @@
 import { vi } from 'vitest'
 
-// Mock getTotalLength for SVG path elements
-// Add it to Element prototype since happy-dom doesn't have SVGPathElement.getTotalLength
-if (typeof Element !== 'undefined' && !Element.prototype.getTotalLength) {
+if (typeof Element !== 'undefined' && !Element.prototype.getTotalLength)
   Element.prototype.getTotalLength = vi.fn().mockReturnValue(100)
-}
-
-// Also try to mock SVGPathElement if it exists
-if (typeof global.SVGPathElement !== 'undefined') {
+if (typeof global.SVGPathElement !== 'undefined')
   global.SVGPathElement.prototype.getTotalLength = vi.fn().mockReturnValue(100)
-}
