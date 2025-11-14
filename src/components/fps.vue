50c0f029449cea5e9b73f551d7ad3e9d69984f99
Scott Fryxell <1165347+scott-fryxell@users.noreply.github.com>
2025-11-13 20:02:42 -0800

    UX: Fine tune aspect ratio work.

--- src/components/fps.vue
+++ src/components/fps.vue
@@ -1,15 +1,68 @@
 <script setup>
+  import { ref, computed, onMounted as mounted, onUnmounted as unmounted } from 'vue'
   import { useFps } from '@vueuse/core'
+  import { animate, animation_speed, aspect_ratio_mode, slice } from '@/utils/preference'
 
   const fps = useFps()
+
+  const animation_status = computed(() => {
+    if (!animate.value) return 'anim:off'
+    return `anim:${animation_speed.value}`
+  })
+
+  const aspect_ratio = computed(() => aspect_ratio_mode.value || 'auto')
+
+  const fps_color = computed(() => {
+    if (fps.value >= 51) return 'green'
+    if (fps.value < 24) return 'red'
+    if (fps.value < 50) return 'orange'
+    return 'green'
+  })
+
+  const fps_style = computed(() => ({
+    '--animate': animate.value ? 1 : 0,
+    '--slice': slice.value ? 1 : 0,
+    '--animate-color': animate.value ? 'red' : 'grey',
+    '--slice-color': slice.value ? 'yellow' : 'grey',
+    '--fps-color': fps_color.value
+  }))
+
+const animation_time = ref(0)
+  const max_cycle_time = 172 // Longest animation cycle in seconds
+
+  let frame_id = null
+
+  const update_animation_time = () => {
+    const svg_element = document.querySelector('svg[itemtype="/posters"]')
+    if (svg_element && svg_element instanceof SVGSVGElement) {
+      const current = svg_element.getCurrentTime()
+      animation_time.value = current % max_cycle_time
+    }
+    frame_id = requestAnimationFrame(update_animation_time)
+  }
+
+  mounted(() => {
+    update_animation_time()
+  })
+
+  unmounted(() => {
+    if (frame_id) cancelAnimationFrame(frame_id)
+  })
 </script>
 
 <template>
-  <span id="fps">{{ fps }}fps</span>
+  <aside id="fps" :style="fps_style">
+    <meter :value="fps" :min="0" :max="60" :optimum="60" :low="24" :high="55">{{ fps }}fps</meter>
+    <meter :value="animation_time" :min="0" :max="max_cycle_time">
+      {{ animation_time.toFixed(0) }}s / {{ max_cycle_time }}s
+    </meter>
+    <output>{{ animation_status }}</output>
+    <output>aspect: {{ aspect_ratio }}</output>
+  </aside>
 </template>
 
 <style lang="stylus">
-  span#fps
+  aside#fps
     position: fixed
     bottom: base-line * .5
     right: base-line * .5
@@ -20,4 +73,47 @@
     font-size: base-line
     text-shadow: -0.66px -0.66px .51px blue
     z-index: 1000
+    font-family: monospace
+    display: flex
+    flex-direction: column
+    gap: base-line * .25
+
+    > meter:first-of-type
+      accent-color: var(--fps-color)
+      color: var(--fps-color)
+      &::-webkit-meter-bar
+        background: transparent
+      &::-webkit-meter-optimum-value
+        background: var(--fps-color)
+      &::-webkit-meter-suboptimum-value
+        background: var(--fps-color)
+      &::-webkit-meter-even-less-good-value
+        background: var(--fps-color)
+      &::-moz-meter-bar
+        background: var(--fps-color)
+      &::-moz-meter-optimum::-moz-meter-bar
+        background: var(--fps-color)
+      &::-moz-meter-sub-optimum::-moz-meter-bar
+        background: var(--fps-color)
+      &::-moz-meter-sub-sub-optimum::-moz-meter-bar
+        background: var(--fps-color)
+    > meter:last-of-type
+      accent-color: blue
+      color: blue
+      font-size: 66%
+      &::-webkit-meter-bar
+        background: transparent
+      &::-webkit-meter-optimum-value
+        background: blue
+      &::-moz-meter-bar
+        background: blue
+    > output
+      font-size: 66%
+      opacity: 0.5 !important
+    > output:nth-of-type(1)
+      color: var(--animate-color)
+      opacity: calc(var(--animate) * 1 + (1 - var(--animate)) * 0.5)
+    > output:nth-of-type(2)
+      color: var(--slice-color)
+      opacity: calc(var(--slice) * 1 + (1 - var(--slice)) * 0.5)
 </style>
